e7e80c1ba03bf757d3dab26a66c928f7fa7c431a
Desmond Hui <desmond@flipsidecrypto.com>
2022-03-01 14:50:45 -0800

    drop _internal schema for dev db

--- macros/create_tasks.sql
+++ macros/create_tasks.sql
@@ -1,7 +1,6 @@
 {% macro create_tasks() %}
-    CREATE SCHEMA IF NOT EXISTS _internal;
-
     {% if target.database == 'SOLANA' %}
+        CREATE SCHEMA IF NOT EXISTS _internal;
         {{ task_run_sp_create_prod_clone('_internal') }};
     {% endif %}
 
