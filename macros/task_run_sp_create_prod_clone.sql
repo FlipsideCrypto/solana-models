bbf882323f44e3d65521eb5b89e4f8f6e5cd610d
Desmond Hui <desmond@flipsidecrypto.com>
2022-03-01 08:58:49 -0800

    set task to resumed state after create

--- macros/task_run_sp_create_prod_clone.sql
+++ macros/task_run_sp_create_prod_clone.sql
@@ -3,5 +3,7 @@
         warehouse = dbt_cloud
         schedule = 'USING CRON 15 6 * * * UTC'
     as
-        call {{ target_schema }}.create_prod_clone('solana', 'solana_dev', 'internal_dev')
+        call {{ target_schema }}.create_prod_clone('solana', 'solana_dev', 'internal_dev');
+
+    alter task {{ target_schema }}.run_sp_create_prod_clone resume
 {%- endmacro %}
