52d2751eae3c2262d6185ff699abc0e0d2c4bc04
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-03-07 09:28:59 -0800

    An 841/votes block agg (#8)
    * aggregate votes by block
    
    * add desc and tests

---
+++ models/silver/silver__votes_agg_block.sql
@@ -0,0 +1,29 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "CONCAT_WS('-', block_id, tx_id)",
+    incremental_strategy = 'delete+insert',
+    cluster_by = ['block_timestamp::DATE'],
+) }}
+
+WITH v AS (
+  SELECT 
+    block_id, 
+    count(block_id) AS num_votes
+  FROM {{ ref('silver__votes') }}
+  
+  {% if is_incremental() %}
+     WHERE ingested_at >= getdate() - interval '2 days'
+  {% endif %}
+
+  GROUP BY block_id
+)
+
+SELECT 
+  t.block_timestamp,
+  v.block_id, 
+  num_votes
+
+FROM v
+
+INNER JOIN (SELECT DISTINCT block_id, block_timestamp FROM {{ ref('silver__transactions') }}) t 
+ON v.block_id = t.block_id
