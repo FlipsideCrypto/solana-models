3cbbc0949e2f13599466edf6f24feca288e82849
Desmond Hui <desmond@flipsidecrypto.com>
2022-03-08 17:57:38 -0800

    create transfers table

--- models/silver/silver__transfers.sql
+++ models/silver/silver__transfers.sql
@@ -5,143 +5,159 @@
     cluster_by = ['ingested_at::DATE'],
 ) }}
 
-WITH postTokenBalances AS (
-    SELECT 
-        tx_id, 
-        index,
-        account_index,
-        mint, 
-        owner, 
-        decimal
-    FROM {{ ref('silver___post_token_balances') }}  
-), 
+WITH base_transfers AS (
 
-base_spl AS (
-    SELECT 
+    SELECT
         e.block_id,
-        e.block_timestamp, 
-        post.tx_id,
-        e.index, 
-        post.account_index,
-        post.mint, 
-        post.owner,  
-        COALESCE(
-          e.instruction :parsed :info :tokenAmount : decimals, 
-          post.decimal
-        ) AS decimal, 
-        COALESCE(
-          e.instruction :parsed :info :amount :: INTEGER, 
-          e.instruction :parsed :info :tokenAmount :amount :: INTEGER  
-        ) AS amount, 
-        e.instruction :parsed :info :authority :: STRING AS authority, 
+        e.block_timestamp,
+        e.tx_id,
+        e.index,
+        e.instruction,
         e.ingested_at
-    FROM postTokenBalances post 
-    
-    LEFT OUTER JOIN {{ ref('silver__events') }} e 
-    ON e.tx_id = post.tx_id 
+    FROM
+        {{ ref('silver__events') }}
+        e
+        INNER JOIN {{ ref('silver__transactions') }}
+        t
+        ON t.tx_id = e.tx_id
 
-    WHERE e.event_type IN ('transfer', 'transferChecked') 
-    AND e.instruction :parsed :info :authority :: STRING IS NOT NULL
+{% if is_incremental() %}
+AND t.ingested_at >= CURRENT_DATE - 2
+{% endif %}
+WHERE
+    e.event_type IN (
+        'transfer',
+        'transferChecked'
+    )
+    AND t.succeeded = TRUE
 
-    {% if is_incremental() %}
-        AND ingested_at :: DATE >= CURRENT_DATE - 2
-    {% endif %}
+{% if is_incremental() %}
+AND e.ingested_at :: DATE >= CURRENT_DATE - 2
+{% endif %}
 ),
-
-base_sol AS (
-    SELECT 
-        block_id,
-        block_timestamp, 
+base_post_token_balances AS (
+    SELECT
         tx_id,
-        index, 
-        instruction :parsed :info :source :: STRING AS tx_from, 
-        instruction :parsed :info :destination :: STRING AS tx_to, 
-        instruction :parsed :info :lamports / POW(10,9) AS amount,
+        owner,
+        account,
+        mint,
+        DECIMAL
+    FROM
+        {{ ref('silver___post_token_balances') }}
+
+{% if is_incremental() %}
+WHERE
+    ingested_at :: DATE >= CURRENT_DATE - 2
+{% endif %}
+),
+base_pre_token_balances AS (
+    SELECT
+        tx_id,
+        owner,
+        account,
+        mint,
+        DECIMAL
+    FROM
+        {{ ref('silver___pre_token_balances') }}
+
+{% if is_incremental() %}
+WHERE
+    ingested_at :: DATE >= CURRENT_DATE - 2
+{% endif %}
+),
+spl_transfers AS (
+    SELECT
+        e.block_id,
+        e.block_timestamp,
+        e.tx_id,
+        e.index,
+        COALESCE(
+            p.owner,
+            e.instruction :parsed :info :authority :: STRING
+        ) AS tx_from,
+        COALESCE(
+            p2.owner,
+            instruction :parsed :info :destination :: STRING
+        ) AS tx_to,
+        COALESCE(
+            e.instruction :parsed :info :tokenAmount: decimals,
+            p.decimal,
+            p2.decimal,
+            p3.decimal,
+            p4.decimal
+        ) AS decimal_adj,
+        COALESCE (
+            e.instruction :parsed :info :amount :: INTEGER,
+            e.instruction :parsed :info :tokenAmount :amount :: INTEGER
+        ) / pow(
+            10,
+            decimal_adj
+        ) AS amount,
+        COALESCE(
+            p.mint,
+            p2.mint,
+            p3.mint,
+            p4.mint
+        ) AS mint,
+        e.ingested_at
+    FROM
+        base_transfers e
+        LEFT OUTER JOIN base_pre_token_balances p
+        ON e.tx_id = p.tx_id
+        AND e.instruction :parsed :info :source :: STRING = p.account
+        LEFT OUTER JOIN base_post_token_balances p2
+        ON e.tx_id = p2.tx_id
+        AND e.instruction :parsed :info :destination :: STRING = p2.account
+        LEFT OUTER JOIN base_post_token_balances p3
+        ON e.tx_id = p3.tx_id
+        AND e.instruction :parsed :info :source :: STRING = p3.account
+        LEFT OUTER JOIN base_pre_token_balances p4
+        ON e.tx_id = p4.tx_id
+        AND e.instruction :parsed :info :destination :: STRING = p4.account
+    WHERE
+        e.instruction :parsed :info :authority :: STRING IS NOT NULL
+),
+sol_transfers AS (
+    SELECT
+        e.block_id,
+        e.block_timestamp,
+        e.tx_id,
+        e.index,
+        instruction :parsed :info :source :: STRING AS tx_from,
+        instruction :parsed :info :destination :: STRING AS tx_to,
+        instruction :parsed :info :lamports / pow(
+            10,
+            9
+        ) AS amount,
         'So11111111111111111111111111111111111111112' AS mint,
-        ingested_at 
-
-    FROM {{ ref('silver__events') }} 
-    
-    WHERE event_type IN ('transfer', 'transferChecked') 
-    AND instruction :parsed :info :lamports :: STRING IS NOT NULL  
-
-    {% if is_incremental() %}
-        AND ingested_at :: DATE >= CURRENT_DATE - 2
-    {% endif %}
-), 
-
-sender AS (
-    SELECT 
-        tx_id, 
-        account_index, 
-        CASE 
-            WHEN owner = authority THEN owner
-            ELSE authority 
-        END AS tx_from 
-    FROM base_spl t       
-), 
-
-receiver AS (
-    SELECT 
-        tx_id,
-        account_index, 
-        owner AS tx_to
-    FROM base_spl t
-    WHERE owner <> authority
-), 
-
-spl_token AS (
-  SELECT 
-       block_id,
-       block_timestamp, 
-       t.tx_id,
-       t.index, 
-       t.account_index, 
-       s.tx_from, 
-       r.tx_to, 
-       t.decimal, 
-       amount / POW(10, t.decimal) AS amount,
-       mint,
-       ingested_at
-  FROM base_spl t
-
-  LEFT OUTER JOIN sender s
-  ON t.tx_id = s.tx_id 
-
-  LEFT OUTER JOIN receiver r
-  ON t.tx_id = r.tx_id AND t.account_index = r.account_index
-
-  WHERE tx_to IS NOT NULL
+        e.ingested_at
+    FROM
+        base_transfers e
+    WHERE
+        instruction :parsed :info :lamports :: STRING IS NOT NULL
 )
-
-SELECT 
-    block_id, 
-    block_timestamp, 
-    tx_id, 
-    index, 
-    tx_from, 
-    tx_to, 
-    amount, 
-    mint, 
-    ingested_at 
-
-FROM base_sol
-
-UNION ALL 
-
-SELECT 
-    block_id, 
-    block_timestamp, 
-    tx_id, 
-    index, 
-    tx_from, 
-    tx_to, 
-    amount, 
-    mint, 
+SELECT
+    block_id,
+    block_timestamp,
+    tx_id,
+    INDEX,
+    tx_from,
+    tx_to,
+    amount,
+    mint,
     ingested_at
-    
-FROM spl_token
-
-qualify(ROW_NUMBER() over(PARTITION BY block_id, tx_id, index
-ORDER BY
-  ingested_at DESC)) = 1
+FROM
+    spl_transfers
+UNION
+SELECT
+    block_id,
+    block_timestamp,
+    tx_id,
+    INDEX,
+    tx_from,
+    tx_to,
+    amount,
+    mint,
+    ingested_at
+FROM
+    sol_transfers
