079d6fd1607286056583a105dcc74e4c4242944f
Desmond Hui <desmond@flipsidecrypto.com>
2022-03-10 15:39:34 -0800

    add core views and expected column checks

---
+++ models/core/core__fact_transactions.sql
@@ -0,0 +1,21 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+SELECT 
+    block_timestamp,
+    block_id,
+    tx_id,
+    recent_block_hash,
+    signers,
+    fee,
+    succeeded,
+    account_keys,
+    pre_balances,
+    post_balances,
+    pre_token_balances,
+    post_token_balances,
+    instructions,
+    inner_instructions
+FROM
+    {{ ref('silver__transactions') }}
---
+++ models/core/core__fact_transfers.sql
@@ -0,0 +1,15 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+SELECT 
+    block_timestamp,
+    block_id,
+    tx_id,
+    index,
+    tx_from,
+    tx_to,
+    amount,
+    mint
+FROM
+    {{ ref('silver__transfers') }}
