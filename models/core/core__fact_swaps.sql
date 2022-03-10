008d4f4270789da66e6b119044b6c0634964e802
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-03-10 11:45:33 -0800

    core swaps (#19)

---
+++ models/core/core__fact_swaps.sql
@@ -0,0 +1,31 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+SELECT
+    'jupiter aggregator v2' AS swap_program,
+    block_timestamp,
+    block_id,
+    tx_id,
+    succeeded,
+    swapper,
+    from_amt AS swap_from_amount,
+    from_mint AS swap_from_mint,
+    to_amt AS swap_to_amount,
+    to_mint AS swap_to_mint
+FROM
+    {{ ref('silver__swaps_jupiter_dex') }}
+UNION
+SELECT
+    'orca' AS swap_program,
+    block_timestamp,
+    block_id,
+    tx_id,
+    succeeded,
+    swapper,
+    from_amt,
+    from_mint,
+    to_amt,
+    to_mint
+FROM
+    {{ ref('silver__swaps_orca_dex') }}
