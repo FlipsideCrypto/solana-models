f3479d4e17dc1040d93b295dba4f562545bde10b
jhuhnke <jessica@flipsidecrypto.com>
2022-03-11 11:58:04 -0600

    changes to ME NFTs

--- models/silver/silver__nfts_magic_eden_v1.sql
+++ models/silver/silver__nfts_magic_eden_v1.sql
@@ -6,42 +6,51 @@
 ) }}
 
 WITH txs AS (
+    SELECT 
+        block_timestamp, 
+        block_id, 
+        tx_id, 
+        program_id,  
+        i.value :parsed :info :lamports / POW(10,9) AS amount, 
+        i.value :parsed :info :account :: STRING AS account, 
+        i.value :parsed :info :newAuthority :: STRING as owner, 
+        inner_instruction, 
+        ingested_at
+    FROM "SOLANA_DEV"."SILVER"."EVENTS", 
+    table(flatten(inner_instruction:instructions)) i
+   
+    WHERE program_id = 'MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8' -- Magic Eden V1 Program ID 
+    AND array_size(inner_instruction:instructions) > 2
+
+    {% if is_incremental() %}
+      AND ingested_at::date >= getdate() - interval '2 days'
+    {% endif %}
+    
+),   
+
+sales_amount AS (
   SELECT 
-      block_timestamp, 
-      block_id, 
-      tx_id, 
-      index, 
-      program_id, 
-      instruction :accounts[0] :: STRING AS purchaser,  
-       COALESCE (inner_instruction:instructions[0]:parsed:info:lamports/POW(10,9), 0) as price_0, 
-       COALESCE (inner_instruction:instructions[1]:parsed:info:lamports/POW(10,9), 0) as price_1,
-       COALESCE (inner_instruction:instructions[2]:parsed:info:lamports/POW(10,9), 0) as price_2, 
-       COALESCE (inner_instruction:instructions[3]:parsed:info:lamports/POW(10,9), 0) as price_3, 
-      COALESCE (inner_instruction:instructions[4]:parsed:info:lamports/POW(10,9), 0) as price_4, 
-      COALESCE (inner_instruction:instructions[5]:parsed:info:lamports/POW(10,9), 0) as price_5,
-      (price_0 + price_1 + price_2 + price_3 + price_4 + price_5) AS sale_amount,  
-      ingested_at
-  FROM "SOLANA_DEV"."SILVER"."EVENTS"
-
-  WHERE program_id = 'MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8' -- Magic Eden V1 Program ID
-  AND array_size(inner_instruction:instructions) > 2
-
-  {% if is_incremental() %}
-      AND ingested_at >= getdate() - interval '2 days'
-  {% endif %}
-)
+    tx_id, 
+    sum(amount) AS sales_amount
+  FROM txs 
+  GROUP BY tx_id
+) 
 
 SELECT 
     t.block_timestamp, 
     t.block_id, 
     t.tx_id, 
-    t.index, 
     t.program_id, 
-    purchaser, 
+    s.sales_amount,
     p.mint AS NFT, 
-    sale_amount, 
+    t.owner AS purchaser, 
     t.ingested_at
 FROM txs t
 
+INNER JOIN sales_amount s
+ON s.tx_id = t.tx_id
+
 INNER JOIN "SOLANA_DEV"."SILVER"."_POST_TOKEN_BALANCES" p
-ON t.tx_id = p.tx_id AND t.index = p.index
+ON p.tx_id = t.tx_id AND p.account = t.account
+
+WHERE t.account IS NOT NULL 
--- models/silver/silver__nfts_magic_eden_v2.sql
+++ models/silver/silver__nfts_magic_eden_v2.sql
@@ -13,30 +13,111 @@ WITH txs AS (
     WHERE program_id = 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K' -- Magic Eden V2 Program ID
     
     {% if is_incremental() %}
-      AND ingested_at >= getdate() - interval '2 days'
+      AND ingested_at::date >= getdate() - interval '2 days'
     {% endif %}
+
+    GROUP BY tx_id HAVING count(tx_id) >= 2
+),  
+
+last_index AS (
+  SELECT
+      e.block_timestamp, 
+      e.block_id, 
+      e.tx_id,
+      t.ct, 
+      e.index, 
+      e.program_id,
+      instruction, 
+      inner_instruction, 
+      e.ingested_at
+  FROM txs t
+
+  INNER JOIN {{ ref('silver__events') }} e
+  ON t.tx_id = e.tx_id
+
+  WHERE e.index = t.ct - 1
+
+  {% if is_incremental() %}
+      AND ingested_at::date >= getdate() - interval '2 days'
+  {% endif %}
+),  
+
+amounts_agg AS (
+  SELECT
+      block_timestamp, 
+      block_id, 
+      tx_id,
+      i.index AS index, 
+      program_id,
+      i.value :parsed :info :lamports / POW(10,9) as amount, 
+      i.value :parsed :info :mint :: STRING AS NFT, 
+      i.value :parsed :info :wallet :: STRING AS wallet, 
+      ingested_at
+  FROM last_index, 
+  table(flatten(inner_instruction:instructions)) i 
+  
+  WHERE amount IS NOT NULL 
+  OR wallet IS NOT NULL
+),  
+
+max_index_remove AS (
+  SELECT
+        tx_id, 
+        max(index) AS max_index
+  FROM amounts_agg
+  GROUP BY tx_id
+), 
+
+NFT AS (
+    SELECT 
+        tx_id, 
+        NFT, 
+        wallet
+    FROM amounts_agg
+    WHERE NFT IS NOT NULL 
+    AND wallet IS NOT NULL
+), 
+
+amounts AS (
+    SELECT 
+      block_timestamp, 
+      block_id, 
+      a.tx_id,
+      index,
+      program_id,
+      amount,  
+      ingested_at
+    FROM amounts_agg a
     
+    INNER JOIN max_index_remove m
+    ON m.tx_id = a.tx_id
+  
+    WHERE index <> m.max_index   
+),  
+
+sales_amount AS (
+    SELECT 
+        tx_id, 
+        sum(amount) AS sales_amount
+    FROM amounts
     GROUP BY tx_id
-) 
+)
 
-SELECT
-    e.block_timestamp, 
-    e.block_id, 
-    e.tx_id,
-    t.ct, 
-    e.index, 
-    e.program_id,
-    instruction :accounts[0] :: STRING AS purchaser, 
-    p.mint AS NFT, 
-    inner_instruction :instructions[0] :parsed :info :lamports / POW(10,9) :: INTEGER AS sale_amount, 
-    e.ingested_at
-FROM txs t
+SELECT 
+    block_timestamp, 
+    block_id, 
+    a.tx_id, 
+    program_id,
+    s.sales_amount, 
+    n.NFT, 
+    n.wallet AS purchaser, 
+    ingested_at
+FROM amounts a
 
-INNER JOIN {{ ref('silver__events') }} e
-ON t.tx_id = e.tx_id 
+INNER JOIN NFT n
+ON n.tx_id = a.tx_id
 
-INNER JOIN {{ ref('silver___post_token_balances') }} p
-ON e.tx_id = p.tx_id AND e.index = p.index 
+INNER JOIN sales_amount s
+ON s.tx_id = a.tx_id
 
-WHERE t.ct = 3 
-AND e.index = 0
+WHERE index = 0
