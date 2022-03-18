5825e3001e1bb5f59f1773c8f9164dfa5d904a40
jhuhnke <jessica@flipsidecrypto.com>
2022-03-18 10:15:09 -0500

    updates to candy machine

--- models/silver/silver__nfts_candy_machine.sql
+++ models/silver/silver__nfts_candy_machine.sql
@@ -6,83 +6,78 @@
 ) }}
     
 WITH txs AS (
-    SELECT
-      block_timestamp, 
-      block_id, 
-      tx_id,   
+   SELECT
+      e.block_timestamp, 
+      e.block_id, 
+      e.tx_id,
+      t.succeeded AS succeeded,   
       program_id, 
       COALESCE(
-          inner_instruction :instructions[0] :parsed :info :lamports :: INTEGER, -- Move this to the last step 
-          inner_instruction :instructions[0] :parsed :info :amount :: INTEGER
+          i.value :parsed :info :lamports :: INTEGER, 
+          i.value :parsed :info :amount :: INTEGER
       ) AS sales_amount,
-      inner_instruction :instructions[0] :parsed :info :authority :: STRING AS authority, -- Can we pull this from the instructions account array? 
-      inner_instruction :instructions[0] :parsed :info :source :: STRING AS source, 
-      inner_instruction :instructions[0] :parsed :info :destination :: STRING AS destination,
-      ingested_at
-      FROM {{ ref('silver__events') }} 
+      i.index AS inner_index, 
+      i.value :parsed :info :mint :: STRING AS NFT, 
+      i.value :parsed :info :multisigAuthority :: STRING AS wallet, 
+      i.value :parsed :info :authority :: STRING AS authority, 
+      i.value :parsed :info :source :: STRING AS source, 
+      i.value: parsed :info :destination :: STRING AS destination,
+      e.ingested_at
+   FROM {{ ref('silver__events') }} e 
 
-      WHERE program_id = 'cndyAnrLdpjq1Ssp1z8xxDsB8dxe7u4HL5Nxi2K5WXZ' 
-      AND index > 0
+   INNER JOIN {{ ref('silver__transactions') }} t 
+   ON e.tx_id = t.tx_id 
 
-    {% if is_incremental() %}
-      AND ingested_at::date >= getdate() - interval '2 days'
-    {% endif %}
-    
-),      
+   LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
+
+   WHERE program_id = 'cndyAnrLdpjq1Ssp1z8xxDsB8dxe7u4HL5Nxi2K5WXZ' 
+   AND e.index > 0
+   AND (authority IS NOT NULL 
+   OR NFT IS NOT NULL
+   OR i.index = 0) 
+   AND t.succeeded = TRUE
+
+   {% if is_incremental() %}
+    AND e.ingested_at :: DATE >= current_date - 2
+   {% endif %}
+),   
 
 mint_currency AS (
-    SELECT 
-    DISTINCT
-        t.tx_id,  
-        CASE WHEN source = p.account THEN p.mint 
-        ELSE 'So11111111111111111111111111111111111111111' END AS mint_currency, 
-        CASE WHEN source = p.account THEN p.decimal 
-        ELSE 9 END AS decimal 
-    FROM txs t
+SELECT 
+  DISTINCT
+    t.tx_id,  
+    t.authority, 
+    p.mint, 
+    p.decimal    
+FROM txs t
 
-    INNER JOIN {{ ref('silver___post_token_balances') }} p
-    ON t.tx_id = p.tx_id
+INNER JOIN {{ ref('silver___post_token_balances') }} p
+ON t.tx_id = p.tx_id 
 
-  {% if is_incremental() %}
-      WHERE t.ingested_at::date >= getdate() - interval '2 days'
-  {% endif %}
-),     
+WHERE source = p.account 
 
-NFT as (
-    SELECT
-        DISTINCT
-        t.tx_id,  
-        p.mint AS NFT 
-    FROM txs t
-
-    INNER JOIN {{ ref('silver___post_token_balances') }} p
-    ON t.tx_id = p.tx_id 
-    AND source <> p.account  
-
-  {% if is_incremental() %}
-      WHERE t.ingested_at::date >= getdate() - interval '2 days'
-  {% endif %}
-)
+{% if is_incremental() %}
+    AND p.ingested_at :: DATE >= current_date - 2
+{% endif %}
+)       
 
 SELECT 
     block_timestamp, 
     block_id, 
     t.tx_id, 
     program_id,
+    succeeded, 
+    max(wallet) AS purchaser,  
+    max(sales_amount / POW(10, COALESCE(p.decimal, 9))) AS sales_amount,
     COALESCE(
-        source, 
-        authority
-     ) AS purchaser, 
-    sum(sales_amount) / POW(10, p.decimal) AS sales_amount,
-    p.mint_currency, 
-    n.NFT, 
+      p.mint,
+      'So11111111111111111111111111111111111111111'
+    ) AS mint_currency, 
+    max(NFT) AS NFT, 
     ingested_at
 FROM txs t
 
-INNER JOIN NFT n 
-ON t.tx_id = n.tx_id  
-
-INNER JOIN mint_currency p 
+LEFT OUTER JOIN mint_currency p 
 ON t.tx_id = p.tx_id
 
-GROUP BY block_timestamp, block_id, t.tx_id, program_id, purchaser, p.decimal, p.mint_currency, n.NFT, ingested_at
+GROUP BY block_timestamp, block_id, t.tx_id, program_id, succeeded, p.mint, ingested_at
