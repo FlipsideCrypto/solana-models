17eca59472a02f3cc9eb878d9c855e9ad3a47395
jhuhnke <jessica@flipsidecrypto.com>
2022-03-22 12:30:46 -0500

    referenced accounts to pull the mint, removed CTE

--- models/silver/silver__nft_sales_smb.sql
+++ models/silver/silver__nft_sales_smb.sql
@@ -13,6 +13,7 @@ WITH base_table AS (
         t.succeeded, 
         e.program_id, 
         instruction :accounts[0] :: STRING AS acct_1, 
+        instruction :accounts[1] :: STRING AS mint, 
         e.ingested_at
     FROM {{ ref('silver__events') }} e
     
@@ -30,37 +31,18 @@ WITH base_table AS (
 price AS (
     SELECT 
         b.tx_id, 
-        p.amount 
-    FROM base_table b
+        e.instruction :parsed :info :lamports :: NUMBER AS amount 
+    FROM {{ ref('silver__events') }} e
   
-    INNER JOIN {{ ref('silver___post_token_balances') }} p 
-    ON b.tx_id = p.tx_id 
+    INNER JOIN base_table b 
+    ON e.tx_id = b.tx_id 
     
-    WHERE p.index = 0
+    WHERE e.event_type = 'transfer'
 
     {% if is_incremental() %}
-        AND p.ingested_at :: DATE >= current_date - 2
+        AND b.ingested_at :: DATE >= current_date - 2
     {% endif %}
-), 
-
-monke AS (
-    SELECT 
-        DISTINCT
-            b.tx_id, 
-            p.mint
-    FROM base_table b
-  
-    INNER JOIN {{ ref('silver___post_token_balances') }} p 
-    ON b.tx_id = p.tx_id 
-    
-    WHERE (p.index = 2
-    OR p.index = 1) 
-    AND p.mint <> 'So11111111111111111111111111111111111111112'
-
-    {% if is_incremental() %}
-        AND p.ingested_at :: DATE >= current_date - 2
-    {% endif %}
-)
+) 
 
 SELECT 
      b.block_timestamp, 
@@ -68,7 +50,7 @@ SELECT
      b.tx_id, 
      b.succeeded, 
      b.program_id, 
-     m.mint AS mint, 
+     b.mint, 
      b.acct_1 AS purchaser, 
      p.amount / POW(10,9) AS sales_amount, 
      b.ingested_at
@@ -77,7 +59,5 @@ FROM base_table b
 INNER JOIN price p
 ON b.tx_id = p.tx_id
 
-INNER JOIN monke m
-ON b.tx_id = m.tx_id 
-
-WHERE amount <> 0 -- To ignore internal wallet transfers on the marketplace
+WHERE p.amount <> 0 -- To ignore internal wallet transfers on the marketplace
+AND b.mint <> 'So11111111111111111111111111111111111111112'
