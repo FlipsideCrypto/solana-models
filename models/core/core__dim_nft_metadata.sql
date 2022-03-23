5135638fae469f676a4eecacd4f675cc730c1b36
jhuhnke <jessica@flipsidecrypto.com>
2022-03-23 13:05:04 -0500

    adds core dim nft metadata table & updates NFT sales

---
+++ models/core/core__dim_nft_metadata.sql
@@ -0,0 +1,21 @@
+{{ 
+    config(
+      materialized='view'
+    ) 
+}}
+
+SELECT 
+    blockchain,
+    contract_address,
+    contract_name,
+    created_at_timestamp,
+    mint,
+    creator_address,
+    creator_name,
+    image_url,
+    project_name,
+    token_id,
+    token_metadata,
+    token_metadata_uri,
+    token_name
+FROM {{ ref('silver__nft_metadata') }}
