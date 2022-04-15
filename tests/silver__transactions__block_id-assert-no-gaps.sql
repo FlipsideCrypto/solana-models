2320bcd519ce8e19828cf95d3fe8a7074cf708db
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2022-04-15 12:52:01 -0500

    An 933 sol no gaps (#39)
    * blocks no gaps test
    
    * added test for bronze transactions
    
    * renamed tests and added votes to tx test
    
    * adds tx count filter

---
+++ tests/silver__transactions__block_id-assert-no-gaps.sql
@@ -0,0 +1,18 @@
+{{ config(error_if = '>500', warn_if = '>500') }}
+
+SELECT 
+    block_id AS slot
+FROM {{ ref('silver__blocks') }}
+WHERE block_id NOT IN (
+    SELECT 
+        block_id 
+    FROM {{ ref('silver__transactions') }}
+)
+
+AND block_id NOT IN (
+    SELECT 
+        block_id 
+    FROM {{ ref('silver__votes') }}
+)
+
+AND tx_count > 0
