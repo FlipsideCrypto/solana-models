7b342b71e3cde56d3333f3f48937920b38fa2bfd
Desmond Hui <desmond@flipsidecrypto.com>
2022-04-28 16:53:06 -0700

    minor fixes and add start date to full refresh

--- models/silver/silver__proposal_votes_marinade.sql
+++ models/silver/silver__proposal_votes_marinade.sql
@@ -1,6 +1,6 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = "CONCAT_WS('-', tx_id, voter, gauge)",
+    unique_key = "CONCAT_WS('-', tx_id, voter_nft, proposal)",
     incremental_strategy = 'delete+insert',
     cluster_by = ['block_timestamp::DATE'],
 ) }}
@@ -8,11 +8,15 @@
 WITH marinade_vote_txs AS (
 
     SELECT
-        DISTINCT tx_id
+        DISTINCT e.tx_id,
+        succeeded
     FROM
         {{ ref('silver__events') }}
-        e,
-        TABLE(
+        e
+        INNER JOIN {{ ref('silver__transactions') }}
+        t
+        ON e.tx_id = t.tx_id
+        LEFT OUTER JOIN TABLE(
             FLATTEN(
                 input => inner_instruction :instructions,
                 outer => TRUE
@@ -23,30 +27,47 @@ WITH marinade_vote_txs AS (
 
 {% if is_incremental() %}
 AND e.ingested_at :: DATE >= CURRENT_DATE - 2
+AND t.ingested_at :: DATE >= CURRENT_DATE - 2
+{% else %}
+    AND e.ingested_at :: DATE >= '2022-04-01'
+    AND t.ingested_at :: DATE >= '2022-04-01'
 {% endif %}
 INTERSECT
 SELECT
-    DISTINCT tx_id
+    DISTINCT e.tx_id,
+    succeeded
 FROM
     {{ ref('silver__events') }}
-    e,
-    TABLE(
+    e
+    INNER JOIN {{ ref('silver__transactions') }}
+    t
+    ON e.tx_id = t.tx_id
+    LEFT OUTER JOIN TABLE(
         FLATTEN(
             input => inner_instruction :instructions,
             outer => TRUE
         )
     ) ii
 WHERE
-    program_id = 'Govz1VyoyLD5BL6CSCxUJLVLsQHRwjfFj1prNsdNg5Jw' -- ignore votes
+    (
+        program_id = 'Govz1VyoyLD5BL6CSCxUJLVLsQHRwjfFj1prNsdNg5Jw' -- ignore votes
+        OR COALESCE(
+            ii.value :programId :: STRING,
+            ''
+        ) = 'Govz1VyoyLD5BL6CSCxUJLVLsQHRwjfFj1prNsdNg5Jw'
+    )
 
 {% if is_incremental() %}
 AND e.ingested_at :: DATE >= CURRENT_DATE - 2
+AND t.ingested_at :: DATE >= CURRENT_DATE - 2
+{% else %}
+    AND e.ingested_at :: DATE >= '2022-04-01'
+    AND t.ingested_at :: DATE >= '2022-04-01'
 {% endif %}
 ),
 b AS (
     SELECT
         t.tx_id,
-        t.succeeded,
         l.index,
         l.value :: STRING AS log_message,
         CASE
@@ -70,6 +91,9 @@ b AS (
 {% if is_incremental() %}
 WHERE
     t.ingested_at :: DATE >= CURRENT_DATE - 2
+{% else %}
+WHERE
+    t.ingested_at :: DATE >= '2022-04-01'
 {% endif %}
 ),
 C AS (
@@ -89,7 +113,6 @@ C AS (
 tx_logs AS (
     SELECT
         C.tx_id,
-        C.succeeded,
         C.index AS log_index,
         C.log_message,
         CASE
@@ -110,14 +133,14 @@ SELECT
     e.block_timestamp,
     e.block_id,
     e.tx_id,
-    l.succeeded,
-    e.index,
+    m.succeeded,
     e.instruction :accounts [3] :: STRING AS voter,
     e.instruction :accounts [1] :: STRING AS voter_nft,
     e.instruction :accounts [5] :: STRING AS voter_account,
     e.instruction :accounts [6] :: STRING AS proposal
 FROM
-    {{ ref('silver__events') }} e
+    {{ ref('silver__events') }}
+    e
     INNER JOIN marinade_vote_txs m
     ON m.tx_id = e.tx_id
     LEFT OUTER JOIN tx_logs l
@@ -129,4 +152,6 @@ WHERE
 
 {% if is_incremental() %}
 AND e.ingested_at :: DATE >= CURRENT_DATE - 2
+{% else %}
+    AND e.ingested_at :: DATE >= '2022-04-01'
 {% endif %}
