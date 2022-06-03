7cf624d305a386a57ffc901106b8dbfeccbffabf
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2022-06-03 14:46:08 -0500

    An 1030 inner instructions test (#48)
    * program_id test
    
    * solana inner instructions test
    
    * remove group by statement

---
+++ tests/silver__inner_instructions__program_id-assert-not-null.sql
@@ -0,0 +1,9 @@
+{{ config(error_if = '>1000', warn_if '>1000') }}
+
+SELECT 
+    DISTINCT 
+        block_timestamp::date, 
+        block_id as slot 
+FROM {{ ref('silver___inner_instructions') }} 
+WHERE value:instructions[0]:programIdIndex::number IS NOT NULL 
+GROUP BY block_timestamp::date, block_id 
