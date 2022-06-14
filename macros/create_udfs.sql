8f22af7f7a52cfafa29f6262ebc87112260c3cbd
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-06-14 11:53:01 -0700

    use python to get signers from accts (#56)

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,6 +1,9 @@
 {% macro create_udfs() %}
     {% set sql %}
     {{ udf_bulk_get_decoded_instructions_data() }};
+    {{ create_udf_ordered_signers(
+        schema = "silver"
+    ) }}
     {% endset %}
     {% do run_query(sql) %}
 {% endmacro %}
