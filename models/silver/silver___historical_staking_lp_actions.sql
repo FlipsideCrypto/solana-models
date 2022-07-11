c31c8a9c8c15821901848c5541e3bc314626136b
jhuhnke <jessica@flipsidecrypto.com>
2022-07-11 18:26:53 -0500

    parsed out inner instructions staking txs

--- models/silver/silver___historical_staking_lp_actions.sql
+++ models/silver/silver___historical_staking_lp_actions.sql
@@ -5,17 +5,43 @@
     cluster_by = ['block_timestamp::DATE'],
 ) }}
 
-WITH txs AS (
-    SELECT
-        DISTINCT tx_id, 
-        i.value :programId :: STRING AS program_id
+WITH instructs AS (
+    SELECT 
+        tx_id, 
+        index, 
+        i.value :parsed :type :: STRING AS event_type, 
+        i.value :programId :: STRING AS program_id, 
+        i.value AS instruction
     FROM 
         {{ source(
             'solana_external', 
             'txs_api') 
         }}, 
-    LATERAL FLATTEN (input => data :result :transaction :message :instructions) i  
-    WHERE i.value :programId :: STRING = 'Stake11111111111111111111111111111111111111'
+    TABLE(FLATTEN (data :result :transaction :message :instructions)) i 
+
+    WHERE program_id = 'Stake11111111111111111111111111111111111111'
+
+    UNION 
+    SELECT
+        tx_id, 
+        index, 
+        ii.value :parsed :type :: STRING AS event_type, 
+        ii.value :programId :: STRING AS program_id,
+        ii.value AS instruction
+    FROM 
+        {{ source(
+            'solana_external', 
+            'txs_api') 
+        }}, 
+    TABLE(FLATTEN (data :result :meta :innerInstructions[0] :instructions)) ii 
+
+    WHERE program_id = 'Stake11111111111111111111111111111111111111'
+),
+
+txs AS (
+    SELECT
+        DISTINCT tx_id
+    FROM instructs
 ), 
 
 tx_base AS (
@@ -43,22 +69,7 @@ tx_base AS (
 
     INNER JOIN txs i 
     ON i.tx_id = t.tx_id
-), 
-
-instructs AS (
-    SELECT 
-        tx_id, 
-        index, 
-        i.value :parsed :type :: STRING AS event_type, 
-        i.value :programId :: STRING AS program_id
-    FROM 
-        {{ source(
-            'solana_external', 
-            'txs_api') 
-        }} ii, 
-    TABLE(FLATTEN (data :result :transaction :message :instructions)) i  
-
-)
+) 
 
 SELECT 
     block_id, 
@@ -70,14 +81,14 @@ SELECT
     program_id, 
     [] :: ARRAY AS signers, 
     account_keys :: ARRAY AS account_keys, 
-    instruction[0] AS instruction, 
+    i.instruction, 
     inner_instruction :: VARIANT AS inner_instruction, 
     pre_balances :: ARRAY AS pre_balances, 
     post_balances :: ARRAY AS post_balances, 
     pre_token_balances :: ARRAY AS pre_token_balances, 
     post_token_balances :: ARRAY AS post_token_balances, 
     NULL AS _inserted_timestamp
-FROM tx_base b
+FROM instructs i
 
-LEFT OUTER JOIN instructs i
+LEFT OUTER JOIN tx_base b
 ON b.tx_id = i.tx_id
