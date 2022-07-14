af052407bfe60e7d30389b7202be776bcedef8f1
Desmond Hui <desmond@flipsidecrypto.com>
2022-07-14 11:44:40 -0700

    resolve conflicts

--- models/silver/silver___all_unknown_stake_vote_accounts.sql
+++ models/silver/silver___all_unknown_stake_vote_accounts.sql
@@ -2,17 +2,28 @@
     materialized = 'view'
 ) }}
 
+WITH base AS (
+
+    SELECT
+        DISTINCT stake_account
+    FROM
+        {{ ref('core__fact_ez_staking_lp_actions') }}
+    WHERE
+        vote_account IS NULL
+    EXCEPT
+    SELECT
+        DISTINCT stake_account
+    FROM
+        {{ ref('core__fact_ez_staking_lp_actions') }}
+    WHERE
+        vote_account IS NOT NULL
+)
 SELECT
-    DISTINCT stake_account
+    stake_account
 FROM
-    {{ ref('core__ez_staking_lp_actions') }}
-WHERE
-    vote_account IS NULL
-    AND event_type IN (
-        'split_source',
-        'merge_destination',
-        'merge_source'
-    )
+    base
+WHERE 
+    stake_account <> 'FeD1HoB2dyEZnxYpzUnkmQm9jTTY3D7cTf9TFaMGeBii' -- this thing has a ton of txs...it does voting doesnt seem like a stake account
 EXCEPT
 SELECT
     account
