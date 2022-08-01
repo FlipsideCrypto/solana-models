d7c4370aba60c08566948e44afc3247a84f12201
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2022-08-01 14:44:39 -0500

    An 1719 realms (#76)
    * wip
    
    * wip
    
    * fix bad join
    
    * all tests passing
    
    * fixes to indexing tx log
    
    * update voter
    
    * incremental logic change

---
+++ models/silver/silver__proposal_votes_realms.sql
@@ -0,0 +1,166 @@
+{{ config(
+  materialized = 'incremental',
+  unique_key = "CONCAT_WS('-', tx_id, index)",
+  incremental_strategy = 'delete+insert',
+  cluster_by = ['block_timestamp::DATE'],
+) }}
+
+WITH vote_programs AS (
+    SELECT 
+        address
+    FROM {{ source(
+        'crosschain',
+        'address_labels'
+    ) }} 
+    WHERE 
+        blockchain = 'solana'
+        AND project_name = 'realms'
+),  
+vote_txs AS (
+    SELECT
+        block_timestamp, 
+        block_id, 
+        tx_id,
+        succeeded, 
+        e.index, 
+        program_id, 
+        instruction :accounts[0] :: STRING AS realms_id, 
+        instruction :accounts[2] :: STRING AS proposal, 
+        instruction :accounts[5] :: STRING AS voter, 
+        instruction :accounts[6] :: STRING AS vote_account, 
+        _inserted_timestamp
+    FROM 
+        {{ ref('silver__events') }}  e 
+  
+    INNER JOIN vote_programs v
+    ON e.program_id = v.address
+
+    WHERE 
+        instruction :data :: STRING <> 'Q'
+    
+    {% if is_incremental() %}
+    AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp)
+    FROM
+        {{ this }}
+    )
+    {% endif %}
+), 
+b AS (
+    SELECT
+        t.tx_id,
+        t.succeeded,
+        l.index,
+        l.value :: STRING AS log_message,
+        CASE
+            WHEN l.value :: STRING LIKE '%invoke%' THEN 1
+            WHEN l.value :: STRING LIKE '%success' THEN -1
+            ELSE 0
+        END AS cnt,
+        SUM(cnt) over (
+            PARTITION BY t.tx_id
+            ORDER BY
+                l.index rows BETWEEN unbounded preceding
+                AND CURRENT ROW
+        ) AS event_cumsum
+    FROM
+        {{ ref('silver__transactions') }}
+        t
+        INNER JOIN (
+            SELECT
+                DISTINCT tx_id
+            FROM
+                vote_txs 
+        ) v
+        ON t.tx_id = v.tx_id
+        LEFT OUTER JOIN TABLE(FLATTEN(t.log_messages)) l
+
+{% if is_incremental() %}
+WHERE _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp)
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+C AS (
+    SELECT
+        b.*,
+        LAG(
+            event_cumsum,
+            1
+        ) over (
+            PARTITION BY tx_id
+            ORDER BY
+                INDEX
+        ) AS prev_event_cumsum
+    FROM
+        b
+),
+tx_logs AS (
+    SELECT
+        C.tx_id,
+        C.succeeded,
+        C.index AS log_index,
+        C.log_message,
+        conditional_true_event(
+            prev_event_cumsum = 0
+        ) over (
+            PARTITION BY tx_id
+            ORDER BY
+                INDEX
+        ) AS event_index
+    FROM
+        C
+), 
+create_vote_logs AS (
+    SELECT 
+        * 
+    FROM 
+        tx_logs
+    WHERE 
+        log_message LIKE 'Program log: GOVERNANCE-INSTRUCTION: CastVote %'
+)
+
+SELECT 
+    v.block_timestamp, 
+    v.block_id,
+    v.tx_id, 
+    v.succeeded,
+    v.index, 
+    v.program_id, 
+    realms_id, 
+    proposal, 
+    voter, 
+    vote_account, 
+    CASE WHEN (log_message LIKE 'Program log: GOVERNANCE-INSTRUCTION: CastVote { vote: Deny }')
+    OR (log_message LIKE 'Program log: GOVERNANCE-INSTRUCTION: CastVote { vote: No }') THEN 
+        'NO'
+    WHEN (log_message LIKE 'Program log: GOVERNANCE-INSTRUCTION: CastVote { vote: Approve%') 
+    OR (log_message LIKE 'Program log: GOVERNANCE-INSTRUCTION: CastVote { vote: Yes }') THEN 
+        'YES'
+    ELSE 
+        'ABSTAIN'
+    END AS vote_choice,
+    CASE WHEN log_message LIKE 'Program log: GOVERNANCE-INSTRUCTION: CastVote { vote: Approve%' THEN 
+        split_part(split_part(split_part(split_part(log_message :: STRING, '{', 3), '}', 1), ',', 1), ':', 2)
+    ELSE 
+        0
+    END AS vote_rank, 
+    CASE WHEN log_message LIKE 'Program log: GOVERNANCE-INSTRUCTION: CastVote { vote: Approve%' THEN 
+        split_part(split_part(split_part(split_part(log_message:: STRING, '{', 3), '}', 1), ',', 2), ':', 2) :: INTEGER
+    WHEN log_message LIKE 'Program log: GOVERNANCE-INSTRUCTION: CastVote { vote: Yes }' THEN 
+        100
+    ELSE 
+        0
+    END AS vote_weight,
+    _inserted_timestamp
+FROM vote_txs v 
+
+INNER JOIN create_vote_logs l 
+ON l.tx_id = v.tx_id
+AND l.event_index = v.index
+
+    
