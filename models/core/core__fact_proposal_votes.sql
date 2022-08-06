71bcfff000163ada952f97a4cc98422e6f519d24
jhuhnke <jessica@flipsidecrypto.com>
2022-08-06 11:44:58 -0500

    realms to gold

--- models/core/core__fact_proposal_votes.sql
+++ models/core/core__fact_proposal_votes.sql
@@ -11,6 +11,31 @@ SELECT
     voter,
     voter_account,
     voter_nft,
-    proposal
+    proposal, 
+    'NULL' AS realms_id, 
+    'NULL' AS vote_choice, 
+    'NULL' AS vote_rank, 
+    'NULL' AS vote_weight
+
 FROM
     {{ ref('silver__proposal_votes_marinade') }}
+
+UNION ALL 
+
+SELECT 
+    'realms' as program_name, 
+    block_timestamp, 
+    block_id, 
+    tx_id, 
+    succeeded,  
+    voter, 
+    vote_account, 
+    'NULL' AS voter_nft, 
+    proposal,
+    realms_id, 
+    vote_choice, 
+    vote_rank, 
+    vote_weight
+
+FROM
+    {{ ref('silver__proposal_votes_realms') }}
