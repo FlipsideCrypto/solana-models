b2c142b9a3e3d3477c4a8bd876ef7525703ccbfc
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2022-08-08 15:55:15 -0500

    realms to gold (#77)
    * realms to gold
    
    * added platform column

--- models/core/core__fact_proposal_votes.sql
+++ models/core/core__fact_proposal_votes.sql
@@ -3,6 +3,7 @@
 ) }}
 
 SELECT 
+    'tribeca' as governance_platform, 
     'marinade' as program_name,
     block_timestamp,
     block_id,
@@ -11,6 +12,32 @@ SELECT
     voter,
     voter_account,
     voter_nft,
-    proposal
+    proposal, 
+    'NULL' AS realms_id, 
+    'NULL' AS vote_choice, 
+    'NULL' AS vote_rank, 
+    'NULL' AS vote_weight
+
 FROM
     {{ ref('silver__proposal_votes_marinade') }}
+
+UNION ALL 
+
+SELECT 
+    'realms' as governance_platform, 
+    program_id as program_name, 
+    block_timestamp, 
+    block_id, 
+    tx_id, 
+    succeeded,  
+    voter, 
+    vote_account, 
+    'NULL' AS voter_nft, 
+    proposal,
+    realms_id, 
+    vote_choice, 
+    vote_rank, 
+    vote_weight
+
+FROM
+    {{ ref('silver__proposal_votes_realms') }}
