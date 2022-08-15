dfc81c194049007b6c5c4671755fa35421e77a61
Desmond Hui <desmond@flipsidecrypto.com>
2022-08-15 15:06:45 -0700

    add macro to be called on dbt cloud

--- macros/create_tasks.sql
+++
@@ -1,7 +0,0 @@
-{% macro create_tasks() %}
-    {% if target.database == 'SOLANA' %}
-        CREATE SCHEMA IF NOT EXISTS _internal;
-        {{ task_run_sp_create_prod_clone('_internal') }};
-    {% endif %}
-
-{% endmacro %}
