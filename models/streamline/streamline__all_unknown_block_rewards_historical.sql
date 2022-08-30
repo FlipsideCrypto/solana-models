a2251a88e3579f332a98bb4610e6378de9642b2a
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-08-30 14:18:30 -0700

    An 1971/update streamline backfill models (#90)
    * remove unused cte
    
    * update streamline backfill to new block range
    
    * run twice an hour because each iteration loads more data than before

--- models/streamline/streamline__all_unknown_block_rewards_historical.sql
+++ models/streamline/streamline__all_unknown_block_rewards_historical.sql
@@ -5,11 +5,12 @@
 WITH pre_final AS (
 
     SELECT
-        SEQ8() AS block_id
+        SEQ8()+98680445 AS block_id
     FROM
-        TABLE(GENERATOR(rowcount => 1000000000))
+        TABLE(GENERATOR(rowcount => 60000000))
     WHERE
-        block_id <= 98680445
+        block_id > 98680445
+        AND block_id <= 148378013
     EXCEPT
     SELECT
         block_id
