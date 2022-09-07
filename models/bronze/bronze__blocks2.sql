ec1fada8e3423721598887d970e32cc849a8e9d0
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2022-09-07 13:52:44 -0500

    An 1937/blocks txs2 (#94)
    * blocks
    
    * transactions and deduping wip
    
    * silver reorganization
    
    * remove tx count from blocks
    
    * wip
    
    * load blocks backfill incrementally
    
    * wip
    
    * wip
    
    * wip
    
    * remove unused comments
    
    * fix ref to wrong table
    
    * update to tests
    
    * test fixes
    
    * moved yawww bids
    
    * merge main & fix refs
    
    Co-authored-by: Desmond Hui <desmond@flipsidecrypto.com>

---
+++ models/bronze/bronze__blocks2.sql
@@ -0,0 +1,20 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+SELECT 
+    value, 
+    _inserted_date, 
+    metadata, 
+    block_id, 
+    data, 
+    error,
+    TO_TIMESTAMP_NTZ(
+        SUBSTR(SPLIT_PART(metadata$filename, '/', 4), 1, 10) :: NUMBER,
+        0
+    ) as _inserted_timestamp
+FROM 
+    {{ source(
+        'solana_external', 
+        'blocks_api'
+    )}}
---
+++ models/bronze/bronze__transactions2.sql
@@ -0,0 +1,21 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+SELECT 
+    value, 
+    _partition_id, 
+    metadata, 
+    block_id, 
+    tx_id, 
+    data, 
+    error,
+    TO_TIMESTAMP_NTZ(
+        SUBSTR(SPLIT_PART(metadata$filename, '/', 4), 1, 10) :: NUMBER,
+        0
+    ) as _inserted_timestamp
+FROM 
+    {{ source(
+        'solana_external', 
+        'block_txs_api'
+    )}}
