af2ce900af65acd3d2cb42df3cda2788b356c403
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-09-14 07:50:59 -0700

    An 1605/nft mints refactor (#102)
    * refactor nft mints model
    
    * An 1949/bt (#95)
    
    * date limit some program
    
    * block timestamp
    
    * fixes
    
    * fix
    
    * An 1937/blocks txs2 (#94)
    
    * blocks
    
    * transactions and deduping wip
    
    * silver reorganization
    
    * remove tx count from blocks
    
    * wip
    
    * load blocks backfill incrementally
    
    * wip
    
    * wip
    
    * wip
    
    * remove unused comments
    
    * fix ref to wrong table
    
    * update to tests
    
    * test fixes
    
    * moved yawww bids
    
    * merge main & fix refs
    
    Co-authored-by: Desmond Hui <desmond@flipsidecrypto.com>
    
    * description updates (#96)
    
    * fix missing incremental block (#97)
    
    * reduce batch load size (#98)
    
    * Adjust tx2 incremental load size (#99)
    
    * reduce batch load size
    
    * revert change
    
    * revert change
    
    * minor cleanup and add model output comparison test
    
    * move models to nft folder
    
    * add test case
    
    * update/add descriptions and tests
    
    * adjust unique key
    
    * adjust unique key, add columns to output
    
    * add alias for new tx_id name from underlying model
    
    * add index + inner_index to output and unique key
    
    * remove inner index test
    
    * fix unique key
    
    Co-authored-by: Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>

---
+++ macros/tests/compare_model_subset.sql
@@ -0,0 +1,29 @@
+{% test compare_model_subset(model, compare_model, compare_columns, model_condition) %}
+
+{% set compare_cols_csv = compare_columns | join(', ') %}
+
+with a as (
+    select {{compare_cols_csv}} from {{ model }}
+    {{ model_condition }}
+),
+b as (
+    select {{compare_cols_csv}} from {{ compare_model }}
+),
+a_minus_b as (
+    select * from a
+    {{ dbt_utils.except() }}
+    select * from b
+),
+b_minus_a as (
+    select * from b
+    {{ dbt_utils.except() }}
+    select * from a
+),
+unioned as (
+    select 'in_actual_not_in_expected' as which_diff, a_minus_b.* from a_minus_b
+    union all
+    select 'in_expected_not_in_actual' as which_diff, b_minus_a.* from b_minus_a
+)
+select * from unioned
+
+{% endtest %}
