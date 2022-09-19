58d1853c87f2ff178997057d7389eac965c75c83
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-09-19 10:44:51 -0700

    An 2141/internal silver models (#108)
    * wip
    
    * add max backfill date/block, include pyth oracle programs
    
    * inner instructions model

---
+++ models/silver/silver___inner_instructions2.sql
@@ -0,0 +1,49 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "CONCAT_WS('-', block_id, tx_id, mapped_instruction_index)",
+    incremental_strategy = 'delete+insert',
+    cluster_by = ['block_timestamp::DATE','_inserted_timestamp::DATE'],
+) }}
+
+SELECT
+    block_timestamp,
+    block_id,
+    tx_id,
+    e.index,
+    e.value :index :: NUMBER AS mapped_instruction_index,
+    e.value,
+    _inserted_timestamp
+FROM
+    {{ ref('silver__transactions2') }}
+    t,
+    TABLE(FLATTEN(inner_instructions)) AS e
+
+{% if is_incremental() and env_var(
+    'DBT_IS_BATCH_LOAD',
+    "false"
+) == "true" %}
+WHERE
+    t.block_id BETWEEN (
+        SELECT
+            LEAST(COALESCE(MAX(block_id), 105368)+1,151386092)
+        FROM
+            {{ this }}
+        )
+        AND (
+        SELECT
+            LEAST(COALESCE(MAX(block_id), 105368)+4000000,151386092)
+        FROM
+            {{ this }}
+        ) 
+{% elif is_incremental() %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(_inserted_timestamp)
+        FROM
+            {{ this }}
+    )
+{% else %}
+WHERE
+    t.block_id between 105368 and 1000000
+{% endif %}
