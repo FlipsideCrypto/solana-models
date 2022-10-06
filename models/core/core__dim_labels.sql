83aae2dad42c47a3521c47d1c0f7f606f193cec0
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2022-10-05 22:29:44 -0500

    change labels reference to xchain repo (#118)

--- models/core/core__dim_labels.sql
+++ models/core/core__dim_labels.sql
@@ -7,8 +7,8 @@ SELECT
   blockchain, 
   creator, 
   address,
-  l1_label as label_type,
-  l2_label as label_subtype,
+  label_type,
+  label_subtype,
   project_name as label, 
   address_name as address_name
 FROM {{ source(
