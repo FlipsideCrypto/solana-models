bbe7cae7d8511fd05b4ca966ce5c487e8cd5294f
Desmond Hui <desmond@flipsidecrypto.com>
2022-10-11 16:23:42 -0700

    test for missing partition in transactions or votes

---
+++ tests/test_silver__transactions2__missing_partitions.sql
@@ -0,0 +1,24 @@
+with max_part_id_tmp as (
+    select max(_partition_id) as _partition_id
+    from solana.silver.votes2
+    union 
+    select max(_partition_id)
+    from solana.silver.transactions2
+),
+base as (
+  select distinct _partition_id
+  from solana.streamline.complete_block_txs
+  where _partition_id <= (select max(_partition_id) from max_part_id_tmp)
+),
+base_txs as (
+    select distinct _partition_id
+    from {{ ref('silver__transactions2') }}
+    union 
+    select distinct _partition_id
+    from solana.silver.votes2
+)
+select b._partition_id 
+from base b
+left outer join base_txs t on b._partition_id = t._partition_id 
+where t._partition_id is null
+
