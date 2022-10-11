e5bb624f38008c6182a98c5738c230f6fc14c812
Desmond Hui <desmond@flipsidecrypto.com>
2022-10-11 12:34:03 -0700

    gap test for blocks2

---
+++ tests/test_silver__blocks2__prev_block_id-assert-no-gaps.sql
@@ -0,0 +1,27 @@
+WITH tmp AS (
+
+    SELECT
+        block_id,
+        previous_block_id
+    FROM
+        {{ ref('silver__blocks2') }}
+    WHERE 
+        _inserted_date < current_date 
+    AND 
+        block_id >= 130000000
+),
+missing AS (
+    SELECT
+        t1.previous_block_id AS missing_block_id
+    FROM
+        tmp t1
+        LEFT OUTER JOIN tmp t2
+        ON t1.previous_block_id = t2.block_id
+    WHERE
+        t2.block_id IS NULL
+        AND t1.previous_block_id IS NOT NULL
+)
+SELECT
+    *
+FROM
+    missing
