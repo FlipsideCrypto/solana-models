aa4ca81be0a811548a311dd26caab89aee6e6659
jhuhnke <jessica@flipsidecrypto.com>
2022-10-12 09:02:01 -0500

    changes to incremental and formatting

--- models/silver/silver__daily_signers.sql
+++ models/silver/silver__daily_signers.sql
@@ -1,113 +1,161 @@
 {{ config(
-  materialized = 'incremental',
-  unique_key = "CONCAT_WS('-', signer, b_date)",
-  incremental_strategy = 'delete+insert',
-  cluster_by = 'signer'
+    materialized = 'incremental',
+    unique_key = "CONCAT_WS('-', signer, b_date)",
+    incremental_strategy = 'delete+insert',
+    cluster_by = 'signer'
 ) }}
 
-WITH 
+WITH
+
 {% if is_incremental() and env_var(
-        'DBT_IS_BATCH_LOAD',
-        "false"
-    ) == "false" %}
+    'DBT_IS_BATCH_LOAD',
+    "false"
+) == "false" %}
 dates_changed AS (
+
     SELECT
-        DISTINCT block_timestamp :: date AS block_timestamp_date
+        DISTINCT block_timestamp :: DATE AS block_timestamp_date
     FROM
         {{ ref('silver__transactions2') }}
-    WHERE _inserted_timestamp >= (
-        SELECT
-            MAX(_inserted_timestamp)
-        FROM 
-            {{ this }}
-    )
+    WHERE
+        _inserted_timestamp >= (
+            SELECT
+                MAX(_inserted_timestamp)
+            FROM
+                {{ this }}
+        )
 ),
 {% endif %}
+
 b AS (
-    SELECT 
-        s.value::string AS signer, 
-        block_timestamp::date AS b_date,
-        first_value(tx_id) over (partition by signer, b_date order by block_timestamp) AS first_tx,
-        last_value(tx_id) over (partition by signer, b_date order by block_timestamp) AS last_tx,
-        *
-    FROM 
-        {{ ref('silver__transactions2') }} t,
-    table(flatten(signers)) s
-    {% if is_incremental() and env_var(
-        'DBT_IS_BATCH_LOAD',
-        "false"
-    ) == "true" %}
-    WHERE
-        _inserted_timestamp :: date BETWEEN (
-            SELECT
-                LEAST(COALESCE(MAX(_inserted_timestamp :: date), '2022-08-12')+1, CURRENT_DATE)
-            FROM
-                {{ this }}
-            )
-            AND (
-            SELECT
-                LEAST(COALESCE(MAX(_inserted_timestamp :: date), '2022-08-12')+10, CURRENT_DATE)
-            FROM
-                {{ this }}
-        ) 
-    {% elif is_incremental() %}
-        WHERE b_date IN (
-            SELECT
-                block_timestamp_date
-            FROM
-                dates_changed
-        )
-    {% else %}
-    WHERE 
-        _inserted_timestamp :: date = '2022-08-12'
-    {% endif %}
-),
-c AS (
     SELECT
-        tx_id, 
-        program_id, 
-        index,
-        _inserted_timestamp
-    FROM 
-        {{ ref('silver__events2') }} e
-    {% if is_incremental() and env_var(
-        'DBT_IS_BATCH_LOAD',
-        "false"
-    ) == "true" %}
-    WHERE
-        e.block_id BETWEEN (
-            SELECT
-                LEAST(COALESCE(MAX(_inserted_timestamp :: date), '2022-08-12')+1, CURRENT_DATE)
-            FROM
-                {{ this }}
+        s.value :: STRING AS signer,
+        block_timestamp :: DATE AS b_date,
+        FIRST_VALUE(tx_id) over (
+            PARTITION BY signer,
+            b_date
+            ORDER BY
+                block_timestamp
+        ) AS first_tx,
+        LAST_VALUE(tx_id) over (
+            PARTITION BY signer,
+            b_date
+            ORDER BY
+                block_timestamp
+        ) AS last_tx,*
+    FROM
+        {{ ref('silver__transactions2') }}
+        t,
+        TABLE(FLATTEN(signers)) s
+
+{% if is_incremental() and env_var(
+    'DBT_IS_BATCH_LOAD',
+    "false"
+) == "true" %}
+WHERE
+    _inserted_timestamp :: DATE BETWEEN (
+        SELECT
+            LEAST(
+                DATEADD(
+                    'day',
+                    1,
+                    COALESCE(MAX(_inserted_timestamp :: DATE), '2022-08-12')
+                ),
+                CURRENT_DATE
             )
-            AND (
-            SELECT
-                LEAST(COALESCE(MAX(_inserted_timestamp :: date), '2022-08-12')+10, CURRENT_DATE)
-            FROM
-                {{ this }}
-        ) 
-    {% elif is_incremental() %}
-        WHERE e.block_timestamp::date IN (
-            SELECT
-                block_timestamp_date
-            FROM
-                dates_changed
-        )
-    {% else %}
-    WHERE 
-        _inserted_timestamp :: date = '2022-08-12'
-    {% endif %}
+        FROM
+            {{ this }}
+    )
+    AND (
+        SELECT
+            LEAST(
+                DATEADD(
+                    'day',
+                    1,
+                    COALESCE(MAX(_inserted_timestamp :: DATE), '2022-08-12')
+                ),
+                CURRENT_DATE
+            )
+        FROM
+            {{ this }}
+    ) {% elif is_incremental() %}
+WHERE
+    b_date IN (
+        SELECT
+            block_timestamp_date
+        FROM
+            dates_changed
+    )
+{% else %}
+WHERE
+    _inserted_timestamp :: DATE = '2022-08-12'
+{% endif %}
+),
+C AS (
+    SELECT
+        tx_id,
+        program_id,
+        INDEX,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__events2') }}
+        e
+
+{% if is_incremental() and env_var(
+    'DBT_IS_BATCH_LOAD',
+    "false"
+) == "true" %}
+WHERE
+    _inserted_timestamp :: DATE BETWEEN (
+        SELECT
+            LEAST(
+                DATEADD(
+                    'day',
+                    1,
+                    COALESCE(MAX(_inserted_timestamp :: DATE), '2022-08-12')
+                ),
+                CURRENT_DATE
+            )
+        FROM
+            {{ this }}
+    )
+    AND (
+        SELECT
+            LEAST(
+                DATEADD(
+                    'day',
+                    10,
+                    COALESCE(MAX(_inserted_timestamp :: DATE), '2022-08-12')
+                ),
+                CURRENT_DATE
+            )
+        FROM
+            {{ this }}
+    ) {% elif is_incremental() %}
+WHERE
+    e.block_timestamp :: DATE IN (
+        SELECT
+            block_timestamp_date
+        FROM
+            dates_changed
+    )
+{% else %}
+WHERE
+    _inserted_timestamp :: DATE = '2022-08-12'
+{% endif %}
 ),
 base_programs AS (
     SELECT
-        tx_id, 
-        array_agg(program_id) within group (order by index) AS program_ids,
-        program_ids[0]::string AS first_program_id,
-        program_ids[array_size(program_ids)-1]::string AS last_program_id
-    FROM 
-        c
-    GROUP BY 
+        tx_id,
+        ARRAY_AGG(program_id) within GROUP (
+            ORDER BY
+                INDEX
+        ) AS program_ids,
+        program_ids [0] :: STRING AS first_program_id,
+        program_ids [array_size(program_ids)-1] :: STRING AS last_program_id
+    FROM
+        C
+    GROUP BY
         tx_id
 ),
 first_last_programs AS (
@@ -115,74 +163,92 @@ first_last_programs AS (
         b.signer,
         b.b_date,
         b.tx_id,
-        first_value(first_program_id) over (partition by signer, b_date order by block_timestamp) AS first_program_id,
-        last_value(last_program_id) over (partition by signer, b_date order by block_timestamp) AS last_program_id, 
-        last_value(b._inserted_timestamp) over (partition by signer, b_date order by block_timestamp) AS _inserted_timestamp
-    FROM 
+        FIRST_VALUE(first_program_id) over (
+            PARTITION BY signer,
+            b_date
+            ORDER BY
+                block_timestamp
+        ) AS first_program_id,
+        LAST_VALUE(last_program_id) over (
+            PARTITION BY signer,
+            b_date
+            ORDER BY
+                block_timestamp
+        ) AS last_program_id,
+        LAST_VALUE(
+            b._inserted_timestamp
+        ) over (
+            PARTITION BY signer,
+            b_date
+            ORDER BY
+                block_timestamp
+        ) AS _inserted_timestamp
+    FROM
         b
-    LEFT OUTER JOIN base_programs p 
-    ON p.tx_id = b.tx_id
+        LEFT OUTER JOIN base_programs p
+        ON p.tx_id = b.tx_id
 ),
 final_programs AS (
-    SELECT 
-        b.signer, 
+    SELECT
+        b.signer,
         b.b_date,
-        b.first_program_id, 
-        b.last_program_id, 
-        array_union_agg(p.program_ids) as unique_program_ids, 
+        b.first_program_id,
+        b.last_program_id,
+        array_union_agg(
+            p.program_ids
+        ) AS unique_program_ids,
         b._inserted_timestamp
-    FROM 
+    FROM
         first_last_programs b
-    LEFT OUTER JOIN base_programs p 
-    ON p.tx_id = b.tx_id
-    GROUP BY 
-        b.signer, 
-        b.b_date, 
-        b.first_program_id, 
-        b.last_program_id, 
+        LEFT OUTER JOIN base_programs p
+        ON p.tx_id = b.tx_id
+    GROUP BY
+        b.signer,
+        b.b_date,
+        b.first_program_id,
+        b.last_program_id,
         b._inserted_timestamp
 ),
 final_fees AS (
-    SELECT 
+    SELECT
         signer,
         b_date,
-        sum(fee) AS total_fees
-    FROM 
+        SUM(fee) AS total_fees
+    FROM
         b
-    WHERE 
-        index = 0
-    GROUP BY 
-        signer, 
+    WHERE
+        INDEX = 0
+    GROUP BY
+        signer,
         b_date
 ),
 final_num_txs AS (
-    SELECT 
+    SELECT
         signer,
         b_date,
         first_tx,
         last_tx,
-        count(*) AS num_txs
-    FROM 
+        COUNT(*) AS num_txs
+    FROM
         b
-    GROUP BY 
-        signer, 
-        b_date, 
-        first_tx, 
+    GROUP BY
+        signer,
+        b_date,
+        first_tx,
         last_tx
 )
-SELECT 
-    s.*, 
-    f.total_fees, 
-    p.first_program_id, 
-    p.last_program_id, 
-    p.unique_program_ids, 
+SELECT
+    s.*,
+    f.total_fees,
+    p.first_program_id,
+    p.last_program_id,
+    p.unique_program_ids,
     p._inserted_timestamp
-FROM 
+FROM
     final_num_txs s
-LEFT OUTER JOIN final_fees f 
-ON f.signer = s.signer
-AND f.b_date = s.b_date
-
-LEFT OUTER JOIN final_programs p 
-ON p.signer = s.signer
-AND p.b_date = s.b_date
+    LEFT OUTER JOIN final_fees f
+    ON f.signer = s.signer
+    AND f.b_date = s.b_date
+    LEFT OUTER JOIN final_programs p
+    ON p.signer = s.signer
+    AND p.b_date = s.b_date
