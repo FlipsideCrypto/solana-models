b34a6c7c9f8591537bfd37f9f70dd1127b1bf38b
jkallogjeri <41237295+jkallogjeri@users.noreply.github.com>
2022-10-13 16:55:56 -0400

    incremental blob + program date

--- models/silver/nfts/silver__nft_sales_hadeswap.sql
+++ models/silver/nfts/silver__nft_sales_hadeswap.sql
@@ -21,6 +21,7 @@ FROM {{ ref('silver__events') }} e
 LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
 
 WHERE program_id = 'hadeK9DLv9eA7ya5KCTqSvSvRZeJC3JgD5a9Y3CNbvu'
+  AND block_timestamp >= '2022-09-22'
   AND value:parsed:type ::STRING = 'transfer'
   AND value:parsed:info:destination ::STRING = INSTRUCTION:accounts[3] ::STRING
   AND succeeded = 'TRUE'
@@ -28,8 +29,11 @@ WHERE program_id = 'hadeK9DLv9eA7ya5KCTqSvSvRZeJC3JgD5a9Y3CNbvu'
   AND INSTRUCTION:accounts[4] ::STRING != '11111111111111111111111111111111'
   AND INSTRUCTION:accounts[5] ::STRING != '11111111111111111111111111111111'
 
-  {% if is_incremental() %}
-    AND _inserted_timestamp >= (SELECT MAX(_inserted_timestamp) FROM {{ this }})
+{% IF is_incremental() AND env_var('DBT_IS_BATCH_LOAD',"false") == "true" %}
+  AND block_timestamp :: DATE BETWEEN (SELECT LEAST(DATEADD('day',1,COALESCE(MAX(block_timestamp) :: DATE, '2022-09-22')),'2022-10-05') FROM {{ this }})
+  AND (SELECT LEAST(DATEADD('day',30,COALESCE(MAX(block_timestamp) :: DATE, '2022-09-22')),'2022-10-05') FROM {{ this }})
+{% elif is_incremental() %}
+  AND _inserted_timestamp >= (SELECT MAX(_inserted_timestamp) FROM {{ this }})
 {% else %}
-    AND block_timestamp :: date >= '2021-08-01'
+  AND block_timestamp :: DATE BETWEEN '2022-09-22' AND '2022-10-22'
 {% endif %}
