edcd37db957ee0d4d1a225714cbbf8e2fc12d982
Desmond Hui <desmond@flipsidecrypto.com>
2022-10-20 12:00:07 -0700

    add var for controlling when to run tags, remove col lvl support

--- macros/tags/snowflake_tagging.sql
+++ macros/tags/snowflake_tagging.sql
@@ -1,68 +1,62 @@
 {% macro apply_meta_as_tags(results) %}
-  {{ log('apply_meta_as_tags', info=True) }}
-  {{ log(results, info=True) }}
-  {% if execute %}
-    
-    {%- set tags_by_schema = {} -%}
-    {% for res in results -%}
-        {% if res.node.meta.database_tags %}
+    {% if var("UPDATE_SNOWFLAKE_TAGS") %}
+        {{ log('apply_meta_as_tags', info=True) }}
+        {{ log(results, info=True) }}
+        {% if execute %}
             
-            {%- set model_database = res.node.database -%}
-            {%- set model_schema = res.node.schema -%}
-            {%- set model_schema_full = model_database+'.'+model_schema -%}
-            {%- set model_alias = res.node.alias -%}
+            {%- set tags_by_schema = {} -%}
+            {% for res in results -%}
+                {% if res.node.meta.database_tags %}
+                    
+                    {%- set model_database = res.node.database -%}
+                    {%- set model_schema = res.node.schema -%}
+                    {%- set model_schema_full = model_database+'.'+model_schema -%}
+                    {%- set model_alias = res.node.alias -%}
 
-            {% if model_schema_full not in tags_by_schema.keys() %}
-                {{ log('need to fetch tags for schema '+model_schema_full, info=True) }}
-                {%- call statement('main', fetch_result=True) -%}
-                    show tags in {{model_database}}.{{model_schema}}
-                {%- endcall -%}
-                {%- set _ = tags_by_schema.update({model_schema_full: load_result('main')['table'].columns.get('name').values()|list}) -%}
-                {{ log('Added tags to cache', info=True) }}
-            {% else %}
-                {{ log('already have tag info for schema', info=True) }}
-            {% endif %}
+                    {% if model_schema_full not in tags_by_schema.keys() %}
+                        {{ log('need to fetch tags for schema '+model_schema_full, info=True) }}
+                        {%- call statement('main', fetch_result=True) -%}
+                            show tags in {{model_database}}.{{model_schema}}
+                        {%- endcall -%}
+                        {%- set _ = tags_by_schema.update({model_schema_full: load_result('main')['table'].columns.get('name').values()|list}) -%}
+                        {{ log('Added tags to cache', info=True) }}
+                    {% else %}
+                        {{ log('already have tag info for schema', info=True) }}
+                    {% endif %}
 
-            {%- set current_tags_in_schema = tags_by_schema[model_schema_full] -%}
-            {{ log('current_tags_in_schema:', info=True) }}
-            {{ log(current_tags_in_schema, info=True) }}
-            {{ log("========== Processing tags for "+model_schema_full+"."+model_alias+" ==========", info=True) }}
+                    {%- set current_tags_in_schema = tags_by_schema[model_schema_full] -%}
+                    {{ log('current_tags_in_schema:', info=True) }}
+                    {{ log(current_tags_in_schema, info=True) }}
+                    {{ log("========== Processing tags for "+model_schema_full+"."+model_alias+" ==========", info=True) }}
 
-            {% set line -%}
-                node: {{ res.node.unique_id }}; status: {{ res.status }} (message: {{ res.message }})
-                node full: {{ res.node}}
-                meta: {{ res.node.meta}}
-                materialized: {{ res.node.config.materialized }}
-            {%- endset %}
-            {{ log(line, info=True) }}
+                    {% set line -%}
+                        node: {{ res.node.unique_id }}; status: {{ res.status }} (message: {{ res.message }})
+                        node full: {{ res.node}}
+                        meta: {{ res.node.meta}}
+                        materialized: {{ res.node.config.materialized }}
+                    {%- endset %}
+                    {{ log(line, info=True) }}
 
-            {%- call statement('main', fetch_result=True) -%}
-                select LEVEL,UPPER(TAG_NAME) as TAG_NAME,TAG_VALUE from table(information_schema.tag_references_all_columns('{{model_schema}}.{{model_alias}}', 'table'))
-            {%- endcall -%}
-            {%- set existing_tags_for_table = load_result('main')['data'] -%}
-            {{ log('Existing tags for table:', info=True) }}
-            {{ log(existing_tags_for_table, info=True) }}
+                    {%- call statement('main', fetch_result=True) -%}
+                        select LEVEL,UPPER(TAG_NAME) as TAG_NAME,TAG_VALUE from table(information_schema.tag_references_all_columns('{{model_schema}}.{{model_alias}}', 'table'))
+                    {%- endcall -%}
+                    {%- set existing_tags_for_table = load_result('main')['data'] -%}
+                    {{ log('Existing tags for table:', info=True) }}
+                    {{ log(existing_tags_for_table, info=True) }}
 
-            {{ log('--', info=True) }}
-            {% for table_tag in res.node.meta.database_tags.table %}
+                    {{ log('--', info=True) }}
+                    {% for table_tag in res.node.meta.database_tags.table %}
 
-                {{ create_tag_if_missing(current_tags_in_schema,table_tag|upper) }}
-                {% set desired_tag_value = res.node.meta.database_tags.table[table_tag] %}
+                        {{ create_tag_if_missing(current_tags_in_schema,table_tag|upper) }}
+                        {% set desired_tag_value = res.node.meta.database_tags.table[table_tag] %}
 
-                {{set_table_tag_value_if_different(model_schema,model_alias,table_tag,desired_tag_value,existing_tags_for_table)}}
+                        {{set_table_tag_value_if_different(model_schema,model_alias,table_tag,desired_tag_value,existing_tags_for_table)}}
+                    {% endfor %}
+                    {{ log("========== Finished processing tags for "+model_alias+" ==========", info=True) }}
+                {% endif %}
             {% endfor %}
-            {% for column in res.node.meta.database_tags.column %}
-                {% for column_tag in res.node.meta.database_tags.column[column] %}
-                    {{log(column_tag,info=True)}}
-                    {{create_tag_if_missing(current_tags_in_schema,column_tag|upper)}}
-                    {% set desired_tag_value = res.node.meta.database_tags.column[column][column_tag] %}
-                    {{set_column_tag_value_if_different(model_alias,column,column_tag,desired_tag_value,existing_tags_for_table)}}
-                {% endfor %}
-            {% endfor %}
-            {{ log("========== Finished processing tags for "+model_alias+" ==========", info=True) }}
         {% endif %}
-    {% endfor %}
-  {% endif %}
+    {% endif %}
 {% endmacro %}
 
 
