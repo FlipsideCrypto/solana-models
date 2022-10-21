970b8a3f350d4f73e08f4778eb7679fd8da6b303
Desmond Hui <desmond@flipsidecrypto.com>
2022-10-21 15:07:23 -0700

    wip

--- models/silver/silver___prices.sql
+++ models/silver/silver___prices.sql
@@ -1,59 +1,68 @@
 {{ config(
-    materialized = 'view',
+    materialized = 'incremental',
+    unique_key = "CONCAT_WS('-', date_hour, id)",
+    incremental_strategy = 'merge',
+    cluster_by = ['date_hour::DATE'],
 ) }}
-
-SELECT
-    recorded_at,
-    symbol,
-    price,
-    total_supply,
-    volume_24h,
-    provider
-FROM
-    {{ source(
-        'shared',
-        'prices_v2'
-    ) }}
-WHERE
-    (
+with date_hours as (
+    select date_hour
+    from crosschain_dev.core.dim_date_hours
+    where date_hour >= '2020-04-10'
+),
+asset_metadata as (
+    select 
+        id, 
+        symbol
+    from crosschain.silver.asset_metadata_coin_market_cap
+    where _inserted_timestamp = (select max(_inserted_timestamp) from crosschain.silver.asset_metadata_coin_market_cap)
+    and id in (5426,8526,11461,9549,12297,9015,7978,12236,11171,13524,6187,3408,825,7129,4195,11181)
+    group by 1,2
+),
+base_date_hours_symbols as (
+    select 
+        date_hour,
+        symbol
+    from date_hours
+    cross join (select symbol from asset_metadata)
+),
+base_legacy_prices as (
+    SELECT
+        date_trunc('hour',recorded_at) as recorded_hour,
+        symbol,
+        price as close
+    FROM
+        flipside_prod_db.silver.prices_v2
+    WHERE
         provider = 'coinmarketcap'
-        AND (
-            asset_id = '5426'
-            OR asset_id = '8526'
-            OR asset_id = '11461'
-            OR asset_id = '9549'
-            OR asset_id = '12297'
-            OR asset_id = '9015'
-            OR asset_id = '7978'
-            OR asset_id = '12236'
-            OR asset_id = '11171'
-            OR asset_id = '13524'
-            OR asset_id = '6187'
-            OR asset_id = '3408'
-            OR asset_id = '825'
-            OR asset_id = '7129'
-            OR asset_id = '4195'
-            OR asset_id = '11181'
-        )
-    )
-    OR (
-        provider = 'coingecko'
-        AND (
-            asset_id = 'solana'
-            OR asset_id = 'cope'
-            OR asset_id = 'bonfida'
-            OR asset_id = 'jet'
-            OR asset_id = 'mercurial'
-            OR asset_id = 'mango-markets'
-            OR asset_id = 'msol'
-            OR asset_id = 'raydium'
-            OR asset_id = 'saber'
-            OR asset_id = 'solend'
-            OR asset_id = 'serum'
-            OR asset_id = 'lido-staked-sol'
-            OR asset_id = 'ftx-token'
-            OR asset_id = 'usd-coin'
-            OR asset_id = 'tether'
-            OR asset_id = 'terrausd'
-        )
-    )
+        AND asset_id in ('5426','8526','11461','9549','12297','9015','7978','12236','11171','13524','6187','3408','825','7129','4195','11181')
+        AND minute(recorded_at) = 59
+        AND recorded_at::date < '2022-07-20'
+),
+base_prices as (
+    select recorded_hour, m.symbol, p.close
+    from crosschain.silver.hourly_prices_coin_market_cap p 
+    left outer join asset_metadata m on m.id = p.id
+    where p.id in (5426,8526,11461,9549,12297,9015,7978,12236,11171,13524,6187,3408,825,7129,4195,11181)
+),
+prices as (
+    select *
+    from base_legacy_prices
+    union 
+    select *
+    from base_prices
+),
+imputed_prices as (
+    select
+        d.*,
+        p.close,
+        last_value(p.close ignore nulls) over (partition by d.symbol order by d.date_hour rows unbounded preceding) as imputed_close
+    from base_date_hours_symbols d 
+    left outer join prices p on p.recorded_hour = d.date_hour and p.symbol = d.symbol
+)
+select 
+    p.date_hour,
+    p.symbol,
+    coalesce(p.close,p.imputed_close) as close2,
+    case when p.close is null then true else false end as imputed
+from imputed_prices p
+where close2 is not null
