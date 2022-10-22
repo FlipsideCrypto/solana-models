ff2764da1ecdbdd30f54abc412d65f09871c0208
jkallogjeri <41237295+jkallogjeri@users.noreply.github.com>
2022-10-21 23:40:43 -0400

    fixed hadeswap nft sales

--- models/silver/nfts/silver__nft_sales_hadeswap.sql
+++ models/silver/nfts/silver__nft_sales_hadeswap.sql
@@ -5,41 +5,56 @@
   cluster_by = ['block_timestamp::DATE'],
 ) }}
 
-SELECT
-  block_timestamp,
-  block_id,
-  tx_id,
-  e.index, 
-  succeeded,
-  program_id,
-  INSTRUCTION :accounts [4] :: STRING AS mint,
-  INSTRUCTION :accounts [5] :: STRING AS purchaser,
-  INSTRUCTION :accounts [3] :: STRING AS seller,
-  i.value :parsed :info :lamports / POW(
-    10,
-    9
-  ) AS sales_amount,
-  _inserted_timestamp
-FROM
-  {{ ref('silver__events') }}
-  e
-  LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
-WHERE
-  program_id = 'hadeK9DLv9eA7ya5KCTqSvSvRZeJC3JgD5a9Y3CNbvu'
+WITH mint as (
+
+SELECT 
+  e.tx_id, 
+  e.index,
+  i.value:parsed:info:mint ::STRING as mint
+FROM {{ ref('silver__events') }} e
+
+LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
+
+WHERE program_id = 'hadeK9DLv9eA7ya5KCTqSvSvRZeJC3JgD5a9Y3CNbvu'
+  AND value:parsed:type ::STRING = 'create'
+  AND SUCCEEDED = 'TRUE'
   AND block_timestamp >= '2022-09-22'
-  AND VALUE :parsed :type :: STRING = 'transfer'
-  AND VALUE :parsed :info :destination :: STRING = INSTRUCTION :accounts [3] :: STRING
-  AND succeeded = 'TRUE'
-  AND INSTRUCTION :accounts [3] :: STRING != '11111111111111111111111111111111'
-  AND INSTRUCTION :accounts [4] :: STRING != '11111111111111111111111111111111'
-  AND INSTRUCTION :accounts [5] :: STRING != '11111111111111111111111111111111'
 
 {% if is_incremental() %}
-AND
-  block_timestamp >= (
-    SELECT
-      MAX(block_timestamp)
-    FROM
-      {{ this }}
-  )
-{% endif %}
+  AND block_timestamp >= (SELECT MAX(block_timestamp) FROM {{ this }})
+{% endif %}  
+
+)
+
+SELECT 
+  block_timestamp,
+  block_id,
+  e.tx_id,
+  e.index,
+  succeeded,
+  program_id,
+  m.mint, 
+  i.value:parsed:info:source ::STRING as purchaser, 
+  i.value:parsed:info:destination ::STRING as seller, 
+  i.value:parsed:info:lamports / POW(10,9) AS sales_amount,
+  _inserted_timestamp
+FROM {{ ref('silver__events') }} e
+
+LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
+
+LEFT OUTER JOIN mint m
+  ON e.tx_id = m.tx_id
+  AND e.index = m.index
+
+WHERE program_id = 'hadeK9DLv9eA7ya5KCTqSvSvRZeJC3JgD5a9Y3CNbvu'
+  AND block_timestamp >= '2022-09-22'
+  AND value:parsed:type ::STRING = 'transfer'
+  AND value:parsed:info:destination ::STRING = INSTRUCTION:accounts[3] ::STRING
+  AND succeeded = 'TRUE'
+  AND INSTRUCTION:accounts[3] ::STRING != '11111111111111111111111111111111'
+  AND INSTRUCTION:accounts[4] ::STRING != '11111111111111111111111111111111'
+  AND INSTRUCTION:accounts[5] ::STRING != '11111111111111111111111111111111'
+  
+{% if is_incremental() %}
+  AND block_timestamp >= (SELECT MAX(block_timestamp) FROM {{ this }})
+{% endif %}  
