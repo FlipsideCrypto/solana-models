59f73cc1b60f33b569fb07d1fd4bee6ebb979c9b
Desmond Hui <desmond@flipsidecrypto.com>
2022-11-02 16:34:19 -0700

    eversol actions and tests

--- models/silver/staking/silver__stake_pool_actions_eversol.sql
+++ models/silver/staking/silver__stake_pool_actions_eversol.sql
@@ -137,6 +137,20 @@ withdraw_events AS (
         AND instruction :accounts [10] :: STRING = 'Stake11111111111111111111111111111111111111'
         AND instruction :accounts [11] :: STRING = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
 ),
+withdraw_dao_events AS (
+    SELECT
+        *
+    FROM
+        base_stake_pool_events
+    WHERE
+        ARRAY_SIZE(
+            instruction :accounts
+        ) = 16
+        AND instruction :accounts [9] :: STRING = 'SysvarC1ock11111111111111111111111111111111'
+        AND instruction :accounts [10] :: STRING = 'SysvarStakeHistory1111111111111111111111111'
+        AND instruction :accounts [11] :: STRING = 'Stake11111111111111111111111111111111111111'
+        AND instruction :accounts [12] :: STRING = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+),
 withdraw_stake_events AS (
     SELECT
         *
@@ -149,142 +163,215 @@ withdraw_stake_events AS (
         AND instruction :accounts [10] :: STRING = 'SysvarC1ock11111111111111111111111111111111'
         AND instruction :accounts [11] :: STRING = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
         AND instruction :accounts [12] :: STRING = 'Stake11111111111111111111111111111111111111'
+),
+withdraw_dao_stake_events AS (
+    SELECT
+        *
+    FROM
+        base_stake_pool_events
+    WHERE
+        ARRAY_SIZE(
+        instruction :accounts
+        ) = 17
+        AND instruction :accounts [10] :: STRING = 'SysvarC1ock11111111111111111111111111111111'
+        AND instruction :accounts [11] :: STRING = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+        AND instruction :accounts [12] :: STRING = 'Stake11111111111111111111111111111111111111'
+),
+deposit_dao_stake_merge AS (
+    SELECT
+        e.tx_id,
+        e.block_id,
+        e.block_timestamp,
+        e.index,
+        e.succeeded,
+        'deposit_dao_stake' AS action,
+        e.instruction :accounts [0] :: STRING AS stake_pool,
+        e.instruction :accounts [2] :: STRING AS stake_pool_withdraw_authority,
+        NULL as stake_pool_deposit_authority,
+        e.instruction :accounts [16] :: STRING AS address,  -- use signers instead of instruction account because of "passthrough" wallets
+        e.instruction :accounts [5] :: STRING AS reserve_stake_address,
+        i.value :parsed :info :destination :: STRING AS merge_destination,
+        silver.udf_get_account_balances_index(
+            merge_destination,
+            b.account_keys
+        ) AS merge_destination_balances_index,
+        silver.udf_get_account_balances_index(
+            reserve_stake_address,
+            b.account_keys
+        ) AS reserve_stake_balances_index,
+        b.post_balances [merge_destination_balances_index] - b.pre_balances [merge_destination_balances_index] + b.post_balances [reserve_stake_balances_index] - b.pre_balances [reserve_stake_balances_index] AS amount,
+        e._inserted_timestamp
+    FROM
+        deposit_dao_stake_events e
+        LEFT OUTER JOIN base_balances b
+        ON b.tx_id = e.tx_id
+        LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
+    WHERE
+        i.value :parsed :type = 'merge'
+        AND i.value :programId = 'Stake11111111111111111111111111111111111111'
 )
-select 'deposit' as action, *
-from deposit_events 
+select
+    e.tx_id,
+    e.block_id,
+    e.block_timestamp,
+    e.index,
+    e.succeeded,
+    'deposit' AS action,
+    e.instruction :accounts [0] :: STRING AS stake_pool,
+    e.instruction :accounts [1] :: STRING AS stake_pool_withdraw_authority,
+    NULL as stake_pool_deposit_authority,
+    b.signers[0] :: STRING AS address,  -- use signers instead of instruction account because of "passthrough" wallets
+    e.instruction :accounts [2] :: STRING AS reserve_stake_address,
+    i.value :parsed :info :lamports AS amount,
+    e._inserted_timestamp,
+    concat_ws('-',e.tx_id,e.index) as _unique_key
+from deposit_events e
+LEFT OUTER JOIN base_balances b
+        ON b.tx_id = e.tx_id
+LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
+WHERE
+    i.value :parsed :info :lamports IS NOT NULL
 union 
-select 'deposit_dao' as action, *
-from deposit_dao_events 
+select 
+    e.tx_id,
+    e.block_id,
+    e.block_timestamp,
+    e.index,
+    e.succeeded,
+    'deposit_dao' AS action,
+    e.instruction :accounts [0] :: STRING AS stake_pool,
+    e.instruction :accounts [1] :: STRING AS stake_pool_withdraw_authority,
+    NULL as stake_pool_deposit_authority,
+    e.instruction :accounts [12] :: STRING AS address,  -- use signers instead of instruction account because of "passthrough" wallets
+    e.instruction :accounts [2] :: STRING AS reserve_stake_address,
+    i.value :parsed :info :lamports AS amount,
+    e._inserted_timestamp,
+    concat_ws('-',e.tx_id,e.index) as _unique_key
+from deposit_dao_events e
+LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
+WHERE
+    i.value :parsed :info :lamports IS NOT NULL
 union 
-select 'deposit_dao_with_referrer' as action, *
-from deposit_dao_with_referrer_events 
+select
+    e.tx_id,
+    e.block_id,
+    e.block_timestamp,
+    e.index,
+    e.succeeded,
+    'deposit_dao_with_referrer' AS action,
+    e.instruction :accounts [0] :: STRING AS stake_pool,
+    e.instruction :accounts [1] :: STRING AS stake_pool_withdraw_authority,
+    NULL as stake_pool_deposit_authority,
+    e.instruction :accounts [13] :: STRING AS address,  -- use signers instead of instruction account because of "passthrough" wallets
+    e.instruction :accounts [2] :: STRING AS reserve_stake_address,
+    i.value :parsed :info :lamports AS amount,
+    e._inserted_timestamp,
+    concat_ws('-',e.tx_id,e.index) as _unique_key
+from deposit_dao_with_referrer_events e
+LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
+WHERE
+    i.value :parsed :info :lamports IS NOT NULL
+-- union 
+-- select 'deposit_stake' as action, *
+-- from deposit_stake_events 
+union
+select 
+    e.tx_id,
+    e.block_id,
+    e.block_timestamp,
+    e.index,
+    e.succeeded,
+    'deposit_dao_stake' AS action,
+    e.stake_pool,
+    e.stake_pool_withdraw_authority,
+    e.stake_pool_deposit_authority,
+    e.address,
+    e.reserve_stake_address,
+    e.amount :: NUMBER AS amount,
+    e._inserted_timestamp,
+    concat_ws(
+        '-',
+        e.tx_id,
+        e.index
+    ) AS _unique_key
+from deposit_dao_stake_merge e 
 union 
-select 'deposit_stake' as action, *
-from deposit_stake_events 
+select 
+    e.tx_id,
+    e.block_id,
+    e.block_timestamp,
+    e.index,
+    e.succeeded,
+    'withdraw' AS action,
+    e.instruction :accounts [0] :: STRING AS stake_pool,
+    e.instruction :accounts [1] :: STRING AS stake_pool_withdraw_authority,
+    NULL as stake_pool_deposit_authority,
+    e.instruction :accounts [5] :: STRING AS address,  -- use signers instead of instruction account because of "passthrough" wallets
+    e.instruction :accounts [4] :: STRING AS reserve_stake_address,
+    i.value :parsed :info :lamports AS amount,
+    e._inserted_timestamp,
+    concat_ws('-',e.tx_id,e.index) as _unique_key
+from withdraw_events e
+LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
+WHERE
+    i.value :parsed :info :lamports IS NOT NULL
 union 
-select 'deposit_dao_stake' as action, *
-from deposit_dao_stake_events 
+select 
+    e.tx_id,
+    e.block_id,
+    e.block_timestamp,
+    e.index,
+    e.succeeded,
+    'withdraw_dao' AS action,
+    e.instruction :accounts [0] :: STRING AS stake_pool,
+    e.instruction :accounts [1] :: STRING AS stake_pool_withdraw_authority,
+    NULL as stake_pool_deposit_authority,
+    e.instruction :accounts [14] :: STRING AS address,  -- use signers instead of instruction account because of "passthrough" wallets
+    e.instruction :accounts [5] :: STRING AS reserve_stake_address,
+    i.value :parsed :info :lamports AS amount,
+    e._inserted_timestamp,
+    concat_ws('-',e.tx_id,e.index) as _unique_key
+from withdraw_dao_events e
+LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
+WHERE
+    i.value :parsed :info :lamports IS NOT NULL
 union 
-select 'withdraw' as action, *
-from withdraw_events
+select 
+    e.tx_id,
+    e.block_id,
+    e.block_timestamp,
+    e.index,
+    e.succeeded,
+    'withdraw_stake' AS action,
+    e.instruction :accounts [0] :: STRING AS stake_pool,
+    e.instruction :accounts [2] :: STRING AS stake_pool_withdraw_authority,
+    NULL as stake_pool_deposit_authority,
+    e.instruction :accounts [5] :: STRING AS address,  -- use signers instead of instruction account because of "passthrough" wallets
+    NULL AS reserve_stake_address,
+    i.value :parsed :info :lamports AS amount,
+    e._inserted_timestamp,
+    concat_ws('-',e.tx_id,e.index) as _unique_key
+from withdraw_stake_events e
+LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
+WHERE
+    i.value :parsed :info :lamports IS NOT NULL
 union 
-select 'withdraw_stake' as action, *
-from withdraw_stake_events
--- deposit_stake_merge AS (
---     SELECT
---         e.tx_id,
---         e.block_id,
---         e.block_timestamp,
---         e.index,
---         e.succeeded,
---         e.instruction :accounts [0] :: STRING AS stake_pool,
---         e.instruction :accounts [3] :: STRING AS stake_pool_withdraw_authority,
---         e.instruction :accounts [2] :: STRING AS stake_pool_deposit_authority,
---         b.signers [0] :: STRING AS address,
---         e.instruction :accounts [6] :: STRING AS reserve_stake_address,
---         i.value :parsed :info :destination :: STRING AS merge_destination,
---         silver.udf_get_account_balances_index(
---             merge_destination,
---             b.account_keys
---         ) AS merge_destination_balances_index,
---         silver.udf_get_account_balances_index(
---             reserve_stake_address,
---             b.account_keys
---         ) AS reserve_stake_balances_index,
---         b.post_balances [merge_destination_balances_index] - b.pre_balances [merge_destination_balances_index] + b.post_balances [reserve_stake_balances_index] - b.pre_balances [reserve_stake_balances_index] AS amount,
---         e._inserted_timestamp
---     FROM
---         deposit_stake_events e
---         LEFT OUTER JOIN base_balances b
---         ON b.tx_id = e.tx_id
---         LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
---     WHERE
---         i.value :parsed :type = 'merge'
---         AND i.value :programId = 'Stake11111111111111111111111111111111111111'
--- )
--- SELECT
---     e.tx_id,
---     e.block_id,
---     e.block_timestamp,
---     e.index,
---     e.succeeded,
---     'deposit' AS action,
---     e.instruction :accounts [0] :: STRING AS stake_pool,
---     e.instruction :accounts [1] :: STRING AS stake_pool_withdraw_authority,
---     NULL as stake_pool_deposit_authority,
---     b.signers[0] :: STRING AS address,  -- use signers instead of instruction account because of "passthrough" wallets
---     e.instruction :accounts [2] :: STRING AS reserve_stake_address,
---     i.value :parsed :info :lamports AS amount,
---     e._inserted_timestamp,
---     concat_ws('-',e.tx_id,e.index) as _unique_key
--- FROM
---     deposit_events e
---     LEFT OUTER JOIN base_balances b
---         ON b.tx_id = e.tx_id
---     LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
--- WHERE
---     i.value :parsed :info :lamports IS NOT NULL
--- UNION
--- SELECT
---     e.tx_id,
---     e.block_id,
---     e.block_timestamp,
---     e.index,
---     e.succeeded,
---     'withdraw' AS action,
---     e.instruction :accounts [0] :: STRING AS stake_pool,
---     e.instruction :accounts [1] :: STRING AS stake_pool_withdraw_authority,
---     NULL as stake_pool_deposit_authority,
---     e.instruction :accounts [5] :: STRING AS address,
---     e.instruction :accounts [4] :: STRING AS reserve_stake_address,
---     i.value :parsed :info :lamports AS amount,
---     e._inserted_timestamp,
---     concat_ws('-',tx_id,e.index) as _unique_key
--- FROM
---     withdraw_events e,
---     TABLE(FLATTEN(inner_instruction :instructions)) i
--- WHERE
---     i.value :parsed :info :lamports IS NOT NULL
--- UNION
--- SELECT
---     e.tx_id,
---     e.block_id,
---     e.block_timestamp,
---     e.index,
---     e.succeeded,
---     'deposit_stake' AS action,
---     e.stake_pool,
---     e.stake_pool_withdraw_authority,
---     e.stake_pool_deposit_authority,
---     e.address,
---     e.reserve_stake_address,
---     e.amount :: NUMBER AS amount,
---     e._inserted_timestamp,
---     concat_ws(
---         '-',
---         e.tx_id,
---         e.index
---     ) AS _unique_key
--- FROM
---     deposit_stake_merge e
--- UNION
--- SELECT
---     e.tx_id,
---     e.block_id,
---     e.block_timestamp,
---     e.index,
---     e.succeeded,
---     'withdraw_stake' AS action,
---     e.instruction :accounts [0] :: STRING AS stake_pool,
---     e.instruction :accounts [2] :: STRING AS stake_pool_withdraw_authority,
---     NULL as stake_pool_deposit_authority,
---     e.instruction :accounts [5] :: STRING AS address,
---     NULL AS reserve_stake_address,
---     i.value :parsed :info :lamports AS amount,
---     e._inserted_timestamp,
---     concat_ws('-',tx_id,e.index) as _unique_key
--- FROM
---     withdraw_stake_events e,
---     TABLE(FLATTEN(inner_instruction :instructions)) i
--- WHERE
---     i.value :parsed :info :lamports IS NOT NULL
+select 
+    e.tx_id,
+    e.block_id,
+    e.block_timestamp,
+    e.index,
+    e.succeeded,
+    'withdraw_dao_stake' AS action,
+    e.instruction :accounts [0] :: STRING AS stake_pool,
+    e.instruction :accounts [2] :: STRING AS stake_pool_withdraw_authority,
+    NULL as stake_pool_deposit_authority,
+    e.instruction :accounts [15] :: STRING AS address,  -- use signers instead of instruction account because of "passthrough" wallets
+    NULL AS reserve_stake_address,
+    i.value :parsed :info :lamports AS amount,
+    e._inserted_timestamp,
+    concat_ws('-',e.tx_id,e.index) as _unique_key
+from withdraw_dao_stake_events e
+LEFT OUTER JOIN TABLE(FLATTEN(inner_instruction :instructions)) i
+WHERE
+    i.value :parsed :info :lamports IS NOT NULL
