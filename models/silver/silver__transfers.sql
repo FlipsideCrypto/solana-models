45fdfca4fb3c235fa81a0a0f6e5cc6dac17a85be
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-12-05 13:51:51 -0800

    An 2151/hot swap transfers2 (#173)
    * make transfers2 the regular transfers model
    
    * keep existing behavior, only successful transfers in view
    
    * add succeeded to yml

--- models/silver/silver__transfers.sql
+++ models/silver/silver__transfers.sql
@@ -6,7 +6,7 @@
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION"
 ) }}
 
-With base_transfers_i AS (
+WITH base_transfers_i AS (
     SELECT
         block_id,
         block_timestamp,
@@ -16,6 +16,7 @@ With base_transfers_i AS (
         program_id,
         instruction,
         inner_instruction,
+        succeeded,
         _inserted_timestamp
     FROM
         {{ ref('silver__events') }}
@@ -25,7 +26,6 @@ With base_transfers_i AS (
         'transferChecked',
         'transferWithSeed'
     )
-    AND succeeded = TRUE
 
 {% if is_incremental() and env_var(
     'DBT_IS_BATCH_LOAD',
@@ -70,14 +70,13 @@ AND
         ii.value :programId :: STRING AS program_id,
         ii.value as instruction,
         NULL AS inner_instruction,
+        e.succeeded,
         _inserted_timestamp
     FROM
         {{ ref('silver__events') }}
         e,
         TABLE(FLATTEN(e.inner_instruction :instructions)) ii
     WHERE
-        e.succeeded = TRUE
-        AND
         ii.value :parsed :type :: STRING IN (
         'transfer',
         'transferChecked',
@@ -200,9 +199,12 @@ spl_transfers AS (
         e.block_timestamp,
         e.tx_id,
         e.index,
+        e.program_id,
+        e.succeeded,
         COALESCE(
             p.owner,
-            e.instruction :parsed :info :authority :: STRING
+            e.instruction :parsed :info :authority :: STRING,
+            e.instruction :parsed :info :multisigAuthority :: STRING
         ) AS tx_from,
         COALESCE(
             p2.owner,
@@ -229,6 +231,8 @@ spl_transfers AS (
             p3.mint,
             p4.mint
         ) AS mint,
+        instruction :parsed :info :source :: STRING as source_token_account,
+        instruction :parsed :info :destination :: STRING as dest_token_account,
         e._inserted_timestamp
     FROM
         base_transfers_i e
@@ -245,7 +249,11 @@ spl_transfers AS (
         ON e.tx_id = p4.tx_id
         AND e.instruction :parsed :info :destination :: STRING = p4.account
     WHERE
-        e.instruction :parsed :info :authority :: STRING IS NOT NULL
+        (
+            e.instruction :parsed :info :authority :: STRING IS NOT NULL
+            OR 
+            e.instruction :parsed :info :multisigAuthority :: STRING IS NOT NULL
+        )
 ),
 sol_transfers AS (
     SELECT
@@ -253,6 +261,8 @@ sol_transfers AS (
         e.block_timestamp,
         e.tx_id,
         e.index,
+        e.program_id,
+        e.succeeded,
         instruction :parsed :info :source :: STRING AS tx_from,
         instruction :parsed :info :destination :: STRING AS tx_to,
         instruction :parsed :info :lamports / pow(
@@ -260,6 +270,8 @@ sol_transfers AS (
             9
         ) AS amount,
         'So11111111111111111111111111111111111111112' AS mint,
+        NULL as source_token_account,
+        NULL as dest_token_account,
         e._inserted_timestamp
     FROM
         base_transfers_i e
@@ -270,11 +282,15 @@ SELECT
     block_id,
     block_timestamp,
     tx_id,
+    program_id,
+    succeeded,
     INDEX,
     tx_from,
     tx_to,
     amount,
     mint,
+    source_token_account,
+    dest_token_account,
     _inserted_timestamp
 FROM
     spl_transfers
@@ -283,11 +299,15 @@ SELECT
     block_id,
     block_timestamp,
     tx_id,
+    program_id,
+    succeeded,
     INDEX,
     tx_from,
     tx_to,
     amount,
     mint,
+    source_token_account,
+    dest_token_account,
     _inserted_timestamp
 FROM
     sol_transfers
