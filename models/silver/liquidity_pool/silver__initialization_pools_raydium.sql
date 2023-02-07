5e00ad3da42f7d0066b8f38bdb8142c594307e4b
tarikceric <tarik@flipsidecrypto.com>
2023-02-06 18:28:04 -0800

    capture additional events

--- models/silver/liquidity_pool/silver__initialization_pools_raydium.sql
+++ models/silver/liquidity_pool/silver__initialization_pools_raydium.sql
@@ -16,75 +16,96 @@ WITH base_events AS(
             '675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8',
             '5quBtoiQqxF9Jv6KYKctB59NT3gtJD2Y65kdnB1Uev3h',
             '27haf8L6oxUeXrHrgEgsexjSY5hbVUWEmvv9Nyxg8vQv'
-        )
--- {% if is_incremental() %}
--- AND _inserted_timestamp >= (
---     SELECT
---         MAX(_inserted_timestamp)
---     FROM
---         {{ this }}
--- )
--- {% else %}
---     AND block_timestamp :: DATE >= '2021-03-06'
--- {% endif %}
+        ) -- {% if is_incremental() %}
+        -- AND _inserted_timestamp >= (
+        --     SELECT
+        --         MAX(_inserted_timestamp)
+        --     FROM
+        --         {{ this }}
+        -- )
+        -- {% else %}
+        --     AND block_timestamp :: DATE >= '2021-03-06'
+        -- {% endif %}
 )
-select
- block_timestamp,
- block_id,
- tx_id,
-instruction:accounts[3]::string as liquidity_pool,
-instruction:accounts[16]::string as owner,
-instruction:accounts[4]::string as mint_authority,
-instruction:accounts[9]::string  as token_a_account,
-instruction:accounts[10]::string  as token_b_account,
-instruction:accounts[6]::string as pool_token,
- _inserted_timestamp
-from base_events e
-where program_id = '675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8'
-and event_type is null
-and succeeded
-and instruction:accounts[0] = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
-and instruction:accounts[1] = '11111111111111111111111111111111'
-and instruction:accounts[2] = 'SysvarRent111111111111111111111111111111111'
-and instruction:accounts[4] = '5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1'
-
-union
-
-select
- block_timestamp,
- block_id,
- tx_id,
-instruction:accounts[3]::string as liquidity_pool,
-'5quBtoiQqxF9Jv6KYKctB59NT3gtJD2Y65kdnB1Uev3h' as owner,
-instruction:accounts[4]::string as mint_authority,
-instruction:accounts[9]::string  as token_a_account,
-instruction:accounts[10]::string  as token_b_account,
-instruction:accounts[6]::string as pool_token,
- _inserted_timestamp
-from base_events e
-where program_id = '5quBtoiQqxF9Jv6KYKctB59NT3gtJD2Y65kdnB1Uev3h'
-and event_type is null
-and succeeded
-and instruction:accounts[0] = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
-and instruction:accounts[1] = '11111111111111111111111111111111'
-and instruction:accounts[2] = 'SysvarRent111111111111111111111111111111111'
-
-union
-
-select
- block_timestamp,
- block_id,
- tx_id,
-instruction:accounts[1]::string as liquidity_pool,
-'27haf8L6oxUeXrHrgEgsexjSY5hbVUWEmvv9Nyxg8vQv' as owner,
-instruction:accounts[2]::string as mint_authority,
-instruction:accounts[7]::string  as token_a_account,
-instruction:accounts[8]::string  as token_b_account,
-instruction:accounts[4]::string as pool_token,
- _inserted_timestamp
-from base_events e
-where program_id = '27haf8L6oxUeXrHrgEgsexjSY5hbVUWEmvv9Nyxg8vQv'
-and event_type is null
-and succeeded
-and instruction:accounts[0] = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
-and instruction:accounts[13] = '9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin'
+SELECT
+    block_timestamp,
+    block_id,
+    tx_id,
+    instruction :accounts [3] :: STRING AS liquidity_pool,
+    '675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8' AS owner,
+    instruction :accounts [4] :: STRING AS mint_authority,
+    instruction :accounts [9] :: STRING AS token_a_account,
+    instruction :accounts [10] :: STRING AS token_b_account,
+    instruction :accounts [6] :: STRING AS pool_token,
+    _inserted_timestamp
+FROM
+    base_events e
+WHERE
+    program_id = '675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8'
+    AND event_type IS NULL
+    AND succeeded
+    AND instruction :accounts [0] = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+    AND instruction :accounts [1] = '11111111111111111111111111111111'
+    AND instruction :accounts [2] = 'SysvarRent111111111111111111111111111111111'
+    AND instruction :accounts [4] = '5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1'
+UNION
+SELECT
+    block_timestamp,
+    block_id,
+    tx_id,
+    instruction :accounts [1] :: STRING AS liquidity_pool,
+    '675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8' AS owner,
+    instruction :accounts [2] :: STRING AS mint_authority,
+    instruction :accounts [7] :: STRING AS token_a_account,
+    instruction :accounts [8] :: STRING AS token_b_account,
+    instruction :accounts [4] :: STRING AS pool_token,
+    _inserted_timestamp
+FROM
+    base_events e
+WHERE
+    program_id = '675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8'
+    AND event_type IS NULL
+    AND succeeded
+    AND instruction :accounts [0] = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+    AND instruction :accounts [12] = '9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin'
+UNION
+SELECT
+    block_timestamp,
+    block_id,
+    tx_id,
+    instruction :accounts [3] :: STRING AS liquidity_pool,
+    '5quBtoiQqxF9Jv6KYKctB59NT3gtJD2Y65kdnB1Uev3h' AS owner,
+    instruction :accounts [4] :: STRING AS mint_authority,
+    instruction :accounts [9] :: STRING AS token_a_account,
+    instruction :accounts [10] :: STRING AS token_b_account,
+    instruction :accounts [6] :: STRING AS pool_token,
+    _inserted_timestamp
+FROM
+    base_events e
+WHERE
+    program_id = '5quBtoiQqxF9Jv6KYKctB59NT3gtJD2Y65kdnB1Uev3h'
+    AND event_type IS NULL
+    AND succeeded
+    AND instruction :accounts [0] = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+    AND instruction :accounts [1] = '11111111111111111111111111111111'
+    AND instruction :accounts [2] = 'SysvarRent111111111111111111111111111111111'
+UNION
+SELECT
+    block_timestamp,
+    block_id,
+    tx_id,
+    instruction :accounts [1] :: STRING AS liquidity_pool,
+    '27haf8L6oxUeXrHrgEgsexjSY5hbVUWEmvv9Nyxg8vQv' AS owner,
+    instruction :accounts [2] :: STRING AS mint_authority,
+    instruction :accounts [7] :: STRING AS token_a_account,
+    instruction :accounts [8] :: STRING AS token_b_account,
+    instruction :accounts [4] :: STRING AS pool_token,
+    _inserted_timestamp
+FROM
+    base_events e
+WHERE
+    program_id = '27haf8L6oxUeXrHrgEgsexjSY5hbVUWEmvv9Nyxg8vQv'
+    AND event_type IS NULL
+    AND succeeded
+    AND instruction :accounts [0] = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+    AND instruction :accounts [13] = '9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin'
