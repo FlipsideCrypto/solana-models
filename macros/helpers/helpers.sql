12442761fcda2f183358751d1ed7e290baaef0a2
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2023-02-09 14:37:48 -0800

    An 2149/account owners model (#237)
    * wip
    
    * wip
    
    * wip
    
    * change dev owner to use new access role
    
    * wip
    
    * update names
    
    * wip
    
    * use macro for dynamic incremental logic
    
    * change names, split stakes acct and token acct into own tables
    
    * workflow to backfill
    
    * disable new models from incremental while it backfills
    
    * include mint of token account on creation
    
    * inner index should not have no null tests, add mint to yml
    
    * make inner_index -1 so that it can be included in unique key merge
    
    * pr fixes
    
    * add dupe tests
    
    * change to full refresh only, works faster for backfills at least
    
    * fix typo
    
    * fix mint null test, only for create events
    
    * account initialization also has mint value
    
    * add ordering based on events to fix same block event issues
    
    * simplify temporary exclude

---
+++ macros/helpers/helpers.sql
@@ -0,0 +1,15 @@
+{% macro get_batch_load_logic(model_name, batch_size_days, end_date) -%}
+    {% set query %}
+    select max(_inserted_timestamp)::date::string from {{ model_name }};
+    {% endset %}
+
+    {% set max_date = run_query(query).columns[0].values()[0] %}
+
+    {% if max_date >= '2022-09-01' and max_date < '2022-09-05' %}
+        {{ "and _inserted_timestamp between (select max(_inserted_timestamp) from " ~ model_name ~ ") and (select dateadd('hour',4,max(_inserted_timestamp)) from " ~ model_name ~ ")" }}
+    {% elif max_date >= '2022-09-05' and max_date < end_date %}
+        {{ "and _inserted_timestamp between (select max(_inserted_timestamp) from " ~ model_name ~ ") and (select dateadd('day',"~ batch_size_days ~",max(_inserted_timestamp)) from " ~ model_name ~ ")" }}
+    {% else %}
+        {{ "and _inserted_timestamp >= (select max(_inserted_timestamp) from " ~ model_name}}
+    {% endif %}
+{%- endmacro %}
---
+++ models/silver/accounts/silver__token_account_owners_intermediate.sql
@@ -0,0 +1,81 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = ["account_address","owner","start_block_id"],
+    cluster_by = ['_inserted_timestamp::DATE'],
+) }}
+
+/*
+for incrementals also select all null end date accounts and combine
+join to eliminate accounts that are not in the subset
+remove all accounts that have the same owner + start block + end block
+*/
+with last_updated_at as (
+    select max(_inserted_timestamp) as _inserted_timestamp
+    from {{ ref('silver__token_account_ownership_events') }}
+),
+base as (
+    select 
+        account_address, 
+        owner, 
+        block_id,
+        case 
+            when event_type in ('create','createIdempotent','createAccount','createAccountWithSeed') then 
+                0
+            when event_type in ('initializeAccount','initializeAccount2','initializeAccount3') then 
+                1
+            else 2
+        end as same_block_order_index
+    from {{ ref('silver__token_account_ownership_events') }}
+    /* incremental condition here */
+    {% if is_incremental() %}
+        where _inserted_timestamp >= (select max(_inserted_timestamp) from {{ this }})
+    {% endif %}
+),
+{% if is_incremental() %}
+current_ownership as (
+    select 
+        t.account_address, 
+        t.owner, 
+        t.start_block_id as block_id
+    from {{ this }} t
+    join (select distinct account_address from base) b on b.account_address = t.account_address
+    where t.end_block_id is null
+    union 
+    select 
+        *
+    from base
+),
+bucketed as (
+    select 
+        *,
+        conditional_change_event(owner) over (partition by account_address order by block_id, same_block_order_index) as bucket
+    from current_ownership
+),
+{% else %}
+bucketed as (
+    select 
+        *,
+        conditional_change_event(owner) over (partition by account_address order by block_id, same_block_order_index) as bucket
+    from base
+),
+{% endif %}
+c as (
+    select 
+        account_address, 
+        owner, 
+        bucket, 
+        min(block_id) as start_block_id
+    from bucketed
+    group by 1,2,3
+)
+select 
+    account_address,
+    owner,
+    start_block_id,
+    lead(start_block_id) ignore nulls over (
+                PARTITION BY account_address
+                ORDER BY bucket
+            ) as end_block_id,
+    _inserted_timestamp
+from c
+join last_updated_at
