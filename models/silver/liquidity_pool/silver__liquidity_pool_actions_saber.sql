f0735e4d4423abf87e4bc25c8a9411d115592f78
tarikceric <tarik@flipsidecrypto.com>
2023-02-22 09:59:37 -0800

    adjust action name to match other models

--- models/silver/liquidity_pool/silver__liquidity_pool_actions_saber.sql
+++ models/silver/liquidity_pool/silver__liquidity_pool_actions_saber.sql
@@ -476,8 +476,8 @@ temp_final AS (
         l.succeeded,
         l.program_id,
         CASE
-            WHEN l.action = 'mintTo' THEN 'mint_LP_tokens'
-            WHEN l.action = 'burn' THEN 'burn_LP_tokens'
+            WHEN l.action = 'mintTo' THEN 'mintTo'
+            WHEN l.action = 'burn' THEN 'burn'
             ELSE NULL
         END AS action,
         l.liquidity_provider,
