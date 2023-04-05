a6aaada846693f1ab99d85b12d65391cc212e408
tarikceric <46071768+tarikceric@users.noreply.github.com>
2023-04-05 10:47:31 -0700

    grant internal_dev access to dev db (#279)

--- macros/run_sp_create_prod_clone.sql
+++ macros/run_sp_create_prod_clone.sql
@@ -1,6 +1,6 @@
 {% macro run_sp_create_prod_clone() %}
 {% set clone_query %}
-call solana._internal.create_prod_clone('solana', 'solana_dev', 'solana_dev_owner');
+call solana._internal.create_prod_clone('solana', 'solana_dev', 'internal_dev');
 {% endset %}
 
 {% do run_query(clone_query) %}
