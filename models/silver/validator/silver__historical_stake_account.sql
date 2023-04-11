07b93a252aecf9740baad5df9aa3878ab0e0a640
tarikceric <tarik@flipsidecrypto.com>
2023-04-11 12:34:19 -0700

    historical validator data tables

---
+++ models/silver/validator/silver__historical_stake_account.sql
@@ -0,0 +1,23 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+SELECT
+    RIGHT(REGEXP_REPLACE(filename, '[^0-9]', ''), 3) AS epoch_ingested_at,
+    json_data :account :data :parsed :info :meta :authorized :staker :: STRING AS authorized_staker,
+    json_data :account :data :parsed :info :meta :authorized :withdrawer :: STRING AS authorized_withdrawer,
+    json_data :account :data :parsed :info :meta :lockup :: variant AS lockup,
+    json_data :account :data :parsed :info :meta :rentExemptReserve :: NUMBER AS rent_exempt_reserve,
+    json_data :account :data :parsed :info :stake :creditsObserved :: NUMBER AS credits_observed,
+    json_data :account :data :parsed :info :stake :delegation :activationEpoch :: NUMBER AS activation_epoch,
+    json_data :account :data :parsed :info :stake :delegation :deactivationEpoch :: NUMBER AS deactivation_epoch,
+    json_data :account :data :parsed :info :stake :delegation :stake :: NUMBER AS stake,
+    json_data :account :data :parsed :info :stake :delegation :voter :: STRING AS voter,
+    json_data :account :data :parsed :info :stake :delegation :warmupCooldownRate :: NUMBER AS warmup_cooldown_rate,
+    json_data :account :data :parsed :type :: STRING AS TYPE,
+    json_data :account :data :program :: STRING AS program,
+    json_data :account :lamports :: NUMBER AS lamports,
+    json_data :account :rentEpoch :: NUMBER AS rentEpoch,
+    json_data :pubkey :: STRING AS pubkey
+FROM
+    solana_dev.bronze.historical_stake_account_data
---
+++ models/silver/validator/silver__historical_vote_account.sql
@@ -0,0 +1,23 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+SELECT
+    RIGHT(REGEXP_REPLACE(filename, '[^0-9]', ''), 3) AS epoch_ingested_at,
+    json_data :account :data :parsed :info :authorizedVoters [0] :authorizedVoter :: STRING AS authorized_voter,
+    json_data :account :data :parsed :info :authorizedVoters [0] :epoch :: STRING AS epoch,
+    json_data :account :data :parsed :info :authorizedWithdrawer :: STRING AS authorized_withdrawer,
+    json_data :account :data :parsed :info :commission :: NUMBER AS commission,
+    json_data :account :data :parsed :info :epochCredits AS epoch_credits,
+    json_data :account :data :parsed :info :lastTimestamp :slot :: NUMBER AS last_timestamp_slot,
+    json_data :account :data :parsed :info :lastTimestamp :timestamp :: timestamp_tz AS last_timestamp,
+    json_data :account :data :parsed :info :nodePubkey :: STRING AS node_pubkey,
+    json_data :account :data :parsed :info :priorVoters AS prior_voters,
+    json_data :account :data :parsed :info :rootSlot :: NUMBER AS root_slot,
+    json_data :account :data :parsed :info :votes AS votes,
+    json_data :account :lamports :: NUMBER AS lamports,
+    json_data :account :owner :: STRING AS owner,
+    json_data :account :rentEpoch :: NUMBER AS rent_epoch,
+    json_data :pubkey :: STRING AS pubkey
+FROM
+    solana_dev.bronze.historical_vote_account_data
