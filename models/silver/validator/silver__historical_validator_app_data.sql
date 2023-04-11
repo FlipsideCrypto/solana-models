e2a43de969d4b70dd6fd2af948bb7556135226ea
tarikceric <46071768+tarikceric@users.noreply.github.com>
2023-04-11 14:34:02 -0700

    An 3004/historical validator tables (#282)
    * historical validator data tables
    
    * exclude validator tables
    
    * convert lamports

---
+++ models/silver/validator/silver__historical_validator_app_data.sql
@@ -0,0 +1,40 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+SELECT
+    DATE_FROM_PARTS(
+        '20' || RIGHT(REGEXP_REPLACE(filename, '[^0-9]', ''), 2) :: INTEGER,
+        SUBSTR(REGEXP_REPLACE(filename, '[^0-9]', ''), 1, 2) :: INTEGER,
+        SUBSTR(REGEXP_REPLACE(filename, '[^0-9]', ''), 3, 2) :: INTEGER
+    ) AS file_date,
+    json_data :account :: STRING AS account,
+    json_data :active_stake :: NUMBER AS active_stake,
+    json_data :admin_warning :: STRING AS admin_warning,
+    json_data :authorized_withdrawer_score :: STRING AS authorized_withdrawer_score,
+    json_data :avatar_url :: STRING AS avatar_url,
+    json_data :commission :: NUMBER AS commission,
+    json_data :consensus_mods_score :: NUMBER AS consensus_mods_score,
+    json_data :created_at :: STRING AS created_at,
+    json_data :data_center_concentration_score :: STRING AS data_center_concentration_score,
+    json_data :delinquent :: BOOLEAN AS delinquent,
+    json_data :details :: STRING AS details,
+    json_data :epoch :: NUMBER AS epoch,
+    json_data :epoch_credits :: NUMBER AS epoch_credits,
+    json_data :keybase_id :: STRING AS keybase_id,
+    json_data :name :: STRING AS NAME,
+    json_data :network :: STRING AS network,
+    json_data :published_information_score :: NUMBER AS published_information_score,
+    json_data :root_distance_score :: NUMBER AS root_distance_score,
+    json_data :security_report_score :: NUMBER AS security_report_score,
+    json_data :skipped_slot_score :: NUMBER AS skipped_slot_score,
+    json_data :software_version :: STRING AS software_version,
+    json_data :software_version_score :: NUMBER AS software_version_score,
+    json_data :stake_concentration_score :: NUMBER AS stake_concentration_score,
+    json_data :total_score :: NUMBER AS total_score,
+    json_data :updated_at :: STRING AS updated_at,
+    json_data :url :: STRING AS url,
+    json_data :vote_distance_score :: NUMBER AS vote_distance_score,
+    json_data :www_url :: STRING AS www_url
+FROM
+    solana_dev.bronze.historical_validator_app_data
