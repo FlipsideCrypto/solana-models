24cc910aa84146127ed17a76c5f861e0d956fb86
tarikceric <tarik@flipsidecrypto.com>
2023-05-03 18:46:19 -0500

    update to select from external table

--- models/bronze/bronze__validators_app_api.sql
+++ models/bronze/bronze__validators_app_api.sql
@@ -3,10 +3,10 @@
 ) }}
 
 SELECT 
-    data as json_data,
+    $1 as json_data,
     TO_TIMESTAMP_NTZ(
-    SUBSTR(SPLIT_PART(filename, '/', 3), 1, 10) :: NUMBER,
+    SUBSTR(SPLIT_PART(METADATA$FILENAME, '/', 3), 1, 10) :: NUMBER,
     0
   ) as _inserted_timestamp
 FROM 
-solana_dev.bronze.temp_streamline_validators_app_api
+streamline.solana_dev.validators_app_list_api
