05249f4ec5c149839b38c35eaa78202731b4405c
tarikceric <tarik@flipsidecrypto.com>
2023-05-15 08:27:13 -0500

    update existing vote acct table

---
+++ models/bronze/bronze__vote_accounts_extended_stats.sql
@@ -0,0 +1,15 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+SELECT 
+    $1 as json_data,
+    TO_TIMESTAMP_NTZ(
+    SUBSTR(SPLIT_PART(METADATA$FILENAME, '/', 3), 1, 10) :: NUMBER,
+    0
+  ) as _inserted_timestamp
+FROM 
+    {{ source(
+        'bronze_streamline', 
+        'validator_vote_accounts'
+    )}}
