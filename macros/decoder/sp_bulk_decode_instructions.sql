87af2252b0b72b0b7e3ae1d60077a00268108ec8
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-06-16 13:12:57 -0400

    format

--- macros/decoder/sp_bulk_decode_instructions.sql
+++ macros/decoder/sp_bulk_decode_instructions.sql
@@ -1,46 +1,28 @@
 {% macro sp_bulk_decode_instructions() %}
   {% if var("UPDATE_UDFS_AND_SPS") %}
     {% set sql %}
-    CREATE
-    OR REPLACE PROCEDURE silver.sp_bulk_decode_instructions() returns TABLE (
-      pid STRING
-    ) LANGUAGE SQL EXECUTE AS owner AS
-  DECLARE
-    cur CURSOR FOR
-  SELECT
-    REPLACE(SPLIT_PART(metadata $ filename, '/', 3), '.json') program_id
-  FROM
-    streamline.solana.decode_instructions_idls;
-  BEGIN
-    FOR cur_row IN cur DO let pid VARCHAR:= cur_row.program_id;
-call silver.sp_bulk_decode_instructions(:pid);
-let cnt VARCHAR:= (
-      SELECT
-        COUNT(*)
-      FROM
-        TABLE(RESULT_SCAN(LAST_QUERY_ID())));
-if (
-          cnt > 0
-        ) THEN
-      INSERT INTO
-        results (pid)
-      VALUES
-        (:pid);
-    END if;
-END FOR;
-let rs resultset:= (
-  SELECT
-    *
-  FROM
-    results
-  ORDER BY
-    pid
-);
--- RETURN test;
--- RETURN TABLE(RESULTSET_FROM_CURSOR(cur));
-RETURN TABLE(rs);
-END;
-{% endset %}
+CREATE OR REPLACE PROCEDURE silver.sp_bulk_decode_instructions()
+RETURNS TABLE (pid string)
+LANGUAGE SQL
+EXECUTE AS OWNER
+AS DECLARE
+  cur CURSOR FOR  
+  Select REPLACE(SPLIT_PART(metadata$filename, '/', 3), '.json') program_id from streamline.solana.decode_instructions_idls    ;
+BEGIN
+  
+    FOR cur_row IN cur DO
+        let pid varchar:= cur_row.program_id;
+        call silver.sp_bulk_decode_instructions(:pid);
+        let cnt varchar := (select count(*) from table(result_scan(last_query_id())));
+        if (cnt > 0) then
+            insert into results (pid) values (:pid);
+        end if;
+    END FOR;
+    let rs resultset := (select * from results order by pid);
+    -- RETURN test;
+    -- RETURN TABLE(RESULTSET_FROM_CURSOR(cur));
+    RETURN TABLE(rs);
+END;{% endset %}
 {% do run_query(sql) %}
 {% endif %}
 {% endmacro %}
