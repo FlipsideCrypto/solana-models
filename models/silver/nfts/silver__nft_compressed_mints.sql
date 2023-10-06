d0a3ebd2f68b0972c008c52e20818f31237aedab
Desmond Hui <desmond@flipsidecrypto.com>
2023-10-06 09:50:00 -0700

    update silver table to use new bronze api data

--- models/silver/nfts/silver__nft_compressed_mints.sql
+++ models/silver/nfts/silver__nft_compressed_mints.sql
@@ -7,54 +7,33 @@
 WITH offchain AS (
 
     SELECT
-        items.value,
-        items.value ['id'] :: STRING AS mint,
+        r.value:tx_id::string as tx_id,
+        coalesce(r.value:mint::string,'') AS mint,
         0.000005 AS mint_price,
         'So11111111111111111111111111111111111111111' AS mint_currency,
         'BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY' AS program_id,
-        page,
-        _inserted_timestamp,
-        nft_collection_mint,
-        items.seq AS seq
+        start_inserted_timestamp as _inserted_timestamp
     FROM
-        {{ target.database }}.bronze_api.helius_compressed_nfts,
-        LATERAL FLATTEN(
-            input => DATA :data [0] :result :items
-        ) AS items
-
-{% if is_incremental() %}
-WHERE _inserted_timestamp >= (
-    SELECT
-        MAX(_inserted_timestamp)
-    FROM
-        {{ this }}
-)
-{% endif %}
+        {{ ref('bronze_api__parse_compressed_nft_mints')}},
+        TABLE(FLATTEN(responses)) AS r
+    WHERE 
+        mint <> ''
+    {% if is_incremental() %}
+    AND _inserted_timestamp >= (
+        SELECT
+            MAX(_inserted_timestamp)
+        FROM
+            {{ this }}
+    )
+    {% endif %}
 
 qualify(ROW_NUMBER() over (PARTITION BY mint
 ORDER BY
     _inserted_timestamp DESC)) = 1
 ),
-offchain_ordered AS (
-    SELECT
-        *,
-        ROW_NUMBER() over (
-            PARTITION BY nft_collection_mint
-            ORDER BY
-                page,
-                seq
-        ) AS ROW_NUMBER
-    FROM
-        offchain
-),
 onchain AS (
     SELECT
-        *,
-        ROW_NUMBER() over (
-            PARTITION BY collection_mint
-            ORDER BY
-                block_timestamp DESC
-        ) AS ROW_NUMBER
+        *
     FROM
         {{ ref('silver__nft_compressed_mints_onchain') }}
 
@@ -71,19 +50,16 @@ SELECT
     A.block_timestamp,
     A.block_id,
     A.succeeded,
-    A.tx_id,
+    b.tx_id,
     A.leaf_owner,
     A.collection_mint,
-    A._inserted_timestamp,
+    b._inserted_timestamp,
     A.creator_address AS purchaser,
     b.mint,
     b.mint_price,
     b.mint_currency,
     b.program_id
 FROM
-    onchain A
-    LEFT JOIN offchain_ordered b
-    ON A.row_number = b.row_number
-    AND A.collection_mint = b.nft_collection_mint
-WHERE
-    b.row_number IS NOT NULL
+    offchain b
+    LEFT JOIN onchain A
+    ON A.tx_id = b.tx_id
