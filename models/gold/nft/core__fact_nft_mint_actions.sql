9e870630b3712e482b444d7da883f465dd99d231
tarikceric <tarik@flipsidecrypto.com>
2023-10-09 20:49:08 -0700

    update location and nft_mint_actions

---
+++ models/gold/nft/core__fact_nft_mint_actions.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+SELECT
+    block_id,
+    block_timestamp,
+    tx_id,
+    succeeded,
+    index,
+    inner_index,
+    event_type,
+    mint,
+    mint_amount,
+    mint_authority,
+    signers,
+    DECIMAL,
+    mint_standard_type
+FROM
+    {{ ref('silver__nft_mint_actions') }}
+
+    
+
+    
