f8090670648cffc304ab9d5713d0d3f29d39e65b
Desmond Hui <desmond@flipsidecrypto.com>
2023-10-10 09:15:19 -0700

    check for blocks with 0 txs

--- models/silver/silver___blocks_tx_count.sql
+++ models/silver/silver___blocks_tx_count.sql
@@ -1,22 +1,27 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = ['block_id'],
-    full_refresh = false
+    unique_key = ['block_id']
 ) }}
 
 SELECT
-    value:id as block_id,
-    case when value:error:code::number = -32009 then 
-        0
-    else 
-        array_size(value:result:signatures::array) 
-    end as tx_count,
+    VALUE :id AS block_id,
+    CASE
+        WHEN VALUE :error :code :: NUMBER = -32009 THEN 0
+        ELSE ARRAY_SIZE(
+            VALUE :result :signatures :: ARRAY
+        )
+    END AS tx_count,
     _inserted_timestamp
 FROM
     {{ ref('bronze_api__blocks_tx_count') }},
-    table(flatten(data)) d
+    TABLE(FLATTEN(DATA)) d
 
 {% if is_incremental() %}
-WHERE 
-    _inserted_timestamp >= (select max(_inserted_timestamp) from {{ this }})
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(_inserted_timestamp)
+        FROM
+            {{ this }}
+    )
 {% endif %}
