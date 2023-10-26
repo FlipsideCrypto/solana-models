be8fa98249345dc4f69b75d35f08e1fb9db39086
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2023-10-26 09:40:03 -0700

    temporarily use a materialized backfill queue (#375)
    d

--- models/bronze/bronze_api/bronze_api__parse_compressed_nft_mints.sql
+++ models/bronze/bronze_api/bronze_api__parse_compressed_nft_mints.sql
@@ -3,73 +3,96 @@
     tags = ['bronze_api']
 ) }}
 
-WITH collection_subset AS (
-
-    SELECT
-        block_timestamp,
-        block_id,
-        tx_id,
-        _inserted_timestamp
-    FROM
-        {{ ref('silver__nft_compressed_mints_onchain') }}
-
-{% if is_incremental() %}
-WHERE
-    _inserted_timestamp >= (
+{% if execute %}
+    {% set query = """
         SELECT
-            MAX(end_inserted_timestamp)
+            min(gn) as min_gn
         FROM
-            {{ this }}
-    )
-{% else %}
-WHERE
-    _inserted_timestamp :: DATE = '2023-02-07'
+            """ ~ source('bronze_api_prod','parse_compressed_nft_mints_requests') ~ """
+        """ 
+    %}
+    {% set incr = "" %}
+    {% if is_incremental() %}
+        {% set incr = """
+            WHERE event_inserted_timestamp >= (
+                SELECT
+                    MAX(end_inserted_timestamp)
+                FROM
+                    """ ~ this ~ """
+            )
+        """ %}
+    {% endif %}
+    {% set min_gn = run_query(query ~ incr).columns[0].values()[0] %}
 {% endif %}
 
-qualify(ROW_NUMBER() over (
-ORDER BY
-    _inserted_timestamp)) <= 5000
-),
-base AS (
-    SELECT
-        e.tx_id,
-        e.index,
-        ii.index AS inner_index,
-        ii.value :data :: STRING AS DATA,
-        OBJECT_CONSTRUCT(
-            'tx_id',
-            e.tx_id,
-            'index',
-            e.index,
-            'inner_index',
-            inner_index,
-            'instruction_data',
-            DATA
-        ) AS request,
-        ii.value :programId :: STRING AS ii_program_id,
-        ROW_NUMBER() over (
-            ORDER BY
-                e._inserted_timestamp,
-                e.tx_id
-        ) AS rn,
-        FLOOR(
-            rn / 1000
-        ) AS gn,
-        e._inserted_timestamp AS event_inserted_timestamp
-    FROM
-        collection_subset C
-        JOIN {{ ref('silver__events') }}
-        e
-        ON C.tx_id = e.tx_id
-        JOIN TABLE(FLATTEN(e.inner_instruction :instructions)) ii
-    WHERE
-        e.program_id IN (
-            'BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY',
-            '1atrmQs3eq1N2FEYWu6tyTXbCjP4uQwExpjtnhXtS8h'
-        )
-        AND e.block_timestamp :: DATE = C.block_timestamp :: DATE
-        AND ii_program_id = 'noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV'
-)
+-- WITH collection_subset AS (
+
+--     SELECT
+--         block_timestamp,
+--         block_id,
+--         tx_id,
+--         _inserted_timestamp
+--     FROM
+--         {{ ref('silver__nft_compressed_mints_onchain') }}
+
+-- {% if is_incremental() %}
+-- WHERE
+--     _inserted_timestamp >= (
+--         SELECT
+--             MAX(end_inserted_timestamp)
+--         FROM
+--             {{ this }}
+--     )
+-- {% else %}
+-- WHERE
+--     _inserted_timestamp :: DATE = '2023-02-07'
+-- {% endif %}
+
+-- qualify(ROW_NUMBER() over (
+-- ORDER BY
+--     _inserted_timestamp)) <= 5000
+-- ),
+-- base AS (
+--     SELECT
+--         e.tx_id,
+--         e.index,
+--         ii.index AS inner_index,
+--         ii.value :data :: STRING AS DATA,
+--         OBJECT_CONSTRUCT(
+--             'tx_id',
+--             e.tx_id,
+--             'index',
+--             e.index,
+--             'inner_index',
+--             inner_index,
+--             'instruction_data',
+--             DATA
+--         ) AS request,
+--         ii.value :programId :: STRING AS ii_program_id,
+--         ROW_NUMBER() over (
+--             ORDER BY
+--                 e._inserted_timestamp,
+--                 e.tx_id
+--         ) AS rn,
+--         FLOOR(
+--             rn / 1000
+--         ) AS gn,
+--         e._inserted_timestamp AS event_inserted_timestamp
+--     FROM
+--         collection_subset C
+--         JOIN {{ ref('silver__events') }}
+--         e
+--         ON C.tx_id = e.tx_id
+--         JOIN TABLE(FLATTEN(e.inner_instruction :instructions)) ii
+--     WHERE
+--         e.program_id IN (
+--             'BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY',
+--             '1atrmQs3eq1N2FEYWu6tyTXbCjP4uQwExpjtnhXtS8h'
+--         )
+--         AND e.block_timestamp :: DATE = C.block_timestamp :: DATE
+--         AND ii_program_id = 'noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV'
+-- )
+
 SELECT
     ARRAY_AGG(request) AS batch_request,
     streamline.udf_bulk_parse_compressed_nft_mints(batch_request) AS responses,
@@ -81,8 +104,8 @@ SELECT
         gn
     ) AS _id
 FROM
-    base
-WHERE
-    gn < 5
+    {{ source('bronze_api_prod','parse_compressed_nft_mints_requests') }}
+WHERE 
+    gn between {{ min_gn }} and {{ min_gn }}+4
 GROUP BY
     gn
