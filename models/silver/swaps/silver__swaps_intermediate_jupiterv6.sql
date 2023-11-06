db6a9acdbe68ed1b1389a2580279c4a56a883f2d
tarikceric <tarik@flipsidecrypto.com>
2023-11-06 10:53:41 -0800

    clean up

--- models/silver/swaps/silver__swaps_intermediate_jupiterv6.sql
+++ models/silver/swaps/silver__swaps_intermediate_jupiterv6.sql
@@ -1,9 +1,8 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = ["block_id","_log_id"],
-    incremental_predicates = ['DBT_INTERNAL_DEST.block_timestamp::date >= LEAST(current_date-7,(select min(block_timestamp)::date from ' ~ generate_tmp_view_name(this) ~ '))'],
+    unique_key = ['tx_id','index','program_id'],
+    merge_exclude_columns = ["inserted_timestamp"],
     cluster_by = ['block_timestamp::DATE','_inserted_timestamp::DATE'],
-    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION"
 ) }}
 
 WITH base AS (
@@ -11,7 +10,7 @@ WITH base AS (
     SELECT
         *
     FROM
-        solana.silver.decoded_instructions
+        {{ ref('silver__decoded_instructions') }}
     WHERE
         program_id = 'JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4'
 
@@ -28,65 +27,27 @@ AND _inserted_timestamp >= (
 ),
 decoded AS (
     SELECT
-        A.block_timestamp,
-        A.block_id,
-        A.tx_id,
-        A.index,
-        A._inserted_timestamp,
-        A.decoded_instruction :args :inAmount :: INT AS in_amount,
-        MAX(
-            CASE
-                WHEN b.value :name = 'userTransferAuthority' THEN b.value :pubkey :: STRING
-            END
-        ) AS swapper,
-        MAX(
-            CASE
-                WHEN b.value :name = 'sourceTokenAccount' THEN b.value :pubkey :: STRING
-            END
-        ) AS source_token_account,
-        MAX(
-            CASE
-                WHEN b.value :name = 'sourceMint' THEN b.value :pubkey :: STRING
-            END
-        ) AS source_mint,
-        MAX(
-            CASE
-                WHEN b.value :name = 'destinationMint' THEN b.value :pubkey :: STRING
-            END
-        ) AS destination_mint,
-        MAX(
-            CASE
-                WHEN b.value :name = 'destinationTokenAccount' THEN b.value :pubkey :: STRING
-            END
-        ) AS destination_token_account,
-        MAX(
-            CASE
-                WHEN b.value :name = 'programDestinationTokenAccount' THEN b.value :pubkey :: STRING
-            END
-        ) AS program_destination_token_account,
-        MAX(
-            CASE
-                WHEN b.value :name = 'programSourceTokenAccount' THEN b.value :pubkey :: STRING
-            END
-        ) AS program_source_token_account
+        block_timestamp,
+        block_id,
+        tx_id,
+        index,
+        _inserted_timestamp,
+        program_id,
+        solana_dev.silver.udf_get_account_pubkey_by_name('userTransferAuthority', decoded_instruction:accounts) as swapper,
+        solana_dev.silver.udf_get_account_pubkey_by_name('sourceTokenAccount', decoded_instruction:accounts) as source_token_account,
+        solana_dev.silver.udf_get_account_pubkey_by_name('sourceMint', decoded_instruction:accounts) as source_mint,
+        solana_dev.silver.udf_get_account_pubkey_by_name('destinationMint', decoded_instruction:accounts) as destination_mint,
+        solana_dev.silver.udf_get_account_pubkey_by_name('destinationTokenAccount', decoded_instruction:accounts) as destination_token_account,
+        solana_dev.silver.udf_get_account_pubkey_by_name('programDestinationTokenAccount', decoded_instruction:accounts) as program_destination_token_account,
+        solana_dev.silver.udf_get_account_pubkey_by_name('programSourceTokenAccount', decoded_instruction:accounts) as program_source_token_account
     FROM
-        base A,
-        LATERAL FLATTEN(
-            input => A.decoded_instruction :accounts
-        ) b
-    GROUP BY
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
+        base
 ),
 transfers AS (
     SELECT
         A.*
     FROM
-        solana.silver.transfers A
+        {{ ref('silver__transfers') }} A
         INNER JOIN (
             SELECT
                 DISTINCT tx_id
@@ -94,9 +55,10 @@ transfers AS (
                 decoded
         ) d
         ON d.tx_id = A.tx_id
+        where a.succeeded
 
 {% if is_incremental() %}
-WHERE
+AND
     A._inserted_timestamp >= (
         SELECT
             MAX(_inserted_timestamp)
@@ -104,15 +66,16 @@ WHERE
             {{ this }}
     )
 {% else %}
-WHERE
+AND
     A.block_timestamp :: DATE >= '2023-07-04'
 {% endif %}
 )
 SELECT
     A.block_id,
     A.block_timestamp,
-    'JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4' AS program_id,
+    A.program_id,
     A.tx_id,
+    A.index,
     C.succeeded,
     A.swapper,
     b.amount AS from_amt,
@@ -120,19 +83,18 @@ SELECT
     C.amount AS to_amt,
     A.destination_mint AS to_mint,
     A._inserted_timestamp,
-    CONCAT(
-        A.tx_id,
-        '-',
-        A.index
-    ) AS _log_id
+  {{ dbt_utils.generate_surrogate_key(['a.tx_id','a.index','a.program_id']) }} as swaps_intermediate_jupiterv6_id,
+    sysdate() as inserted_timestamp,
+    sysdate() as modified_timestamp,
+    '{{ invocation_id }}' AS invocation_id
 FROM
     decoded A
-    LEFT JOIN transfers b
+    inner JOIN transfers b
     ON A.tx_id = b.tx_id
     AND A.source_token_account = b.source_token_account
     AND A.program_source_token_account = b.dest_token_account
     AND COALESCE(SPLIT_PART(b.index :: text, '.', 1) :: INT, b.index :: INT) = A.index
-    LEFT JOIN transfers C
+    inner JOIN transfers C
     ON A.tx_id = C.tx_id
     AND A.destination_token_account = C.dest_token_account
     AND A.program_destination_token_account = C.source_token_account
