fb2dcdd3e5e69e1813198595e59dbdf079eba621
tarikceric <tarik@flipsidecrypto.com>
2023-11-07 11:30:07 -0800

    remove old views

--- models/gold/defi/core__fact_swaps.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    materialized = 'view',
-    meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'SWAPS' }}}
-) }}
-
-SELECT
-    *
-FROM
-    {{ ref('defi__fact_swaps') }}
--- models/gold/price/core__ez_token_prices_hourly.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-    materialized = 'view'
-) }}
-
-SELECT
-    *
-FROM
-    {{ ref('price__ez_token_prices_hourly') }}
