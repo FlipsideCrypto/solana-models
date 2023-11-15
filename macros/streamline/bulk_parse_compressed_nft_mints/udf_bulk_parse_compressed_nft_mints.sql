6d84ac1cfa31986079d9f96ccdc4dc9408bc3f9b
Desmond Hui <desmond@flipsidecrypto.com>
2023-11-15 08:08:50 -0800

    force paralell execution for each udf request

--- macros/streamline/bulk_parse_compressed_nft_mints/udf_bulk_parse_compressed_nft_mints.sql
+++ macros/streamline/bulk_parse_compressed_nft_mints/udf_bulk_parse_compressed_nft_mints.sql
@@ -1,6 +1,10 @@
 {% macro udf_bulk_parse_compressed_nft_mints() %}
     CREATE
-    OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_parse_compressed_nft_mints("JSON" ARRAY) returns ARRAY api_integration = aws_solana_api_dev AS {% if target.database == 'SOLANA' -%}
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_parse_compressed_nft_mints("JSON" ARRAY) 
+    returns ARRAY 
+    api_integration = aws_solana_api_dev 
+    max_batch_rows = 1
+    AS {% if target.database == 'SOLANA' -%}
         'https://cpzzn7ohu0.execute-api.us-east-1.amazonaws.com/prod/parse'
     {% else %}
         'https://rd7pddtgl9.execute-api.us-east-1.amazonaws.com/dev/parse'
