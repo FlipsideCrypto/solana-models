424db897ef81d636cea1e6eeb831aa0b1a444976
tarikceric <tarik@flipsidecrypto.com>
2023-11-15 21:17:34 -0800

    move logic to silver

--- models/silver/metadata/helius/silver__helius_nft_metadata.sql
+++ models/silver/metadata/helius/silver__helius_nft_metadata.sql
@@ -3,24 +3,23 @@
     unique_key = "mint",
     incremental_strategy = 'delete+insert',
     cluster_by = ['_inserted_timestamp::DATE'],
-    tags = ['helius']
+    tags = ['nft_api']
 ) }}
 
+with pre_final as (
 SELECT
     items.value ['id'] :: STRING AS mint,
     items.value ['authorities'] AS authorities,
-    items.value ['burnt']::boolean AS burnt,
-    items.value ['compression'] AS compression,
+    -- items.value ['burnt']::boolean AS burnt,
+    -- items.value ['compression'] AS compression,
     items.value ['content'] AS content,
     items.value ['content'] ['metadata'] AS metadata,
     items.value ['creators'] AS creators,
-    items.value ['grouping'] AS GROUPING,
-    grouping[0]:group_key::string AS group_key,
-    grouping[0]:group_value::string AS group_value,
-    items.value ['interface'] :: STRING AS INTERFACE,
-    items.value ['mutable'] :: boolean AS mutable,
-    items.value ['ownership'] AS ownership,
-    items.value ['royalty'] AS royalty,
+    items.value ['grouping'][0]:group_value::string AS group_value,
+    -- items.value ['interface'] :: STRING AS INTERFACE,
+    -- items.value ['mutable'] :: boolean AS mutable,
+    -- items.value ['ownership'] AS ownership,
+    -- items.value ['royalty'] AS royalty,
     items.value ['supply'] AS supply,
     _inserted_timestamp
 FROM
@@ -29,16 +28,30 @@ FROM
         input => DATA :data :result
     ) AS items
     WHERE mint is not NULL
-{% if is_incremental() %}
-AND
-    _inserted_timestamp >= (
-        SELECT
-            MAX(_inserted_timestamp)
-        FROM
-            {{ this }}
-    )
-{% endif %}
+    and _inserted_timestamp <= '2023-10-13 05:35:36.367'
+
     qualify(ROW_NUMBER() over (PARTITION BY mint
 ORDER BY
-    _inserted_timestamp DESC)) = 1
+    _inserted_timestamp DESC)) = 1)
+
+select 
+    mint,
+    authorities[0]:address::string as creator_address,
+    metadata:attributes::array as metadata,
+    content:links:image::string as image_url,
+    content:json_uri::string as metadata_uri,
+    supply:edition_nonce::int as edition,
+    metadata:name::string as nft_name,
+    _inserted_timestamp,
+    {{ dbt_utils.generate_surrogate_key(['mint']) }} AS nft_metadata_id,
+    CASE 
+        WHEN group_value IS NULL 
+        THEN NULL 
+        ELSE {{ dbt_utils.generate_surrogate_key(['group_value']) }} 
+    END AS nft_collection_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS invocation_id
+from pre_final
+
 
---
+++ models/silver/metadata/silver__nft_collection.sql
@@ -0,0 +1,103 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "collection_id",
+    incremental_strategy = 'merge',
+    merge_exclude_columns = ["inserted_timestamp"],
+    tags = ['nft_api']
+) }}
+
+with collections as (
+SELECT
+    items.value ['id'] :: STRING AS mint,
+    items.value ['grouping'] AS GROUPING,
+    grouping[0]:group_key::string AS group_key,
+    grouping[0]:group_value::string AS collection_id,
+    ROW_NUMBER() over (
+            PARTITION BY collection_id
+            ORDER BY
+                _inserted_timestamp
+        ) AS temp_rn,
+    _inserted_timestamp
+FROM
+    {{ ref('bronze_api__helius_nft_metadata') }},
+    LATERAL FLATTEN(
+        input => DATA :data :result
+    ) AS items
+WHERE
+    group_key = 'collection'
+{% if is_incremental() %}
+AND
+    _inserted_timestamp >= (
+        SELECT
+            MAX(_inserted_timestamp)
+        FROM
+            {{ this }}
+    )
+{% else %}
+    AND _inserted_timestamp :: DATE = '2023-10-03'
+{% endif %}
+    ),
+distinct_collections AS (
+    SELECT
+        mint,
+        collection_id,
+        _inserted_timestamp,
+        ROW_NUMBER() over (
+            ORDER BY
+                _inserted_timestamp
+        ) AS rn
+    FROM
+        collections
+    WHERE
+        temp_rn = 1
+{% if is_incremental() %}
+AND
+    collection_id not in (
+        SELECT
+            distinct collection_id
+        FROM
+            {{ this }}
+    )
+{% else %}
+    AND _inserted_timestamp :: DATE = '2023-10-03'
+{% endif %}
+
+),
+response AS ({% for item in range(1, 500) %}
+    (
+    SELECT
+        mint, 
+        collection_id, 
+        _inserted_timestamp, 
+        livequery_dev.live.udf_api('GET', 'https://pro-api.solscan.io/v1.0/nft/token/info/' || (mint), OBJECT_CONSTRUCT('Accept', 'application/json', 'token', (
+            SELECT
+                api_key
+            FROM
+                crosschain.silver.apis_keys
+            WHERE
+                api_name = 'solscan')),{}) AS DATA
+    FROM
+        distinct_collections
+WHERE
+    rn = {{ item }}) {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+{% endfor %})
+SELECT
+    collection_id,
+    CASE 
+        WHEN DATA :data :data [0] :nft_collection_name :: STRING = '' 
+        THEN NULL 
+        ELSE DATA :data :data [0] :nft_collection_name :: STRING 
+    END AS nft_collection_name,
+    _inserted_timestamp,
+    CASE 
+        WHEN collection_id IS NULL 
+        THEN NULL 
+        ELSE {{ dbt_utils.generate_surrogate_key(['collection_id']) }} 
+    END AS nft_collection_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS invocation_id
+FROM
+    response
