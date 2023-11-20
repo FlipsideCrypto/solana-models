c89e43c3d6a0f9c016e550ae7dfcda608b9dcb47
tarikceric <tarik@flipsidecrypto.com>
2023-11-20 11:25:44 -0800

    cleanup, update surrogate name

--- models/silver/metadata/helius/silver__helius_nft_metadata.sql
+++ models/silver/metadata/helius/silver__helius_nft_metadata.sql
@@ -10,16 +10,10 @@ with pre_final as (
 SELECT
     items.value ['id'] :: STRING AS mint,
     items.value ['authorities'] AS authorities,
-    -- items.value ['burnt']::boolean AS burnt,
-    -- items.value ['compression'] AS compression,
     items.value ['content'] AS content,
     items.value ['content'] ['metadata'] AS metadata,
     items.value ['creators'] AS creators,
     items.value ['grouping'][0]:group_value::string AS group_value,
-    -- items.value ['interface'] :: STRING AS INTERFACE,
-    -- items.value ['mutable'] :: boolean AS mutable,
-    -- items.value ['ownership'] AS ownership,
-    -- items.value ['royalty'] AS royalty,
     items.value ['supply'] AS supply,
     _inserted_timestamp
 FROM
@@ -47,10 +41,9 @@ select
     metadata:attributes::array as metadata,
     content:links:image::string as image_url,
     content:json_uri::string as metadata_uri,
-    supply:edition_nonce::int as edition,
     metadata:name::string as nft_name,
     _inserted_timestamp,
-    {{ dbt_utils.generate_surrogate_key(['mint']) }} AS nft_metadata_id,
+    {{ dbt_utils.generate_surrogate_key(['mint']) }} AS helius_nft_metadata_id,
     CASE 
         WHEN group_value IS NULL 
         THEN NULL 
