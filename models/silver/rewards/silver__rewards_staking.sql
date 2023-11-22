1836bcd2c9c04acedf4e5851fda0683b2fd4bc3d
tarikceric <tarik@flipsidecrypto.com>
2023-11-22 12:05:35 -0600

    add to incremental, update test

--- models/silver/rewards/silver__rewards_staking.sql
+++ models/silver/rewards/silver__rewards_staking.sql
@@ -4,7 +4,7 @@
     merge_exclude_columns = ["inserted_timestamp"],
     cluster_by = ['block_timestamp::DATE','floor(block_id,-6)','_inserted_timestamp::DATE'],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(stake_pubkey, epoch_earned);",
-    tags = ['rewards']
+    tags = ['rewards', 'scheduled_non_core']
 ) }}
 
 WITH base AS (
