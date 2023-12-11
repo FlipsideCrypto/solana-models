7621a120b184e5a67e64e9005edd4b55e46cc053
tarikceric <tarik@flipsidecrypto.com>
2023-12-11 13:46:43 -0800

    separate _generic logic

--- models/gold/defi/defi__fact_stake_pool_actions.sql
+++ models/gold/defi/defi__fact_stake_pool_actions.sql
@@ -4,20 +4,10 @@
     tags = ['scheduled_non_core']
 ) }}
 
-{% for model_suffix in ["generic","socean","lido","eversol"] %}
+{% for model_suffix in ["socean","lido","eversol"] %}
 
     SELECT
-        CASE
-            WHEN '{{ model_suffix }}' = 'generic' THEN CASE
-                WHEN stake_pool = '7ge2xKsZXmqPxa3YmXxXmzCp9Hc2ezrTxh6PECaxCwrL' THEN 'daopool'
-                WHEN stake_pool = 'CtMyWsrUtAwXWiGr9WjHT5fC3p3fgV8cyGpLTo2LJzG1' THEN 'jpool'
-                WHEN stake_pool = 'stk9ApL5HeVAwPLr3TLhDXdZS8ptVu7zp6ov8HFDuMi' THEN 'blazestake'
-                WHEN stake_pool = 'Jito4APyf642JPZPx3hGc6WWJ8zPKtRbRs4P815Awbb' THEN 'jito'
-                WHEN stake_pool = 'CgnTSoL3DgY9SFHxcLj6CgCgKKoTBr6tp4CPAEWy25DE' THEN 'cogent'
-                WHEN stake_pool = 'LAinEtNLgpmCP9Rvsf5Hn8W6EhNiKLZQti1xfWMLy6X' THEN 'laine'
-            END
-            ELSE '{{ model_suffix }}'
-        END AS stake_pool_name,
+        '{{ model_suffix }}' AS stake_pool_name,
         tx_id,
         block_id,
         block_timestamp,
@@ -28,20 +18,12 @@
         stake_pool,
         amount,
         'SOL' AS token,
-        CASE
-            WHEN '{{ model_suffix }}' = 'generic' THEN COALESCE (
-                'stake_pool_actions_' + '{{ model_suffix }}' + '_id',
-                {{ dbt_utils.generate_surrogate_key(
-                    ['tx_id', 'index', 'inner_index']
-                ) }}
-            )
-            WHEN '{{ model_suffix }}' in ('socean','lido','eversol') THEN COALESCE (
-                'stake_pool_actions_' + '{{ model_suffix }}' + '_id',
-                {{ dbt_utils.generate_surrogate_key(
-                    ['tx_id', 'index']
-                ) }}
-            )
-        END AS fact_stake_pool_actions_id,
+        COALESCE (
+         'stake_pool_actions_' + '{{ model_suffix }}' + '_id',
+            {{ dbt_utils.generate_surrogate_key(
+                ['tx_id','index']
+            ) }}
+        ) AS fact_stake_pool_actions_id,
         COALESCE(
             inserted_timestamp,
             '2000-01-01'
@@ -59,6 +41,44 @@
         UNION ALL
         {% endif %}
     {% endfor %}
+UNION ALL
+SELECT
+    CASE
+        WHEN stake_pool = '7ge2xKsZXmqPxa3YmXxXmzCp9Hc2ezrTxh6PECaxCwrL' THEN 'daopool'
+        WHEN stake_pool = 'CtMyWsrUtAwXWiGr9WjHT5fC3p3fgV8cyGpLTo2LJzG1' THEN 'jpool'
+        WHEN stake_pool = 'stk9ApL5HeVAwPLr3TLhDXdZS8ptVu7zp6ov8HFDuMi' THEN 'blazestake'
+        WHEN stake_pool = 'Jito4APyf642JPZPx3hGc6WWJ8zPKtRbRs4P815Awbb' THEN 'jito'
+        WHEN stake_pool = 'CgnTSoL3DgY9SFHxcLj6CgCgKKoTBr6tp4CPAEWy25DE' THEN 'cogent'
+        WHEN stake_pool = 'LAinEtNLgpmCP9Rvsf5Hn8W6EhNiKLZQti1xfWMLy6X' THEN 'laine'
+    END AS stake_pool_name,
+    tx_id,
+    block_id,
+    block_timestamp,
+    INDEX,
+    succeeded,
+    action,
+    address,
+    stake_pool,
+    amount,
+    'SOL' AS token,
+    COALESCE (
+        stake_pool_actions_generic_id,
+        {{ dbt_utils.generate_surrogate_key(
+            ['tx_id','index', 'inner_index']
+        ) }}
+    ) AS fact_stake_pool_actions_id,
+    COALESCE(
+        inserted_timestamp,
+        '2000-01-01'
+    ) AS inserted_timestamp,
+    COALESCE(
+        modified_timestamp,
+        '2000-01-01'
+    ) AS modified_timestamp
+FROM
+    {{ ref(
+        'silver__stake_pool_actions_generic'
+    ) }}
 UNION ALL
 SELECT
     'marinade' AS stake_pool_name,
