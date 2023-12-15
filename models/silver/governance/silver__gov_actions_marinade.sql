55afc0717734c6253b8c0a3a96b496a0f5fec54e
tarikceric <tarik@flipsidecrypto.com>
2023-12-15 12:11:33 -0800

    test and run intervals

--- models/silver/governance/silver__gov_actions_marinade.sql
+++ models/silver/governance/silver__gov_actions_marinade.sql
@@ -3,7 +3,7 @@
     unique_key = "tx_id",
     incremental_strategy = 'delete+insert',
     cluster_by = ['block_timestamp::DATE'],
-    tags = ['scheduled_non_core']
+    tags = ['daily']
 ) }}
 
 WITH base AS (
--- models/silver/governance/silver__proposal_votes_realms.sql
+++ models/silver/governance/silver__proposal_votes_realms.sql
@@ -3,7 +3,7 @@
   unique_key = "CONCAT_WS('-', tx_id, index)",
   incremental_strategy = 'delete+insert',
   cluster_by = ['block_timestamp::DATE'],
-  tags = ['scheduled_non_core']
+  tags = ['daily']
 ) }}
 
 WITH vote_programs AS (
