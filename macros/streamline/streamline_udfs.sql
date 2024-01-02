f1d351da4d978caed5e5fdc16d38d6f1a8c3b5b0
Desmond Hui <desmond@flipsidecrypto.com>
2024-01-02 10:31:51 -0800

    update with prod endpoints

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -3,7 +3,7 @@
     OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_instructions_decoder(
         json variant
     ) returns text api_integration = aws_solana_api_dev AS {% if target.database == 'SOLANA' -%}
-        'https://7938mznoq8.execute-api.us-east-1.amazonaws.com/dev/udf_bulk_instructions_decoder'
+        'https://l426aqju0g.execute-api.us-east-1.amazonaws.com/prod/udf_bulk_instructions_decoder'
     {% else %}
         'https://7938mznoq8.execute-api.us-east-1.amazonaws.com/dev/udf_bulk_instructions_decoder'
     {%- endif %}
@@ -12,7 +12,7 @@
 {% macro create_udf_verify_idl() %}
     CREATE
     OR REPLACE EXTERNAL FUNCTION streamline.udf_verify_idl("JSON" ARRAY) returns VARIANT api_integration = aws_solana_api_dev AS {% if target.database == 'SOLANA' -%}
-        'https://7938mznoq8.execute-api.us-east-1.amazonaws.com/dev/verify_idl'
+        'https://l426aqju0g.execute-api.us-east-1.amazonaws.com/prod/verify_idl'
     {% else %}
         'https://7938mznoq8.execute-api.us-east-1.amazonaws.com/dev/verify_idl'
     {%- endif %}
