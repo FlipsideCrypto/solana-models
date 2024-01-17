8df42e9d2e35c58930dcfb7763fe672003213ade
tarikceric <tarik@flipsidecrypto.com>
2024-01-17 15:27:34 -0800

    add incremental logic

--- models/silver/idls/silver__idl.sql
+++ models/silver/idls/silver__idl.sql
@@ -1,6 +1,7 @@
 {{ config(
     materialized = 'incremental',
     unique_key = "program_id",
+    merge_exclude_columns = ["inserted_timestamp"],
     tags = ['scheduled_non_core']
 ) }}
 
@@ -11,22 +12,33 @@ WITH idls AS (
         program_id
     FROM
         {{ ref('silver__decoded_instructions') }}
-    GROUP BY
-        program_id
+
+{% if is_incremental() %}
+WHERE
+    _inserted_timestamp >= SYSDATE() - INTERVAL '2 hours'
+{% endif %}
+GROUP BY
+    program_id
 ),
 pre_final AS (
     SELECT
         A.earliest_decoded_block,
         A.program_id,
-        C.idl,
-        C.idl_hash
+        b.idl,
+        b.idl_hash
     FROM
         idls A
-        LEFT JOIN {{ ref('streamline__idls_history') }}
+        INNER JOIN {{ ref('silver__verified_idls') }}
         b
         ON A.program_id = b.program_id
-        INNER JOIN {{ ref('silver__verified_idls') }} C
-        ON A.program_id = C.program_id
+
+{% if is_incremental() %}
+LEFT JOIN {{ this }} C
+ON A.program_id = C.program_id
+WHERE
+    A.earliest_decoded_block < C.earliest_decoded_block
+    OR C.earliest_decoded_block IS NULL
+{% endif %}
 )
 SELECT
     *,
