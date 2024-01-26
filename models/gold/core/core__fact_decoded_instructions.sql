fc2615c3b7ef1486088f927fb9fa2ac8dfbf5581
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-01-26 14:11:33 -0500

    simplify

--- models/gold/core/core__fact_decoded_instructions.sql
+++ models/gold/core/core__fact_decoded_instructions.sql
@@ -7,9 +7,11 @@ SELECT
     block_timestamp,
     block_id,
     tx_id,
+    signers,
     INDEX,
     inner_index,
     program_id,
+    event_type,
     decoded_instruction,
     COALESCE (
         decoded_instructions_id,
