bf636479e54bde9d2003f699be5d2f37428f4473
Desmond Hui <desmond@flipsidecrypto.com>
2024-01-26 06:43:32 -0800

    fix

--- models/silver/parser/silver__decoded_instructions.sql
+++ models/silver/parser/silver__decoded_instructions.sql
@@ -61,8 +61,8 @@ ON A.block_id = b.block_id
 {% if is_incremental() %}
 WHERE
     A._inserted_timestamp >= '{{ max_inserted_timestamp }}'
--- AND 
-    -- A._partition_by_created_date_hour between dateadd('hour', -2, date_trunc('hour','{{ max_inserted_timestamp }}'::timestamp_ntz)) and dateadd('hour',1,date_trunc('hour','{{ max_inserted_timestamp }}'::timestamp_ntz))
+AND 
+    A._partition_by_created_date_hour between dateadd('hour', -2, date_trunc('hour','{{ max_inserted_timestamp }}'::timestamp_ntz)) and dateadd('hour',1,date_trunc('hour','{{ max_inserted_timestamp }}'::timestamp_ntz))
 {% endif %}
 
 qualify(ROW_NUMBER() over (PARTITION BY tx_id, INDEX, coalesce(inner_index,-1)
