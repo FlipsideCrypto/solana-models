137fa2eb2aea265813407100b80aafa1b23d22c3
eric-laurello <102970824+eric-laurello@users.noreply.github.com>
2024-01-31 10:13:52 -0500

    testing second model for decoded instruc (#472)
    * initial
    
    * block range macro and model work
    
    * fix incr
    
    * drop _2 update workflow
    
    * delete the right file
    
    ---------
    
    Co-authored-by: Desmond Hui <desmond@flipsidecrypto.com>

---
+++ macros/helpers/dynamic_block_date_ranges.sql
@@ -0,0 +1,102 @@
+{% macro dynamic_block_date_ranges(source) -%}
+    {% set get_limits_query %}
+        WITH full_range AS (
+            SELECT
+                MIN(
+                    block_timestamp :: DATE
+                ) AS full_range_start_block_date,
+                MAX(
+                    block_timestamp :: DATE
+                ) AS full_range_end_block_date
+            FROM
+                {{ source }}
+        ),
+        block_range AS (
+            SELECT
+                date_day,
+                ROW_NUMBER() over (
+                    ORDER BY
+                        date_day
+                ) - 1 AS rn
+            FROM
+                crosschain.core.dim_dates
+            WHERE
+                date_day BETWEEN (
+                    SELECT
+                        full_range_start_block_date
+                    FROM
+                        full_range
+                )
+                AND (
+                    SELECT
+                        full_range_end_block_date
+                    FROM
+                        full_range
+                )
+        ),
+        partition_block_counts AS (
+            SELECT
+                b.date_day AS block_date,
+                COUNT(*) AS count_in_window
+            FROM
+                block_range b
+                LEFT OUTER JOIN {{ source }}
+                r
+                ON r.block_timestamp :: DATE = b.date_day
+            GROUP BY
+                1
+        ),
+        range_groupings AS (
+            SELECT
+                block_date,
+                count_in_window,
+                conditional_change_event(
+                    count_in_window > 1
+                ) over (
+                    ORDER BY
+                        block_date
+                ) AS group_val
+            FROM
+                partition_block_counts
+        ),
+        contiguous_ranges AS (
+            SELECT
+                MIN(block_date) AS start_block_date,
+                MAX(block_date) AS end_block_date
+            FROM
+                range_groupings
+            WHERE
+                count_in_window > 1
+            GROUP BY
+                group_val
+        ),
+        between_stmts AS (
+            SELECT
+                CONCAT(
+                    'block_timestamp::date between \'',
+                    start_block_date,
+                    '\' and \'',
+                    end_block_date,
+                    '\''
+                ) AS b
+            FROM
+                contiguous_ranges
+        )
+        SELECT
+            CONCAT('(', LISTAGG(b, ' OR '), ')')
+        FROM
+            between_stmts 
+    {% endset %}
+    
+    {% set between_stmts = run_query(get_limits_query).columns[0].values()[0] %}
+
+    {% if between_stmts != '()' %}
+        /* in case empty update array */
+        {% set predicate_override = between_stmts %}
+    {% else %}
+        {% set predicate_override = '1=1' %}
+        /* need to have something or it will error since 'dynamic_block_date_ranges' is not a column */
+    {% endif %}
+
+    {{ return(predicate_override) }}
+{% endmacro %}
---
+++ models/silver/parser/silver__decoded_instructions_combined.sql
@@ -0,0 +1,68 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_predicates = ["dynamic_block_date_ranges"],
+    unique_key = "decoded_instructions_combined_id",
+    cluster_by = ['program_id','block_timestamp::DATE','_inserted_timestamp::DATE'],
+    post_hook = enable_search_optimization(
+        '{{this.schema}}',
+        '{{this.identifier}}'
+    ),
+    merge_exclude_columns = ["inserted_timestamp"],
+) }}
+-- depends_on: {{ ref('silver__decoded_instructions') }}
+/* run incremental timestamp value first then use it as a static value */
+{% if execute %}
+
+    {% if is_incremental() %}
+        {% set max_inserted_query %}
+        SELECT
+            MAX(_inserted_timestamp) AS _inserted_timestamp
+        FROM
+            {{ this }}
+
+        {% endset %}
+        {% set max_inserted_timestamp = run_query(max_inserted_query).columns [0].values() [0] %}
+    {% endif %}
+
+    {% set query = """ CREATE OR REPLACE TEMPORARY TABLE silver.decoded_instructions__intermediate_tmp AS SELECT block_timestamp, block_id, tx_id, inner_index, program_id, decoded_instruction, decoded_instructions_id, _inserted_timestamp FROM """ ~ ref('silver__decoded_instructions') ~ """ WHERE block_timestamp::date >= '2024-01-28'""" %}
+    {% set incr = "" %}
+    {% if is_incremental() %}
+        {% set incr = """ AND _inserted_timestamp >= '""" ~ max_inserted_timestamp ~ """' """ %}
+    {% endif %}
+
+    {% do run_query(query ~ incr) %}
+
+    {% set between_stmts = dynamic_block_date_ranges("silver.decoded_instructions__intermediate_tmp") %}
+{% endif %}
+
+
+WITH txs AS (
+    SELECT
+        block_timestamp,
+        tx_id,
+        signers, 
+        succeeded
+    FROM
+        {{ ref('silver__transactions') }}
+    WHERE
+        {{ between_stmts }}
+)
+SELECT
+    A.block_timestamp,
+    A.block_id,
+    A.tx_id,
+    A.inner_index,
+    b.signers,
+    b.succeeded,
+    A.program_id,
+    A.decoded_instruction,
+    A._inserted_timestamp,
+    decoded_instructions_id AS decoded_instructions_combined_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+    silver.decoded_instructions__intermediate_tmp A
+    JOIN txs b
+    ON A.tx_id = b.tx_id
+    AND A.block_timestamp :: DATE = b.block_timestamp :: DATE
