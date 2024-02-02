095bfec313117694ec97a3d495a7d532f2ad588e
Desmond Hui <desmond@flipsidecrypto.com>
2024-02-02 08:05:54 -0800

    move to new folder

---
+++ models/streamline/decode_instructions/streamline__idls_history.sql
@@ -0,0 +1,48 @@
+{{ config (
+    materialized = "incremental",
+    unique_key = 'program_id',
+    tags = ['streamline'],
+) }}
+
+WITH idls AS (
+
+    SELECT
+        program_id
+    FROM
+        {{ ref('silver__verified_idls') }}
+
+{% if is_incremental() %}
+WHERE
+    program_id NOT IN (
+        SELECT
+            program_id
+        FROM
+            {{ this }}
+    )
+{% endif %}
+),
+event_history AS (
+    SELECT
+        program_id,
+        MIN(block_id) AS first_block_id,
+        MAX(block_timestamp) AS default_backfill_start_block_timestamp,
+        MAX(block_id) AS default_backfill_start_block_id
+    FROM
+        {{ ref('silver__events') }}
+    WHERE
+        program_id IN (
+            SELECT
+                program_id
+            FROM
+                idls
+        )
+    GROUP BY
+        program_id
+)
+SELECT
+    program_id,
+    first_block_id,
+    default_backfill_start_block_timestamp,
+    default_backfill_start_block_id
+FROM
+    event_history
