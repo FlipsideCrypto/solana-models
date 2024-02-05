e9500c7d612acfab5236d68c9657ab8331a4d426
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2024-02-05 08:59:31 -0800

    revert patch fix, force udf max batch rows, decrease batch size (#477)
    * revert patch fix, force udf max batch rows, decrease batch size
    
    * remove dev ref

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -20,7 +20,11 @@
 
 {% macro create_udf_decode_compressed_mint_change_logs() %}
     CREATE
-    OR REPLACE EXTERNAL FUNCTION streamline.udf_decode_compressed_mint_change_logs("JSON" ARRAY) returns VARIANT api_integration = aws_solana_api_dev AS {% if target.database == 'SOLANA' -%}
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_decode_compressed_mint_change_logs("JSON" ARRAY) 
+    returns VARIANT 
+    api_integration = aws_solana_api_dev 
+    max_batch_rows = 1
+    AS {% if target.database == 'SOLANA' -%}
         'https://l426aqju0g.execute-api.us-east-1.amazonaws.com/prod/udf_decode_compressed_mint_change_logs'
     {% else %}
         'https://7938mznoq8.execute-api.us-east-1.amazonaws.com/dev/udf_decode_compressed_mint_change_logs'
