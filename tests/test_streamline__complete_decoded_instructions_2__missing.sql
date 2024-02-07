bced0246b3a09fd0f858f8ec63a0eff7fa9011f7
Desmond Hui <desmond@flipsidecrypto.com>
2024-02-07 09:43:10 -0800

    add hourly test to check missing completed

---
+++ tests/test_streamline__complete_decoded_instructions_2__missing.sql
@@ -0,0 +1,17 @@
+SELECT
+    MD5(
+        CAST(
+            COALESCE(CAST(block_id AS text), '_dbt_utils_surrogate_key_null_') || '-' || COALESCE(CAST(tx_id AS text), '_dbt_utils_surrogate_key_null_') || '-' || COALESCE(CAST(INDEX AS text), '_dbt_utils_surrogate_key_null_') || '-' || COALESCE(CAST(inner_index AS text), '_dbt_utils_surrogate_key_null_') || '-' || COALESCE(CAST(program_id AS text), '_dbt_utils_surrogate_key_null_') AS text
+        )
+    ) AS complete_decoded_instructions_2_id
+FROM
+    {{ target.database }}.bronze.streamline_decoded_instructions_2
+WHERE
+    _partition_by_created_date_hour BETWEEN dateadd('hour',-3,date_trunc('hour',current_timestamp())) and dateadd('hour',-2, date_trunc('hour',current_timestamp()))
+EXCEPT
+SELECT
+    complete_decoded_instructions_2_id
+FROM
+    {{ ref('streamline__complete_decoded_instructions_2') }}
+WHERE
+    _inserted_timestamp BETWEEN dateadd('hour',-3,date_trunc('hour',current_timestamp())) and dateadd('hour',-2, date_trunc('hour',current_timestamp()));
