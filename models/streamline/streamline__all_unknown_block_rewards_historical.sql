50691dfc3683dcc0f7dc9f717e22463d4e2893cc
tarikceric <46071768+tarikceric@users.noreply.github.com>
2024-02-14 14:01:06 -0800

    update rewards backfill (#486)

--- models/streamline/streamline__all_unknown_block_rewards_historical.sql
+++ models/streamline/streamline__all_unknown_block_rewards_historical.sql
@@ -6,17 +6,19 @@
 WITH pre_final AS (
 
     SELECT
-        SEQ8()+148693779 AS block_id
+        SEQ8()+99360012 AS block_id
     FROM
         TABLE(GENERATOR(rowcount => 80000000))
     WHERE
-        block_id >= 148693779
-        AND block_id <= 226117675
+        block_id >= 99360012
+        AND block_id <= 163728008
     EXCEPT
     SELECT
         block_id
     FROM
         {{ ref('streamline__complete_block_rewards') }}
+    WHERE 
+        _partition_id > 38754
 )
 SELECT
     block_id,
