41f0417890d25b36b35ca2fa220585112c438dfa
tarikceric <46071768+tarikceric@users.noreply.github.com>
2024-02-19 09:13:11 -0800

    An 4449/staking tables update (#487)
    * update jito staking
    
    * update token

--- models/gold/defi/defi__fact_stake_pool_actions.sql
+++ models/gold/defi/defi__fact_stake_pool_actions.sql
@@ -69,7 +69,6 @@ SELECT
         WHEN stake_pool = '7ge2xKsZXmqPxa3YmXxXmzCp9Hc2ezrTxh6PECaxCwrL' THEN 'daopool'
         WHEN stake_pool = 'CtMyWsrUtAwXWiGr9WjHT5fC3p3fgV8cyGpLTo2LJzG1' THEN 'jpool'
         WHEN stake_pool = 'stk9ApL5HeVAwPLr3TLhDXdZS8ptVu7zp6ov8HFDuMi' THEN 'blazestake'
-        WHEN stake_pool = 'Jito4APyf642JPZPx3hGc6WWJ8zPKtRbRs4P815Awbb' THEN 'jito'
         WHEN stake_pool = 'CgnTSoL3DgY9SFHxcLj6CgCgKKoTBr6tp4CPAEWy25DE' THEN 'cogent'
         WHEN stake_pool = 'LAinEtNLgpmCP9Rvsf5Hn8W6EhNiKLZQti1xfWMLy6X' THEN 'laine'
     END AS stake_pool_name,
@@ -131,4 +130,24 @@ SELECT
 FROM
     {{ ref(
         'silver__stake_pool_actions_marinade'
+    ) }}
+UNION ALL
+SELECT
+    'jito' AS stake_pool_name,
+    tx_id,
+    block_id,
+    block_timestamp,
+    INDEX,
+    succeeded,
+    action,
+    address,
+    stake_pool,
+    amount,
+    'SOL' AS token,
+    stake_pool_actions_jito_id AS fact_stake_pool_actions_id,
+    inserted_timestamp,
+     modified_timestamp
+FROM
+    {{ ref(
+        'silver__stake_pool_actions_jito'
     ) }}
