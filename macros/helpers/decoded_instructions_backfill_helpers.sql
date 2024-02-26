8db3020f4071cecb649e010b75dd9404a82fa54d
tarikceric <tarik@flipsidecrypto.com>
2024-02-26 15:19:50 -0800

    update adhoc phoenix

--- macros/helpers/decoded_instructions_backfill_helpers.sql
+++ macros/helpers/decoded_instructions_backfill_helpers.sql
@@ -191,7 +191,8 @@
     {% endfor %}
 {% endmacro %}
 
-{% macro adhoc_phoenix_decoded_instructions_backfill_generate_views(program_id) %}
+{% macro old_adhoc_phoenix_decoded_instructions_backfill_generate_views(program_id) %}
+-- old version - see new below
 
     {% set min_block_id = 243913016 | int %}
     {% set max_block_id = 248213854 | int %}
@@ -263,6 +264,99 @@
         select 
             *
         from retry_events""" %}
+
+        {% do run_query(query) %}
+    {% endfor %}
+{% endmacro %}
+
+{% macro adhoc_phoenix_decoded_instructions_backfill_generate_views(program_id) %}
+
+    {% set min_block_id = 243913016 | int %}
+    {% set max_block_id = 248213854 | int %}
+    {% set step = 250000 %}
+
+    {% for i in range(min_block_id, max_block_id, step) %}
+        {% if i == min_block_id %}
+            {% set start_block = i %}
+        {% else %}
+            {% set start_block = i+1 %}
+        {% endif %}
+
+        {% if i+step >= max_block_id %}
+            {% set end_block = max_block_id %}
+        {% else %}
+            {% set end_block = i+step %}
+        {% endif %}
+        {% set query %}
+        create or replace view streamline.decoded_instructions_backfill_{{ start_block }}_{{ end_block }}_retry_{{ program_id }} AS 
+       with retries as (
+            select *
+            from {{ ref('silver__decoded_instructions_combined') }}
+            where program_id = 'PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY'
+            and block_id between {{ start_block }} and {{ end_block }}
+            and event_type is null      
+        ),
+        retry_events as (
+            select 
+                e.block_id,
+                e.tx_id,
+                e.index,
+                NULL as inner_index,
+                e.instruction,
+                e.program_id,
+                e.block_timestamp,
+                {{ dbt_utils.generate_surrogate_key(['e.block_id','e.tx_id','e.index','inner_index','e.program_id']) }} as id
+            from retries r
+            join {{ ref('silver__events') }} e 
+                on r.block_id = e.block_id 
+                and r.tx_id = e.tx_id 
+                and r.index = e.index 
+            where e.program_id = 'PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY'
+            and e.block_id between {{ start_block }} and {{ end_block }}
+            and e.succeeded
+            and r.inner_index is null
+            union all
+            select
+                e.block_id,
+                e.tx_id,
+                e.index,
+                i.index as inner_index,
+                i.value as instruction, 
+                i.value :programId :: STRING AS inner_program_id,
+                e.block_timestamp,
+                {{ dbt_utils.generate_surrogate_key(['e.block_id','e.tx_id','e.index','inner_index','inner_program_id']) }} as id
+            from retries r
+            join {{ ref('silver__events') }} e  
+                on r.block_id = e.block_id 
+                and r.tx_id = e.tx_id 
+                and r.index = e.index 
+            join table(flatten(inner_instruction:instructions)) i
+            where array_contains('PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY'::variant, inner_instruction_program_ids)
+            and inner_program_id = 'PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY'
+            and e.block_id between {{ start_block }} and {{ end_block }}
+            and e.succeeded
+            and r.inner_index is not null
+            and r.inner_index = i.index
+        ),
+        completed_subset as (
+                select 
+                    *
+                from 
+                    {{ ref('streamline__complete_decoded_instructions_2') }}
+                where 
+                    program_id = '{{ program_id }}'
+                and 
+                    _inserted_timestamp >= '{{ retry_start_timestamp }}'
+            )
+            select 
+                e.*
+            from retry_events e 
+            left outer join completed_subset c 
+                    on c.complete_decoded_instructions_2_id = e.id
+            where 
+                c.complete_decoded_instructions_2_id is null
+            ;
+        {% endset %}
 
         {% do run_query(query) %}
     {% endfor %}
