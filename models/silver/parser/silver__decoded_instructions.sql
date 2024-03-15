7630ce8037629e755c67d66c85b907d22d6b3c83
Desmond Hui <desmond@flipsidecrypto.com>
2024-03-15 06:33:15 -0700

    remove upper bound

--- models/silver/parser/silver__decoded_instructions.sql
+++ models/silver/parser/silver__decoded_instructions.sql
@@ -61,7 +61,7 @@ ON A.block_id = b.block_id
 WHERE
     A._inserted_timestamp >= dateadd('minute', -5, '{{ max_inserted_timestamp }}')
 AND 
-    A._partition_by_created_date_hour between dateadd('hour', -2, date_trunc('hour','{{ max_inserted_timestamp }}'::timestamp_ntz)) and dateadd('hour',1,date_trunc('hour','{{ max_inserted_timestamp }}'::timestamp_ntz))
+    A._partition_by_created_date_hour between dateadd('hour', -2, date_trunc('hour','{{ max_inserted_timestamp }}'::timestamp_ntz))
 {% endif %}
 
 qualify(ROW_NUMBER() over (PARTITION BY tx_id, INDEX, coalesce(inner_index,-1)
