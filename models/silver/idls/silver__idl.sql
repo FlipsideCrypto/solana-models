f67b508a8f90ae89328f133d64636f2fb89b18d6
tarikceric <tarik@flipsidecrypto.com>
2024-04-16 14:17:08 -0700

    add union for idl_hash change

--- models/silver/idls/silver__idl.sql
+++ models/silver/idls/silver__idl.sql
@@ -47,11 +47,10 @@ idls_in_progress AS (
             ARRAY_SIZE(SPLIT(table_name, '_'))
         ) AS in_progress_program_id
     FROM
-        solana_dev.information_schema.views
+        information_schema.views
     WHERE
         table_name LIKE 'DECODED_INSTRUCTIONS_BACKFILL_%'
-    ORDER BY
-        table_name DESC
+        AND table_name NOT LIKE '%RETRY%'
 ),
 pre_final AS (
     SELECT
@@ -76,9 +75,31 @@ LEFT JOIN {{ this }}
 d
 ON A.program_id = d.program_id
 WHERE
-    b.earliest_decoded_block < d.earliest_decoded_block
-    OR d.earliest_decoded_block IS NULL
-{% endif %}
+    IFF(d.earliest_decoded_block IS NULL, TRUE, b.earliest_decoded_block <= d.earliest_decoded_block)
+UNION
+    SELECT
+        A.program_id,
+        A.idl,
+        A.idl_hash,
+        A.is_valid,
+        A.discord_username,
+        A._inserted_timestamp,
+        A.first_block_id,
+        d.earliest_decoded_block,
+        C.in_progress_program_id
+    FROM
+        submitted_idls A
+        LEFT JOIN idl_decoded_history b
+        ON A.program_id = b.program_id
+        LEFT JOIN idls_in_progress C
+        ON A.program_id = C.in_progress_program_id
+    LEFT JOIN {{ this }}
+    d
+    ON A.program_id = d.program_id
+    WHERE
+        A.idl_hash <> D.idl_hash
+        OR c.in_progress_program_id IS NOT NULL
+    {% endif %}
 )
 SELECT
     program_id,
@@ -90,10 +111,10 @@ SELECT
     first_block_id,
     earliest_decoded_block,
     CASE
-        WHEN earliest_decoded_block = first_block_id THEN 'complete'
-        WHEN in_progress_program_id IS NOT NULL THEN 'in progress'
+        WHEN earliest_decoded_block = first_block_id AND in_progress_program_id IS NOT NULL THEN 'complete' 
+        WHEN in_progress_program_id IS NOT NULL THEN 'in_progress'
         WHEN NOT is_valid THEN NULL
-        ELSE 'not started'
+        ELSE 'not_started'
     END AS backfill_status,
     {{ dbt_utils.generate_surrogate_key(['program_id']) }} AS idl_id,
     SYSDATE() AS inserted_timestamp,
