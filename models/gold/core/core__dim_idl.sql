299e3d08372cbfb0fd7e6b10aa4f83f4bf5a97ae
tarikceric <tarik@flipsidecrypto.com>
2024-04-16 08:03:22 -0700

    update idl table

--- models/gold/core/core__dim_idl.sql
+++ models/gold/core/core__dim_idl.sql
@@ -8,7 +8,12 @@ SELECT
     program_id,
     idl,
     idl_hash,
+    is_valid,
+    submitted_by,
+    _inserted_timestamp,
+    first_block_id,
     earliest_decoded_block,
+    backfill_status,
     idl_id as dim_idl_id,
     inserted_timestamp,
     modified_timestamp
