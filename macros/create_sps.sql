fe092eea99af65423b74516a4eee35d783e5328e
Desmond Hui <desmond@flipsidecrypto.com>
2024-04-24 07:28:14 -0700

    hotfix macro to compile only on execution

--- macros/create_sps.sql
+++ macros/create_sps.sql
@@ -8,7 +8,9 @@
 {% endmacro %}
 
 {% macro enable_search_optimization(schema_name, table_name, condition = '') %}
-    {% if target.database == 'SOLANA' %}
-        ALTER TABLE {{ schema_name }}.{{ table_name }} ADD SEARCH OPTIMIZATION {{ condition }}
+    {% if execute %}
+        {% if target.database == 'SOLANA' %}
+            ALTER TABLE {{ schema_name }}.{{ table_name }} ADD SEARCH OPTIMIZATION {{ condition }}
+        {% endif %}
     {% endif %}
 {% endmacro %}
