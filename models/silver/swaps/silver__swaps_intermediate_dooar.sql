9db5cea171f74d60a167e5b6637df4472226db70
Desmond Hui <desmond@flipsidecrypto.com>
2024-05-03 07:59:58 -0700

    revert temp change, add back to scheduler

--- models/silver/swaps/silver__swaps_intermediate_dooar.sql
+++ models/silver/swaps/silver__swaps_intermediate_dooar.sql
@@ -5,6 +5,7 @@
     incremental_predicates = ["dynamic_range_predicate", "block_timestamp::date"],
     merge_exclude_columns = ["inserted_timestamp"],
     cluster_by = ['block_timestamp::DATE','_inserted_timestamp::DATE'],
+    tags = ['scheduled_non_core']
 ) }}
 
 {% if execute %}
