d579c28d300bf640f2711a45441ec65f41c79234
desmond-hui <desmond@flipsidecrypto.com>
2024-05-31 09:17:20 -0700

    cleanup and formatting, remove hardcoded value

--- models/silver/accounts/silver__token_account_owners_2.sql
+++ models/silver/accounts/silver__token_account_owners_2.sql
@@ -1,7 +1,7 @@
 {{ config(
     materialized = 'incremental',
     incremental_strategy = "delete+insert",
-    incremental_predicates = ['dynamic_range_predicate', 'start_block_id', generate_view_name(this) ~ ".start_block_id >= " ~ generate_tmp_view_name(this) ~ ".start_block_id"],
+    incremental_predicates = ['min_value_predicate', 'start_block_id', generate_view_name(this) ~ ".start_block_id >= " ~ generate_tmp_view_name(this) ~ ".start_block_id"],
     unique_key = ["account_address"],
     cluster_by = ["round(start_block_id,-5)"],
     post_hook = enable_search_optimization('{{this.schema}}','{{this.identifier}}','ON EQUALITY(account_address)'),
@@ -16,8 +16,7 @@ WITH new_events AS (
         start_block_id,
         _inserted_timestamp,
     FROM
-        solana.silver.token_account_owners_intermediate
-        /*{{ ref('silver__token_account_owners_intermediate') }}*/
+        {{ ref('silver__token_account_owners_intermediate') }}
     WHERE
     {% if is_incremental() %}
         _inserted_timestamp > (
@@ -37,13 +36,6 @@ WITH new_events AS (
         _inserted_timestamp :: DATE = '2022-09-01'
     {% endif %}
         AND start_block_id <> coalesce(end_block_id,-1)
-        -- AND account_address IN ('38wX3iF3twRED4E2eyvGDQpxrEyK7ZEpjK53AcnYDa2k',
-        -- 'Ff7fDF545jffUYiU3ReAzzP4VfBCAh1yfwThjBz4UxcB',
-        -- '9HBBbt3PSGdfeGfbTv6XHXqBUv5jk3cfz4kFFn8vJGFL',
-        -- '4geDNAvCnE1tLogZnfLYkkbwSENfqxfWh2Cx79Nct9eq',
-        -- '9Lf8YK6H28g5Rut3XjPSn5kQRwGqcUYRK5FjThvPxLrj',
-        -- 'ArhaTaKiMCiBMwGSxCKMy8V6guioB7N1c8R8B3HGb7U3',
-        -- '5Afw8nCxWmPHXbxYNZzSUUWjQiaKe1mPGbNyYYi8U1vN')
 ),
 
 {% if is_incremental() %}
@@ -63,24 +55,12 @@ events_to_reprocess AS (
         C.start_block_id,
         C._inserted_timestamp,
     FROM
-        solana.silver.token_account_owners_intermediate C
-        /*{{ ref('silver__token_account_owners_intermediate') }}*/
+        {{ ref('silver__token_account_owners_intermediate') }} C
     JOIN 
         distinct_states d
         USING(account_address)
     WHERE
-        /*(
-            C.end_block_id >= d.min_block_id
-            OR 
-            C.end_block_id IS NULL 
-        )
-        AND start_block_id <> coalesce(end_block_id,-1)
-        AND _inserted_timestamp < (SELECT max(_inserted_timestamp) FROM new_events)*/
-        (
-            C.start_block_id >= d.min_block_id
-            -- OR 
-            -- C.end_block_id IS NULL 
-        )
+        C.start_block_id >= d.min_block_id
         AND start_block_id <> coalesce(end_block_id,-1)
         AND _inserted_timestamp <= (SELECT max(_inserted_timestamp) FROM new_events)
 ),
@@ -130,17 +110,21 @@ changed_states AS (
     FROM 
         all_states 
     QUALIFY 
-        coalesce(lag(owner) over (partition by account_address order by start_block_id),'abc') <> owner
+        coalesce(lag(owner) OVER (PARTITION BY account_address ORDER BY start_block_id),'abc') <> owner
 )
 SELECT
     account_address,
     owner,
     start_block_id,
-    LEAD(start_block_id) over (
+    LEAD(start_block_id) OVER (
         PARTITION BY account_address
         ORDER BY
             start_block_id
     ) AS end_block_id,
     _inserted_timestamp,
+    {{ dbt_utils.generate_surrogate_key(['account_address','start_block_id']) }} AS token_account_owners_id,
+    sysdate() AS inserted_timestamp,
+    sysdate() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
 FROM
     changed_states
