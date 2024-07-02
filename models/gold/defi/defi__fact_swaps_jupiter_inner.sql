1d40ba1e55236b3947a40d0097d729d943162aa5
desmond-hui <desmond@flipsidecrypto.com>
2024-07-02 07:00:08 -0700

    wip

---
+++ models/gold/defi/defi__fact_swaps_jupiter_inner.sql
@@ -0,0 +1,26 @@
+{{ config(
+    materialized = 'view',
+    meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'SWAPS' }}},
+    tags = ['scheduled_non_core']
+) }}
+
+SELECT
+    block_timestamp,
+    block_id,
+    tx_id,
+    index,
+    inner_index,
+    swap_index,
+    succeeded,
+    swapper,
+    from_mint,
+    from_amount,
+    to_mint,
+    to_amount,
+    swap_program_id,
+    aggregator_program_id,
+    swaps_inner_intermediate_jupiterv6_id AS fact_swaps_jupiter_inner_id,
+    inserted_timestamp,
+    modified_timestamp,
+FROM
+    {{ ref('silver__swaps_inner_intermediate_jupiterv6') }}
