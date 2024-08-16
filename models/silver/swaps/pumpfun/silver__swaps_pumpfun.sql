99fec66cdb76ca9de72946cd368bf9f73c4377fd
tarikceric <tarik@flipsidecrypto.com>
2024-08-16 09:51:05 -0500

    silver update and add to gold

--- models/silver/swaps/pumpfun/silver__swaps_pumpfun.sql
+++ models/silver/swaps/pumpfun/silver__swaps_pumpfun.sql
@@ -49,7 +49,7 @@ WITH base AS (
     SELECT 
         *
     FROM
-        silver.swaps_inner_intermediate_baseswap__intermediate_tmp
+        silver.swaps_pumpfun__intermediate_tmp
 ),
 token_decimals AS (
     SELECT 
@@ -98,24 +98,32 @@ case when is_buy
     end as to_amount
     ,
     _inserted_timestamp
-from base)
+from base),
 
+pre_final as (
 select
     a.block_timestamp,
     a.block_id,
+    a.program_id,
     a.tx_id,
+    a.succeeded,
+    ROW_NUMBER() over (
+        PARTITION BY a.tx_id
+        ORDER BY
+            a.INDEX,
+            a.inner_index
+    ) AS swap_index,
     a.index,
     a.inner_index,
-    a.succeeded,
-    a.program_id,
     a.swapper,
-    a.mint,
     a.from_mint,
     a.to_mint,
+    a.from_amount as from_amount_int,
     a.from_amount * pow(10,-d.decimal) AS from_amount,
+    a.to_amount as to_amount_int,
     a.to_amount * pow(10,-d2.decimal) AS to_amount,
-    _inserted_timestamp
-    {{ dbt_utils.generate_surrogate_key(['b.tx_id','b.index','b.inner_index']) }} as swaps_pumpfun_id,
+    a._inserted_timestamp,
+    {{ dbt_utils.generate_surrogate_key(['a.tx_id','a.index','a.inner_index']) }} as swaps_inner_intermediate_jupiterv6_id,
     sysdate() as inserted_timestamp,
     sysdate() as modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
@@ -126,5 +134,89 @@ LEFT OUTER JOIN
     ON a.from_mint = d.mint
 LEFT OUTER JOIN
     token_decimals d2
-    ON a.to_mint = d2.mint;
-
+    ON a.to_mint = d2.mint
+)
+,
+distinct_missing_decimals AS (
+    SELECT DISTINCT
+        to_mint AS mint
+    FROM
+        pre_final
+    WHERE
+        to_amount IS NULL
+    UNION
+    SELECT DISTINCT
+        from_mint
+    FROM
+        pre_final
+    WHERE
+        from_amount IS NULL
+),
+get_missing_decimals AS (
+    SELECT
+        mint,
+        solana.live.udf_api(
+            'POST',
+            '{service}/{Authentication}',
+            OBJECT_CONSTRUCT(
+                'Content-Type',
+                'application/json'
+            ),
+            OBJECT_CONSTRUCT(
+                'id',
+                1,
+                'jsonrpc',
+                '2.0',
+                'method',
+                'getAccountInfo',
+                'params',
+                ARRAY_CONSTRUCT(
+                    mint,
+                    OBJECT_CONSTRUCT(
+                        'encoding',
+                        'jsonParsed'
+                    )
+                )
+            ),
+            'Vault/prod/solana/quicknode/mainnet'
+        ):data:result:value:data:parsed:info:decimals::int AS decimal
+    FROM
+        distinct_missing_decimals
+)
+SELECT
+    block_timestamp,
+    block_id,
+    tx_id,
+    index,
+    inner_index,
+    swap_index,
+    succeeded,
+    program_id,
+    swapper,
+    from_mint,
+    CASE
+        WHEN from_amount IS NULL THEN
+            from_amount_int * pow(10, -d.decimal)
+        ELSE
+            from_amount
+    END AS from_amount,
+    to_mint,
+    CASE
+        WHEN to_amount IS NULL THEN
+            to_amount_int * pow(10, -d2.decimal)
+        ELSE
+            to_amount
+    END AS to_amount,
+    _inserted_timestamp,
+    swaps_inner_intermediate_jupiterv6_id,
+    inserted_timestamp,
+    modified_timestamp,
+    _invocation_id
+FROM
+    pre_final
+LEFT OUTER JOIN
+    get_missing_decimals d
+    ON from_mint = d.mint
+LEFT OUTER JOIN
+    get_missing_decimals d2
+    ON to_mint = d2.mint
