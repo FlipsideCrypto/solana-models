854a6bc8707c03070d31c9b2deb7f25a2a624c62
desmond-hui <desmond@flipsidecrypto.com>
2024-08-16 09:49:28 -0700

    make and use v2 streamline external table macro

--- models/bronze/streamline/bronze__streamline_block_rewards.sql
+++ models/bronze/streamline/bronze__streamline_block_rewards.sql
@@ -3,7 +3,7 @@
 ) }}
 
 {% set model = "block_rewards" %}
-{{ streamline_external_table_query(
+{{ streamline_external_table_query_v2(
     model,
     partition_function = "to_number(split_part(split_part(split_part(file_name,'/',3),'=',2),'_',1))",
     partition_name = "_partition_id",
