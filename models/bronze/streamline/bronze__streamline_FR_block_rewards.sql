cba05a9adbc3966a1a7ddac0deac98dc48a36105
desmond-hui <desmond@flipsidecrypto.com>
2024-08-16 08:56:40 -0700

    adjust partition key logic for block rewards

--- models/bronze/streamline/bronze__streamline_FR_block_rewards.sql
+++ models/bronze/streamline/bronze__streamline_FR_block_rewards.sql
@@ -5,7 +5,7 @@
 {% set model = "block_rewards" %}
 {{ streamline_external_table_FR_query(
     model,
-    partition_function = "to_number(split_part(split_part(file_name,'/',3),'=',2))",
+    partition_function = "to_number(split_part(split_part(split_part(file_name,'/',3),'=',2),'_',1))",
     partition_name = "_partition_id",
     unique_key = "block_id",
     other_cols="error"
