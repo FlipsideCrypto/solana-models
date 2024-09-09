2c204f8567784e96c4970bcc13643860d31a8f09
desmond-hui <desmond@flipsidecrypto.com>
2024-09-09 09:23:42 -0700

    change bronze partition key

--- models/bronze/streamline/bronze__streamline_FR_stake_prgram_accounts_2.sql
+++ models/bronze/streamline/bronze__streamline_FR_stake_prgram_accounts_2.sql
@@ -6,7 +6,7 @@
 {{ streamline_external_table_FR_query(
     model,
     partition_function = "split_part(file_name, '/', 3)",
-    partition_name = "_partition_by_created_year",
+    partition_name = "_partition_by_created_date",
     unique_key = "",
     other_cols = ""
 ) }}
