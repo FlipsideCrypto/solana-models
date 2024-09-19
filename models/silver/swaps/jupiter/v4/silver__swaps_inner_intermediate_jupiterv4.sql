9640d75a7896be540ea3359873163c9a18746f22
tarikceric <tarik@flipsidecrypto.com>
2024-09-18 22:34:24 -0700

    tests and cleanup

--- models/silver/swaps/jupiter/v4/silver__swaps_inner_intermediate_jupiterv4.sql
+++ models/silver/swaps/jupiter/v4/silver__swaps_inner_intermediate_jupiterv4.sql
@@ -1,4 +1,4 @@
- -- depends_on: {{ ref('silver__decoded_logs') }}
+-- depends_on: {{ ref('silver__decoded_logs') }}
 {{ config(
     materialized = 'incremental',
     unique_key = "swaps_inner_intermediate_jupiterv4_id",
@@ -15,22 +15,24 @@
 
 {% if execute %}
     {% set base_query %}
-    CREATE OR REPLACE TEMPORARY TABLE silver.swaps_inner_intermediate_jupiterv4__intermediate_tmp AS
+    CREATE
+    OR REPLACE temporary TABLE silver.swaps_inner_intermediate_jupiterv4__intermediate_tmp AS
+
     SELECT
         block_timestamp,
         block_id,
         tx_id,
         program_id,
-        index,
+        INDEX,
         inner_index,
         log_index,
         succeeded,
         event_type,
-        decoded_log:args:amm::string AS swap_program_id,
-        decoded_log:args:inputMint::string AS from_mint,
-        decoded_log:args:inputAmount::string AS from_amount,
-        decoded_log:args:outputMint::string AS to_mint,
-        decoded_log:args:outputAmount::string AS to_amount,
+        decoded_log :args :amm :: STRING AS swap_program_id,
+        decoded_log :args :inputMint :: STRING AS from_mint,
+        decoded_log :args :inputAmount :: STRING AS from_amount,
+        decoded_log :args :outputMint :: STRING AS to_mint,
+        decoded_log :args :outputAmount :: STRING AS to_amount,
         _inserted_timestamp
     FROM
         {{ ref('silver__decoded_logs') }}
@@ -39,29 +41,27 @@
         AND event_type = 'Swap'
         AND succeeded
 
-    {% if is_incremental() %}
-    AND _inserted_timestamp >= (
-        SELECT
-            MAX(_inserted_timestamp) - INTERVAL '1 hour'
-        FROM
-            {{ this }}
-    )
-    -- TO DO: update the date range
-    {% else %}
-        AND block_timestamp :: DATE > '2023-07-15'
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '1 hour'
+    FROM
+        {{ this }}
+)
+{% else %}
+    AND block_timestamp :: DATE > '2023-01-18'
+{% endif %}
 
-    {% endif %}
-    {% endset %}
-    
-    {% do run_query(base_query) %}
-    {% set between_stmts = fsc_utils.dynamic_range_predicate(
-        "silver.swaps_inner_intermediate_jupiterv4__intermediate_tmp",
-        "block_timestamp::date"
-    ) %}
+{% endset %}
+{% do run_query(base_query) %}
+{% set between_stmts = fsc_utils.dynamic_range_predicate(
+    "silver.swaps_inner_intermediate_jupiterv4__intermediate_tmp",
+    "block_timestamp::date"
+) %}
 {% endif %}
 
 WITH base AS (
-    SELECT 
+    SELECT
         *
     FROM
         silver.swaps_inner_intermediate_jupiterv4__intermediate_tmp
@@ -71,21 +71,44 @@ swappers AS (
         tx_id,
         INDEX,
         inner_index,
-        silver.udf_get_account_pubkey_by_name(
-            'userTransferAuthority',
-            decoded_instruction :accounts
-        ) AS swapper,
-        lead(inner_index) OVER (PARTITION BY tx_id, index ORDER BY inner_index) AS next_summary_swap_index,
+        CASE
+            WHEN event_type = 'route' THEN solana_dev.silver.udf_get_account_pubkey_by_name(
+                'userTransferAuthority',
+                decoded_instruction :accounts
+            )
+            WHEN event_type = 'whirlpoolSwapExactOutput' THEN solana_dev.silver.udf_get_account_pubkey_by_name(
+                'tokenAuthority',
+                decoded_instruction :accounts
+            )
+            WHEN event_type = 'raydiumClmmSwapExactOutput' THEN solana_dev.silver.udf_get_account_pubkey_by_name(
+                'payer',
+                decoded_instruction :accounts
+            )
+            WHEN event_type = 'raydiumSwapExactOutput' THEN solana_dev.silver.udf_get_account_pubkey_by_name(
+                'userSourceOwner',
+                decoded_instruction :accounts
+            )
+        END AS swapper,
+        LEAD(inner_index) over (
+            PARTITION BY tx_id,
+            INDEX
+            ORDER BY
+                inner_index
+        ) AS next_summary_swap_index,
         _inserted_timestamp
     FROM
         {{ ref('silver__decoded_instructions_combined') }}
     WHERE
-
         {{ between_stmts }}
         AND program_id = 'JUP4Fb2cqiRUcaTHdrPC8h2gNsA2ETXiPDD33WcGuJB'
-        AND event_type = 'route'
+        AND event_type IN (
+            'route',
+            'raydiumSwapExactOutput',
+            'raydiumClmmSwapExactOutput',
+            'whirlpoolSwapExactOutput'
+        )
         AND swapper IS NOT NULL
-        and succeeded
+        AND succeeded
 ),
 token_decimals AS (
     SELECT
@@ -137,14 +160,15 @@ pre_final AS (
         ON b.tx_id = s.tx_id
         AND b.index = s.index
         AND (
-                s.inner_index IS NULL 
-                OR
-                (
-                    b.inner_index = s.inner_index
-                    AND (b.inner_index < s.next_summary_swap_index 
-                        OR s.next_summary_swap_index IS NULL)
+            s.inner_index IS NULL
+            OR (
+                b.inner_index = s.inner_index
+                AND (
+                    b.inner_index < s.next_summary_swap_index
+                    OR s.next_summary_swap_index IS NULL
                 )
             )
+        )
         LEFT OUTER JOIN token_decimals d
         ON b.from_mint = d.mint
         LEFT OUTER JOIN token_decimals d2
@@ -167,7 +191,7 @@ SELECT
     to_mint,
     to_amount,
     _inserted_timestamp,
-    {{ dbt_utils.generate_surrogate_key(['tx_id','index','log_index']) }} AS swaps_inner_intermediate_jupiterv5_id,
+    {{ dbt_utils.generate_surrogate_key(['tx_id','index','log_index']) }} AS swaps_inner_intermediate_jupiterv4_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
