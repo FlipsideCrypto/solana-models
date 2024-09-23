9d00c8da972029d21fdb905201db1ead6143e299
tarikceric <46071768+tarikceric@users.noreply.github.com>
2024-09-23 12:38:11 -0700

    event_inner to schedule (#658)

--- models/gold/core/core__fact_events_inner.sql
+++ models/gold/core/core__fact_events_inner.sql
@@ -6,7 +6,7 @@
     merge_exclude_columns = ["inserted_timestamp"],
     post_hook = enable_search_optimization('{{this.schema}}', '{{this.identifier}}', 'ON EQUALITY(tx_id, program_id, instruction_program_id, instruction_index, inner_index, event_type)'),
     full_refresh = false,
-    tags = ['events_inner_backfill']
+    tags = ['scheduled_core']
 ) }}
 
 {% if execute %}
@@ -42,6 +42,5 @@ FROM
 
 {% if is_incremental() %}
 WHERE
-    modified_timestamp > '{{ max_modified_timestamp }}'
-    -- to-do: revert back to >= after backfill
+    modified_timestamp >= '{{ max_modified_timestamp }}'
 {% endif %}
