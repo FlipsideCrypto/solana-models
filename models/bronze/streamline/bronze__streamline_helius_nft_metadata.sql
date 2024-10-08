19cde176e34a502ebc9e3e14ece181c67119604d
desmond-hui <desmond@flipsidecrypto.com>
2024-10-08 07:18:44 -0700

    update columns and explode key

--- models/bronze/streamline/bronze__streamline_helius_nft_metadata.sql
+++ models/bronze/streamline/bronze__streamline_helius_nft_metadata.sql
@@ -5,8 +5,8 @@
 {% set model = "helius_nft_metadata" %}
 {{ streamline_external_table_query(
     model,
-    partition_function = "to_date(split_part(split_part(file_name,'/',4),'_',1))",
-    partition_name = "_partition_id",
-    unique_key = "mint",
-    other_cols="helius_nft_metadata_requests_id,max_mint_event_inserted_timestamp"
+    partition_function = "concat_ws('-',split_part(split_part(file_name,'/',3),'_',1),split_part(split_part(file_name,'/',3),'_',2),split_part(split_part(file_name,'/',3),'_',3))",
+    partition_name = "_partition_by_created_date",
+    unique_key = "data:id::STRING AS mint",
+    other_cols="HELIUS_NFT_METADATA_REQUESTS_ID, MAX_MINT_EVENT_INSERTED_TIMESTAMP"
 ) }}
