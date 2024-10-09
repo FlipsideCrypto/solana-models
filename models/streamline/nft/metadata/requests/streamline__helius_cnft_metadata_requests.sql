d453620c657d6e9fb0455b14e2068bd806ee8e5f
desmond-hui <desmond@flipsidecrypto.com>
2024-10-09 07:49:30 -0700

    formatting

--- models/streamline/nft/metadata/requests/streamline__helius_cnft_metadata_requests.sql
+++ models/streamline/nft/metadata/requests/streamline__helius_cnft_metadata_requests.sql
@@ -1,15 +1,15 @@
 {{
     config(
-        materialized="view",
+        materialized = "view",
         post_hook = fsc_utils.if_data_call_function_v2(
             func = 'streamline.udf_bulk_rest_api_v2',
             target = "{{this.schema}}.{{this.identifier}}",
-            params ={ 
-                "external_table" :"helius_nft_metadata",
-                "sql_limit" :"100",
-                "producer_batch_size" :"100",
-                "worker_batch_size" :"10",
-                "sql_source" :"{{this.identifier}}",
+            params = { 
+                "external_table": "helius_nft_metadata",
+                "sql_limit": "100",
+                "producer_batch_size": "100",
+                "worker_batch_size": "10",
+                "sql_source": "{{this.identifier}}",
                 "exploded_key": tojson(["result"]),
                 "order_by_column": "group_num",
             }
@@ -25,7 +25,7 @@ WITH all_unknown_metadata AS (
     WHERE 
         _inserted_timestamp >= (
             SELECT 
-                coalesce(max(max_mint_event_inserted_timestamp),'2000-01-01') 
+                coalesce(max(max_mint_event_inserted_timestamp), '2000-01-01') 
             FROM 
                 {{ ref('streamline__complete_helius_cnft_metadata_requests') }}
         )
@@ -38,7 +38,7 @@ WITH all_unknown_metadata AS (
 numbered AS (
     SELECT
         m.*,
-        ROW_NUMBER() over (ORDER BY m._inserted_timestamp) AS row_num
+        row_number() OVER (ORDER BY m._inserted_timestamp) AS row_num
     FROM
         {{ ref('silver__nft_compressed_mints') }} m
     INNER JOIN
@@ -47,7 +47,7 @@ numbered AS (
     WHERE 
         _inserted_timestamp >= (
             SELECT 
-                coalesce(max(max_mint_event_inserted_timestamp),'2000-01-01') 
+                coalesce(max(max_mint_event_inserted_timestamp), '2000-01-01') 
             FROM 
                 {{ ref('streamline__complete_helius_cnft_metadata_requests') }}
         )
@@ -55,15 +55,15 @@ numbered AS (
 grouped AS (
     SELECT 
         mint,
-        FLOOR((row_num - 1) / 1000) + 1 AS group_num,
+        floor((row_num - 1) / 1000) + 1 AS group_num,
         _inserted_timestamp
     FROM
         numbered
 ),
 list_mints AS (
     SELECT
-        ARRAY_AGG(mint) AS list_mint,
-        MAX(_inserted_timestamp) AS max_mint_event_inserted_timestamp,
+        array_agg(mint) AS list_mint,
+        max(_inserted_timestamp) AS max_mint_event_inserted_timestamp,
         group_num
     FROM
         grouped
@@ -72,17 +72,17 @@ list_mints AS (
 )
 SELECT
     group_num,
-    concat_ws('_',current_timestamp,group_num) AS helius_nft_metadata_requests_id,
+    concat_ws('_', current_timestamp, group_num) AS helius_nft_metadata_requests_id,
     max_mint_event_inserted_timestamp::string AS max_mint_event_inserted_timestamp,
-    replace(current_date::string,'-','_') AS partition_key, -- Issue with streamline handling `-` in partition key so changing to `_`
+    replace(current_date::string, '-', '_') AS partition_key,
     {{ target.database }}.live.udf_api(
         'POST',
         '{service}/?api-key={Authentication}',
-        OBJECT_CONSTRUCT(
+        object_construct(
             'Content-Type',
             'application/json'
         ),
-        OBJECT_CONSTRUCT(
+        object_construct(
             'id',
             helius_nft_metadata_requests_id,
             'jsonrpc',
@@ -90,7 +90,7 @@ SELECT
             'method',
             'getAssetBatch',
             'params',
-            OBJECT_CONSTRUCT(
+            object_construct(
                 'ids',
                 list_mint
             )
