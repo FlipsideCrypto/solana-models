30850c6cae091a8af6992b293425a3c4465d3e31
desmond-hui <desmond@flipsidecrypto.com>
2024-10-11 08:14:41 -0700

    hotfix, tmp hardcode partition to start with

--- models/silver/silver__votes.sql
+++ models/silver/silver__votes.sql
@@ -41,7 +41,7 @@ WITH pre_final AS (
         ) = 'Vote111111111111111111111111111111111111111'
     {% if is_incremental() %}
     AND 
-        _partition_id > 116864 /* TODO REVERT */
+        _partition_id > 116863 /* TODO REVERT */
     AND
         _partition_id <= (
             SELECT 
