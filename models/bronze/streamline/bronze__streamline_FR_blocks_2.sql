da80fc5f62a29cf10ec4a94b90ae7ac5a2238929
desmond-hui <desmond@flipsidecrypto.com>
2024-10-15 07:09:35 -0700

    bronze streamline blocks

---
+++ models/bronze/streamline/bronze__streamline_FR_blocks_2.sql
@@ -0,0 +1,12 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+{% set model = "blocks_2" %}
+{{ streamline_external_table_FR_query_v2(
+    model,
+    partition_function = "split_part(file_name, '/', 3)",
+    partition_name = "_partition_by_created_date",
+    unique_key = "block_id",
+    other_cols="error"
+) }}
