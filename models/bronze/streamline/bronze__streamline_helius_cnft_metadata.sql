3cbe091af0e08bbb66966421f506ebf923169139
desmond-hui <desmond@flipsidecrypto.com>
2024-10-16 07:44:35 -0700

    update partition function

--- models/bronze/streamline/bronze__streamline_helius_cnft_metadata.sql
+++ models/bronze/streamline/bronze__streamline_helius_cnft_metadata.sql
@@ -5,7 +5,7 @@
 {% set model = "helius_nft_metadata" %}
 {{ streamline_external_table_query(
     model,
-    partition_function = "to_timestamp(split_part(file_name, '/', -2), 'YYYY_MM_DD_HH24')",
+    partition_function = "to_timestamp(split_part(split_part(file_name, '/', -2), '_result', 1), 'YYYY_MM_DD_HH24')",
     partition_name = "_partition_by_created_hour",
     unique_key = "data:id::STRING AS mint",
     other_cols="HELIUS_NFT_METADATA_REQUESTS_ID, MAX_MINT_EVENT_INSERTED_TIMESTAMP"
