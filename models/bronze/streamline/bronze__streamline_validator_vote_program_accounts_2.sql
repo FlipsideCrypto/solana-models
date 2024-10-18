2a7c52a74503245b8980ef2d2ca914742afd9c81
desmond-hui <desmond@flipsidecrypto.com>
2024-10-18 08:28:57 -0700

    bronze models

---
+++ models/bronze/streamline/bronze__streamline_validator_vote_program_accounts_2.sql
@@ -0,0 +1,12 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+{% set model = "validator_vote_program_accounts_2" %}
+{{ streamline_external_table_query(
+    model,
+    partition_function = "to_date(split_part(split_part(file_name, '/', -2), '_result', 1), 'YYYY_MM_DD')",
+    partition_name = "_partition_by_created_date",
+    unique_key = "",
+    other_cols = ""
+) }}
