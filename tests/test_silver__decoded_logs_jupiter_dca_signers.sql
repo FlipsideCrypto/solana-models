b3ac67247c666a1a4f219f22d46079d6b112a335
desmond-hui <desmond@flipsidecrypto.com>
2024-10-18 09:21:51 -0700

    hotfix missing comma

--- tests/test_silver__decoded_logs_jupiter_dca_signers.sql
+++ tests/test_silver__decoded_logs_jupiter_dca_signers.sql
@@ -9,7 +9,7 @@ WHERE
     AND signer NOT IN ('DCAKxn5PFNN1mBREPWGdk1RXg5aVH9rPErLfBFEi2Emb',
         'DCAK36VfExkPdAkYUQg6ewgxyinvcEyPLyHjRbmveKFw',
         'DCAKuApAuZtVNYLk3KTAVW9GLWVvPbnb5CxxRRmVgcTr',
-        'BFQ2te7ERN319HA87mn6NJ9oxMUvNxyifqEhUWHFTie9' /* this was used early on for a few dca txs */
+        'BFQ2te7ERN319HA87mn6NJ9oxMUvNxyifqEhUWHFTie9', /* this was used early on for a few dca txs */
         'JD1dHSqYkrXvqUVL8s6gzL1yB7kpYymsHfwsGxgwp55h',
         'JD38n7ynKYcgPpF7k1BhXEeREu1KqptU93fVGy3S624k',
         'JD25qVdtd65FoiXNmR89JjmoJdYk9sjYQeSTZAALFiMy'
