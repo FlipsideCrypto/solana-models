6c1514c17639399aa18096ecfd4d3f91d55099d3
desmond-hui <desmond@flipsidecrypto.com>
2024-10-31 07:52:00 -0700

    remove legacy pipeline assets

--- macros/streamline/snapshot_get_vote_accounts/udf_snapshot_get_vote_accounts.sql
+++
@@ -1,8 +0,0 @@
-{% macro udf_snapshot_get_vote_accounts() %}
-    CREATE
-    OR REPLACE EXTERNAL FUNCTION silver.udf_snapshot_get_vote_accounts() returns text api_integration = aws_solana_api_dev AS {% if target.database == 'SOLANA' -%}
-        'https://pj4rqb8z96.execute-api.us-east-1.amazonaws.com/prod/snapshot_get_vote_accounts'
-    {% else %}
-        'https://11zlwk4fm3.execute-api.us-east-1.amazonaws.com/dev/snapshot_get_vote_accounts'
-    {%- endif %}
-{% endmacro %}
