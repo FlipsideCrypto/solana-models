7035565c6c4418b8098ac938b760344b9ab3dc9b
desmond-hui <desmond@flipsidecrypto.com>
2024-10-31 09:34:07 -0700

    update TODO

--- models/streamline/core/realtime/streamline__solscan_blocks.sql
+++ models/streamline/core/realtime/streamline__solscan_blocks.sql
@@ -37,7 +37,7 @@ block_ids AS (
 SELECT
     block_id,
     replace(current_date::string,'-','_') AS partition_key, -- Issue with streamline handling `-` in partition key so changing to `_`
-    /* TODO: change to use service URI from vault when the sync is fixed */
+    /* TODO: change vault secret path when the sync is fixed */
     live.udf_api(
         'GET',
         concat('{service}/block/',block_id),
