b962553b98e863ea87044ccb89e8abb4f7d4f09e
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2024-11-04 08:56:05 -0800

    turn off legacy snowflake tasks that populate bronze.blocks2 (#690)

--- tests/test_streamline__snowflake_tasks_running.sql
+++ tests/test_streamline__snowflake_tasks_running.sql
@@ -13,16 +13,6 @@ WITH block_txs_real_time_task_counts AS (
             task_name=>'BULK_GET_BLOCK_TXS_REAL_TIME'))
     WHERE
         state = 'SUCCEEDED'
-),
-blocks_real_time_task_counts AS (
-    SELECT 
-        count(*) AS success_count
-    FROM 
-        TABLE(solana.information_schema.task_history(
-            scheduled_time_range_start=>dateadd('hour',-1,current_timestamp()),
-            task_name=>'BULK_GET_BLOCKS_REAL_TIME'))
-    WHERE
-        state = 'SUCCEEDED'
 )
 SELECT
     'block_txs_real_time' AS pipeline,
@@ -31,11 +21,3 @@ FROM
     block_txs_real_time_task_counts
 WHERE
     success_count < 7
-UNION ALL
-SELECT
-    'blocks_real_time' AS pipeline,
-    success_count
-FROM
-    blocks_real_time_task_counts
-WHERE
-    success_count < 10
