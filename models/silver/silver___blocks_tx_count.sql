b74ff15fd6588d1af9354a77b07db36f75cdac7c
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2024-11-05 09:53:04 -0800

    Migrate get solscan blocks to 2.0 (#685)
    * streamline requests for solscan blocks
    
    * bronze views for solscan streamline data
    
    * workflow schedule for solscan streamline requests
    
    * add todo comment
    
    * update uri
    
    * update the silver table before requesting the next set of blocks
    
    * swap to using 2.0 data
    
    * update TODO
    
    * change vault path

--- models/silver/silver___blocks_tx_count.sql
+++ models/silver/silver___blocks_tx_count.sql
@@ -4,21 +4,19 @@
 ) }}
 
 SELECT
-    VALUE :block_id AS block_id,
-    value:result:transactionCount as transaction_count,
+    block_id,
+    data:result:transactionCount as transaction_count,
     _inserted_timestamp
 FROM
-    {{ ref('bronze_api__solscan_blocks') }},
-    TABLE(FLATTEN(DATA)) d
-
+    {{ ref('bronze__streamline_solscan_blocks_2') }}
 {% if is_incremental() %}
 WHERE
     _inserted_timestamp >= (
         SELECT
-            MAX(_inserted_timestamp)
+            max(_inserted_timestamp)
         FROM
             {{ this }}
     )
 {% endif %}
 QUALIFY 
-    row_number() over (partition by block_id order by _inserted_timestamp, transaction_count desc) = 1
+    row_number() over (partition by block_id order by _inserted_timestamp desc, transaction_count desc) = 1
---
+++ models/streamline/core/realtime/streamline__solscan_blocks.sql
@@ -0,0 +1,53 @@
+{{ config(
+    materialized = 'view',
+    post_hook = fsc_utils.if_data_call_function_v2(
+        func = 'streamline.udf_bulk_rest_api_v2',
+        target = "{{this.schema}}.{{this.identifier}}",
+        params ={ "external_table" :"solscan_blocks_2",
+        "sql_limit" :"10000",
+        "producer_batch_size" :"1000",
+        "worker_batch_size" :"1000",
+        "sql_source" :"{{this.identifier}}" }
+    )
+) }}
+
+{% set producer_limit_size = 10000 %}
+
+WITH base AS (
+    SELECT 
+        b.block_id
+    FROM 
+        {{ ref('silver__blocks') }} b
+    LEFT OUTER JOIN 
+        {{ source('solana_silver','_blocks_tx_count') }} b2
+        ON b.block_id = b2.block_id
+    WHERE 
+        b.block_id >= 226000000
+        AND b2.block_id IS NULL
+        AND b.block_timestamp::DATE <= current_date
+),
+block_ids AS (
+    SELECT 
+        block_id 
+    FROM 
+        base b
+    QUALIFY 
+        row_number() OVER (ORDER BY b.block_id DESC) <= {{ producer_limit_size }}
+)
+SELECT
+    block_id,
+    replace(current_date::string,'-','_') AS partition_key, -- Issue with streamline handling `-` in partition key so changing to `_`
+    live.udf_api(
+        'GET',
+        concat('{Service}/block/',block_id),
+        object_construct(
+            'Content-Type',
+            'application/json',
+            'token',
+            '{Authentication}'
+        ),
+        {},
+        'Vault/prod/solana/solscan/v1'
+    ) AS request
+FROM
+    block_ids
