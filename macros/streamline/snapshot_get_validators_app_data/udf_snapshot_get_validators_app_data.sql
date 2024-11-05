0b5863cbf261303f637eb729be45ca40a28d4b42
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2024-11-05 09:07:42 -0800

    Deprecate legacy validators app list api (#696)
    * remove legacy assets
    
    * use streamline 2.0 data in silver model moving forward

--- macros/streamline/snapshot_get_validators_app_data/udf_snapshot_get_validators_app_data.sql
+++
@@ -1,8 +0,0 @@
-{% macro udf_snapshot_get_validators_app_data() %}
-    CREATE
-    OR REPLACE EXTERNAL FUNCTION silver.udf_snapshot_get_validators_app_data() returns text api_integration = aws_solana_api_dev AS {% if target.database == 'SOLANA' -%}
-        'https://pj4rqb8z96.execute-api.us-east-1.amazonaws.com/prod/snapshot_get_validators_list'
-    {% else %}
-        'https://11zlwk4fm3.execute-api.us-east-1.amazonaws.com/dev/snapshot_get_validators_list'
-    {%- endif %}
-{% endmacro %}
