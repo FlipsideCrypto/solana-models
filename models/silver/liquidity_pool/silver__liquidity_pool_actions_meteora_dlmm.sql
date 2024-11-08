665403672476641fc22d833bd6a47e48ffba5132
tarikceric <tarik@flipsidecrypto.com>
2024-11-08 13:54:48 -0800

    handle edge cases

--- models/silver/liquidity_pool/silver__liquidity_pool_actions_meteora_dlmm.sql
+++ models/silver/liquidity_pool/silver__liquidity_pool_actions_meteora_dlmm.sql
@@ -64,17 +64,29 @@ WITH base AS (
 ),
 base_transfers AS (
     SELECT
-        block_timestamp,
-        tx_id,
-        INDEX,
-        mint,
-        amount,
-        dest_token_account,
-        source_token_account
+        a.block_timestamp,
+        a.tx_id,
+        a.INDEX,
+        a.mint,
+        a.amount,
+        a.dest_token_account,
+        a.source_token_account
     FROM
-        {{ ref('silver__transfers') }}
+        {{ ref('silver__transfers') }} a
+    INNER JOIN
+        (
+            SELECT DISTINCT
+                tx_id,
+                block_timestamp::DATE AS block_date
+            FROM
+                base
+        ) b
+    ON
+        b.block_date = a.block_timestamp::DATE
+        AND b.tx_id = a.tx_id
     WHERE
         {{ between_stmts }}
+        AND succeeded
 ),
 decoded AS (
     SELECT
@@ -108,6 +120,10 @@ decoded AS (
             'reserveY',
             decoded_instruction :accounts
         ) AS liquidity_b_token_vault,
+        silver.udf_get_account_pubkey_by_name(
+            'tokenXMint',
+            decoded_instruction :accounts
+        ) AS token_x_mint,
         _inserted_timestamp,
     FROM
         base
@@ -168,31 +184,28 @@ withdraws AS (
         ) :: INT AS inner_index,
         d.program_id,
         d.action,
-        t.mint,
-        t.amount,
+        -- Handle edge case where removeAllLiquidity actions do not transfer anything
+        CASE
+            WHEN d.action = 'removeAllLiquidity' AND t.mint IS NULL THEN d.token_x_mint
+            ELSE t.mint
+        END AS mint,
+        CASE
+            WHEN d.action = 'removeAllLiquidity' AND t.amount IS NULL THEN 0
+            ELSE t.amount
+        END AS amount,
         d.liquidity_provider,
         d.liquidity_pool_address,
         d._inserted_timestamp
     FROM
-        base_transfers t
-        INNER JOIN decoded d
+        decoded d
+    LEFT JOIN base_transfers t
         ON t.block_timestamp :: DATE = d.block_timestamp :: DATE
         AND t.tx_id = d.tx_id
-        AND SPLIT_PART(
-            t.index,
-            '.',
-            1
-        ) = d.index
+        AND SPLIT_PART(t.index, '.', 1) = d.index
+        AND source_token_account IN (liquidity_a_token_vault, liquidity_b_token_vault)
     WHERE
-        d.action IN (
-            'removeLiquidityByRange',
-            'removeLiquidity',
-            'removeAllLiquidity'
-        )
-        AND source_token_account IN (
-            liquidity_a_token_vault,
-            liquidity_b_token_vault
-        )
+        d.action IN ('removeLiquidityByRange', 'removeLiquidity', 'removeAllLiquidity')
+
 ),
 pre_final AS (
     SELECT
