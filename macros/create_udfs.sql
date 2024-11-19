3fea5e340b6c6df91f44cf56c4cfb3f050f77295
desmond-hui <desmond@flipsidecrypto.com>
2024-11-18 17:14:59 -0800

    add more v2 apis

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -12,7 +12,9 @@
             {{ create_udf_bulk_instructions_decoder() }};
             {{ create_udf_bulk_instructions_decoder_v2() }};
             {{ create_udf_verify_idl() }};
+            {{ create_udf_verify_idl_v2() }};
             {{ create_udf_decode_compressed_mint_change_logs() }};
+            {{ create_udf_decode_compressed_mint_change_logs_v2() }};
             {{ create_udf_bulk_rest_api_v2() }};
         {% endif %}
 
