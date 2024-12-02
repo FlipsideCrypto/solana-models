c45ab078be1d53ab191e23c0cbd4ab1ea5146e98
desmond-hui <desmond@flipsidecrypto.com>
2024-12-02 09:57:07 -0800

    formatting

--- models/silver/parser/silver__decoded_logs.sql
+++ models/silver/parser/silver__decoded_logs.sql
@@ -3,11 +3,12 @@
 -- depends_on: {{ ref('bronze__streamline_decoded_logs') }}
 -- depends_on: {{ ref('bronze__streamline_FR_decoded_logs') }}
 -- depends_on: {{ ref('bronze__streamline_decoded_logs_2') }}
+
 {{ config(
     materialized = 'incremental',
     incremental_predicates = ["dynamic_range_predicate", "block_timestamp::date"],
     unique_key = "decoded_logs_id",
-    cluster_by = ['block_timestamp::DATE','_inserted_timestamp::DATE','program_id'],
+    cluster_by = ['block_timestamp::DATE', '_inserted_timestamp::DATE', 'program_id'],
     merge_exclude_columns = ["inserted_timestamp"],
     post_hook = enable_search_optimization(
         '{{this.schema}}',
@@ -15,7 +16,7 @@
         'ON EQUALITY(tx_id, event_type, decoded_logs_id)'
     ),
     full_refresh = false,
-    tags = ['scheduled_non_core'],
+    tags = ['scheduled_non_core']
 ) }}
 
 {% set CUTOVER_DATETIME = modules.datetime.datetime.strptime("2024-07-16 17:00:00", "%Y-%m-%d %H:%M:%S") %}
@@ -27,7 +28,7 @@
     {% if is_incremental() %}
         {% set max_inserted_query %}
             SELECT
-                MAX(_inserted_timestamp) - INTERVAL '1 HOUR' AS _inserted_timestamp
+                max(_inserted_timestamp) - INTERVAL '1 HOUR' AS _inserted_timestamp
             FROM
                 {{ this }}
         {% endset %}
@@ -48,12 +49,12 @@
                 log_index,
                 program_id,
                 data,
-                _inserted_timestamp,
+                _inserted_timestamp
             FROM
                 {% if is_incremental() %}
-                {{ ref('bronze__streamline_decoded_logs') }} A
+                    {{ ref('bronze__streamline_decoded_logs') }} A
                 {% else %}
-                {{ ref('bronze__streamline_FR_decoded_logs') }} A
+                    {{ ref('bronze__streamline_FR_decoded_logs') }} A
                 {% endif %}
             JOIN
                 {{ ref('silver__blocks') }}
@@ -68,7 +69,6 @@
 
         {% set between_stmts = fsc_utils.dynamic_range_predicate("silver.decoded_logs__intermediate_tmp","block_timestamp::date") %}
     {% endif %}
-
 {% endif %}
 
 {% if use_legacy_logic %}
@@ -95,11 +95,11 @@ SELECT
     t.succeeded,
     d.program_id,
     d.data AS decoded_log,
-    decoded_log :name :: STRING AS event_type,
+    decoded_log:name::STRING AS event_type,
     d._inserted_timestamp,
-    {{ dbt_utils.generate_surrogate_key(['d.tx_id', 'd.index', 'd.inner_index','d.log_index']) }} AS decoded_logs_id,
-    SYSDATE() AS inserted_timestamp,
-    SYSDATE() AS modified_timestamp,
+    {{ dbt_utils.generate_surrogate_key(['d.tx_id', 'd.index', 'd.inner_index', 'd.log_index']) }} AS decoded_logs_id,
+    sysdate() AS inserted_timestamp,
+    sysdate() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM
     silver.decoded_logs__intermediate_tmp d
@@ -108,7 +108,7 @@ JOIN
     ON d.block_id = t.block_id
     AND d.tx_id = t.tx_id
 QUALIFY
-    ROW_NUMBER() over (
+    row_number() OVER (
         PARTITION BY decoded_logs_id
         ORDER BY d._inserted_timestamp DESC
     ) = 1
@@ -126,7 +126,7 @@ SELECT
     data AS decoded_log,
     decoded_log:name::string AS event_type,
     _inserted_timestamp,
-    {{ dbt_utils.generate_surrogate_key(['tx_id', 'index', 'inner_index','log_index']) }} AS decoded_logs_id,
+    {{ dbt_utils.generate_surrogate_key(['tx_id', 'index', 'inner_index', 'log_index']) }} AS decoded_logs_id,
     sysdate() AS inserted_timestamp,
     sysdate() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
@@ -149,5 +149,8 @@ WHERE
     AND _partition_by_created_date_hour >= dateadd('hour', -2, date_trunc('hour','{{ max_inserted_timestamp }}'::timestamp_ntz))
 {% endif %}
 QUALIFY
-    row_number() OVER (PARTITION BY decoded_logs_id ORDER BY _inserted_timestamp DESC) = 1
+    row_number() OVER (
+        PARTITION BY decoded_logs_id
+        ORDER BY _inserted_timestamp DESC
+    ) = 1
 {% endif %}
