5d93cda7a3024d997d2c34dc41a54c9e2467110a
desmond-hui <desmond@flipsidecrypto.com>
2024-12-02 07:57:08 -0800

    update requests model

--- models/streamline/decode_instructions/streamline__decode_instructions_3_pyth_realtime.sql
+++ models/streamline/decode_instructions/streamline__decode_instructions_3_pyth_realtime.sql
@@ -7,12 +7,83 @@
     tags = ['streamline_decoder']
 ) }}
 
-/* 
-while we are running in parallel, can just select from the existing table 
-once we are done, we can move the existing code into this table 
-and it should be mostly the same except for the completed table references
-*/
+{% if execute %}
+    {% set max_block_id_query %}
+        select max(block_id)
+        from {{ ref('silver__events') }}
+    {% endset %}
+    {% set max_block_id = run_query(max_block_id_query).columns[0].values()[0] %}
+    {% set min_block_id = max_block_id - 150000 %}
+{% endif %}
+
+WITH event_subset AS (
+    SELECT
+        e.program_id,
+        e.tx_id,
+        e.index,
+        NULL as inner_index,
+        e.instruction,
+        e.block_id,
+        e.block_timestamp,
+        {{ dbt_utils.generate_surrogate_key(['e.block_id','e.tx_id','e.index','inner_index','e.program_id']) }} as id
+    FROM
+        {{ ref('silver__events') }}
+        e
+    WHERE
+        e.program_id = 'FsJ3A3u2vn5cTVofAjvy6y5kwABJAqYWpe4975bi2epH'
+    AND 
+        e.block_timestamp >= CURRENT_DATE - 1
+    AND 
+        e.block_id between {{ min_block_id }} and {{ max_block_id}}
+    AND 
+        e.succeeded
+    UNION ALL
+    SELECT
+        i.value :programId :: STRING AS inner_program_id,
+        e.tx_id,
+        e.index,
+        i.index AS inner_index,
+        i.value AS instruction,
+        e.block_id,
+        e.block_timestamp,
+        {{ dbt_utils.generate_surrogate_key(['e.block_id','e.tx_id','e.index','inner_index','inner_program_id']) }} as id
+    FROM
+        {{ ref('silver__events') }}
+        e
+        JOIN table(flatten(e.inner_instruction:instructions)) i 
+    WHERE
+        ARRAY_CONTAINS('FsJ3A3u2vn5cTVofAjvy6y5kwABJAqYWpe4975bi2epH'::variant, e.inner_instruction_program_ids) 
+    AND
+        e.block_timestamp >= CURRENT_DATE - 1
+    AND 
+        e.block_id between {{ min_block_id }} and {{ max_block_id}}
+    AND 
+        e.succeeded
+    AND 
+        inner_program_id = 'FsJ3A3u2vn5cTVofAjvy6y5kwABJAqYWpe4975bi2epH'
+    
+),
+completed_subset AS (
+    SELECT
+        block_id,
+        complete_decoded_instructions_2_id as id
+    FROM
+        {{ ref('streamline__complete_decoded_instructions_3') }}
+    WHERE
+        block_id >= {{ min_block_id }}
+)
 SELECT
-    *
+    e.program_id,
+    e.tx_id,
+    e.index,
+    e.inner_index,
+    e.instruction,
+    e.block_id,
+    e.block_timestamp
 FROM
-    {{ ref('streamline__decode_instructions_2_pyth_realtime') }}
+    event_subset e
+    LEFT OUTER JOIN completed_subset C
+    ON C.block_id = e.block_id
+    AND e.id = C.id
+WHERE
+    C.block_id IS NULL
