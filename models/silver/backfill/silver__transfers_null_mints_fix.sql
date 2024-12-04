f34ae7dc68d74fbfb1612dcc4be03b87c8b15789
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2024-12-04 09:05:16 -0800

    temporary job to check if all null mints are wsol (#725)

---
+++ models/silver/backfill/silver__transfers_null_mints_fix.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        materialized = 'incremental',
+        full_refresh = false
+    )
+}}
+
+{% if execute and is_incremental() %}
+    {% set next_block_date_to_process_query %}
+        select min(block_timestamp::date)-1 from {{ this }}
+    {% endset %}
+    {% set next_block_date_to_process = run_query(next_block_date_to_process_query)[0][0] %}
+{% endif %}
+
+select 
+    t.block_timestamp,
+    tr.*,
+    (
+        silver.udf_get_account_balances_index(dest_token_account, t.account_keys) IS NOT NULL
+        OR silver.udf_get_account_balances_index(source_token_account, t.account_keys) IS NOT NULL
+    ) AS is_wsol
+from solana_dev.silver.transfers_null_mints AS tr
+join solana.silver.transactions AS t 
+    using(tx_id)
+where
+    {% if is_incremental() %}
+        t.block_timestamp::date = '{{ next_block_date_to_process }}'
+    {% else %}
+        t.block_timestamp::date = '2024-12-04'
+    {% endif %}
