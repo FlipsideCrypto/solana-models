ef07bd37f5086c8f3a6c4b94748221bdce436be8
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2024-12-06 09:29:07 -0800

    Add backup block txs count pipeline (#729)
    * add backup pipeline to get tx counts from helius
    
    * use helius if solscan not available
    
    * temporarily replace solscan call w/ helius call

--- models/silver/non_core/silver___blocks_tx_count.sql
+++ models/silver/non_core/silver___blocks_tx_count.sql
@@ -3,20 +3,48 @@
     unique_key = ['block_id'],
 ) }}
 
+WITH solscan_blocks AS (
+    SELECT
+        block_id,
+        coalesce(data:data:transactions_count,data:result:transactionCount) AS solscan_transaction_count,
+        _inserted_timestamp
+    FROM
+        {{ ref('bronze__streamline_solscan_blocks_2') }}
+    {% if is_incremental() %}
+    WHERE
+        _inserted_timestamp >= (
+            SELECT
+                max(_inserted_timestamp) - INTERVAL '15 MINUTE'
+            FROM
+                {{ this }}
+        )
+    {% endif %}
+),
+helius_blocks AS (
+    SELECT
+        block_id,
+        transaction_count AS helius_transaction_count,
+        _inserted_timestamp
+    FROM
+        {{ ref('bronze__streamline_helius_blocks') }}
+    {% if is_incremental() %}
+    WHERE
+        _inserted_timestamp >= (
+            SELECT
+                max(_inserted_timestamp) - INTERVAL '15 MINUTE'
+            FROM
+                {{ this }}
+        )
+    {% endif %}
+)
 SELECT
     block_id,
-    coalesce(data:data:transactions_count,data:result:transactionCount) as transaction_count,
-    _inserted_timestamp
+    coalesce(s.solscan_transaction_count,helius_transaction_count) AS transaction_count,
+    least_ignore_nulls(s._inserted_timestamp,h._inserted_timestamp) AS _inserted_timestamp
 FROM
-    {{ ref('bronze__streamline_solscan_blocks_2') }}
-{% if is_incremental() %}
-WHERE
-    _inserted_timestamp >= (
-        SELECT
-            max(_inserted_timestamp)
-        FROM
-            {{ this }}
-    )
-{% endif %}
+    solscan_blocks AS s
+FULL OUTER JOIN
+    helius_blocks AS h
+    USING (block_id)
 QUALIFY 
-    row_number() over (partition by block_id order by _inserted_timestamp desc, transaction_count desc) = 1
+    row_number() over (partition by block_id order by least_ignore_nulls(s._inserted_timestamp,h._inserted_timestamp) desc, transaction_count desc) = 1
---
+++ models/streamline/core/realtime/streamline__helius_blocks.sql
@@ -0,0 +1,74 @@
+{{ config(
+    materialized = 'view',
+    post_hook = fsc_utils.if_data_call_function_v2(
+        func = 'streamline.udf_bulk_rest_api_v2',
+        target = "{{this.schema}}.{{this.identifier}}",
+        params ={ "external_table" :"helius_blocks",
+            "sql_limit" :"10000",
+            "producer_batch_size" :"1000",
+            "worker_batch_size" :"1000",
+            "sql_source" :"{{this.identifier}}", 
+        }
+    )
+) }}
+
+{% set producer_limit_size = 10000 %}
+
+WITH base AS (
+    SELECT 
+        b.block_id
+    FROM 
+        {{ ref('silver__blocks') }} b
+    LEFT OUTER JOIN 
+        {{ source('solana_silver','_blocks_tx_count') }} b2
+        ON b.block_id = b2.block_id
+    WHERE 
+        b.block_id >= 226000000
+        AND (b2.block_id IS NULL 
+            OR b2.transaction_count IS NULL)
+        AND b.block_timestamp::DATE <= current_date
+),
+block_ids AS (
+    SELECT 
+        block_id 
+    FROM 
+        base b
+    QUALIFY 
+        row_number() OVER (ORDER BY b.block_id DESC) <= {{ producer_limit_size }}
+)
+SELECT
+    block_id,
+    replace(current_date::string,'-','_') AS partition_key, -- Issue with streamline handling `-` in partition key so changing to `_`
+    {{ target.database }}.live.udf_api(
+        'POST',
+        '{service}/?api-key={Authentication}',
+        OBJECT_CONSTRUCT(
+            'Content-Type',
+            'application/json'
+        ),
+        OBJECT_CONSTRUCT(
+            'id',
+            block_id,
+            'jsonrpc',
+            '2.0',
+            'method',
+            'getBlock',
+            'params',
+            ARRAY_CONSTRUCT(
+                block_id,
+                OBJECT_CONSTRUCT(
+                    'encoding',
+                    'jsonParsed',
+                    'rewards',
+                    False,
+                    'transactionDetails',
+                    'signatures',
+                    'maxSupportedTransactionVersion',
+                    0
+                )
+            )
+        ),
+        'Vault/prod/solana/helius/mainnet'
+    ) AS request
+FROM
+    block_ids
