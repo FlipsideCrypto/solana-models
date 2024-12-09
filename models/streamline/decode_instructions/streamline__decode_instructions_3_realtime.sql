8ea30e9285fa526b15728899e864d773ce1ac710
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2024-12-09 08:05:34 -0800

    Deprecate legacy decode instructions pipeline (#723)
    * update requests model
    
    * only call streamline 2.0 endpoint
    
    * use streamline 2.0 data after cutoff, formatting
    
    * fix id column reference
    
    * remove FR and add comment
    
    * update helper to use new completed table
    
    * update cutoff date, extend lookback

--- models/streamline/decode_instructions/streamline__decode_instructions_3_realtime.sql
+++ models/streamline/decode_instructions/streamline__decode_instructions_3_realtime.sql
@@ -7,12 +7,101 @@
     tags = ['streamline_decoder']
 ) }}
 
-/* 
-while we are running in parallel, can just select from the existing table 
-once we are done, we can move the existing code into this table 
-and it should be mostly the same except for the completed table references
-*/
+{% if execute %}
+    {% set min_event_block_id_query %}
+        select min(block_id)
+        from {{ ref('silver__events') }}
+        where 
+            block_timestamp >= CURRENT_DATE - 2
+    {% endset %}
+    {% set min_event_block_id = run_query(min_event_block_id_query).columns[0].values()[0] %}
+{% endif %}
+
+WITH idl_in_play AS (
+
+    SELECT
+        program_id
+    FROM
+        {{ ref('silver__verified_idls') }}
+    WHERE   
+        program_id <> 'FsJ3A3u2vn5cTVofAjvy6y5kwABJAqYWpe4975bi2epH'
+),
+event_subset AS (
+    SELECT
+        e.program_id,
+        e.tx_id,
+        e.index,
+        NULL as inner_index,
+        e.instruction,
+        e.block_id,
+        e.block_timestamp,
+        {{ dbt_utils.generate_surrogate_key(['e.block_id','e.tx_id','e.index','inner_index','e.program_id']) }} as id
+    FROM
+        {{ ref('silver__events') }}
+        e
+        JOIN idl_in_play b
+        ON e.program_id = b.program_id
+    WHERE
+        e.block_timestamp >= CURRENT_DATE - 2
+    AND 
+        e.succeeded
+    UNION ALL
+    SELECT
+        i.value :programId :: STRING AS inner_program_id,
+        e.tx_id,
+        e.index,
+        i.index AS inner_index,
+        i.value AS instruction,
+        e.block_id,
+        e.block_timestamp,
+        {{ dbt_utils.generate_surrogate_key(['e.block_id','e.tx_id','e.index','inner_index','inner_program_id']) }} as id
+    FROM
+        {{ ref('silver__events') }}
+        e
+        JOIN idl_in_play b
+        ON ARRAY_CONTAINS(b.program_id::variant, e.inner_instruction_program_ids) 
+        JOIN table(flatten(e.inner_instruction:instructions)) i 
+    WHERE
+        e.block_timestamp >= CURRENT_DATE - 2
+    AND 
+        e.succeeded
+    AND 
+        i.value :programId :: STRING = b.program_id
+    AND (
+            (
+                inner_program_id in ('FLASH6Lo6h3iasJKWDs2F8TkW2UKf3s15C8PMGuVfgBn','SNPRohhBurQwrpwAptw1QYtpFdfEKitr4WSJ125cN1g','GovaE4iu227srtG2s3tZzB4RmWBzw8sTwrCLZz7kN7rY','JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4','DCA265Vj8a9CEuX1eb1LWRnDT7uK6q1xMipnNyatn23M','PERPHjGBqRHArX4DySjwM6UJHiR3sWAatqfdBS2qQJu','LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo','PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY','6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P')
+                AND array_size(i.value:accounts) > 1
+            )
+            OR inner_program_id not in ('FLASH6Lo6h3iasJKWDs2F8TkW2UKf3s15C8PMGuVfgBn','SNPRohhBurQwrpwAptw1QYtpFdfEKitr4WSJ125cN1g','GovaE4iu227srtG2s3tZzB4RmWBzw8sTwrCLZz7kN7rY','JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4','DCA265Vj8a9CEuX1eb1LWRnDT7uK6q1xMipnNyatn23M','PERPHjGBqRHArX4DySjwM6UJHiR3sWAatqfdBS2qQJu','LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo','PhoeNiXZ8ByJGLkxNfZRnkUfjvmuYqLR89jjFHGqdXY','6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P')
+        )
+),
+completed_subset AS (
+    SELECT
+        block_id,
+        complete_decoded_instructions_3_id as id
+    FROM
+        {{ ref('streamline__complete_decoded_instructions_3') }}
+    WHERE
+        block_id >= {{ min_event_block_id }} --ensure we at least prune to last 2 days worth of blocks since the dynamic below will scan everything
+    AND block_id >= (
+            SELECT
+                MIN(block_id)
+            FROM
+                event_subset
+        )
+)
 SELECT
-    *
+    e.program_id,
+    e.tx_id,
+    e.index,
+    e.inner_index,
+    e.instruction,
+    e.block_id,
+    e.block_timestamp
 FROM
-    {{ ref('streamline__decode_instructions_2_realtime') }}
+    event_subset e
+    LEFT OUTER JOIN completed_subset C
+    ON C.block_id = e.block_id
+    AND e.id = C.id
+WHERE
+    C.block_id IS NULL
