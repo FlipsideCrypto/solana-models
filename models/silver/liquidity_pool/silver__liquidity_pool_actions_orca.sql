31620693758b9d1828815f8bb85958c9c60429c0
tarikceric <tarik@flipsidecrypto.com>
2024-12-10 13:20:03 -0800

    deprecate non-whirlpool models

--- models/silver/liquidity_pool/silver__liquidity_pool_actions_orca.sql
+++ models/silver/liquidity_pool/silver__liquidity_pool_actions_orca.sql
@@ -7,7 +7,8 @@
     tags = ['scheduled_non_core'],
 ) }}
 
-
+-- Select from the deprecated _view models only during a FR
+{% if not is_incremental() %}
 SELECT 
     block_id,
     block_timestamp,
@@ -27,7 +28,7 @@ SELECT
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM 
-    {{ ref('silver__burns_orca_non_whirlpool') }}
+    {{ ref('silver__burns_orca_non_whirlpool_view') }}
 {% if is_incremental() %}
 WHERE _inserted_timestamp >= (
     SELECT
@@ -37,6 +38,66 @@ WHERE _inserted_timestamp >= (
 )
 {% endif %}
 UNION
+SELECT
+    block_id,
+    block_timestamp,
+    tx_id,
+    succeeded,
+    index,
+    inner_index,
+    program_id,
+    action,
+    mint,
+    Amount,
+    liquidity_provider,
+    liquidity_pool_address,
+    _inserted_timestamp,
+    {{ dbt_utils.generate_surrogate_key(['TX_ID','INDEX','INNER_INDEX']) }} AS liquidity_pool_actions_orca_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM 
+    {{ ref('silver__mints_orca_non_whirlpool_view') }}
+{% if is_incremental() %}
+WHERE _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp)
+    FROM
+        {{ this }}
+)
+{% endif %}
+UNION
+SELECT
+    block_id,
+    block_timestamp,
+    tx_id,
+    succeeded,
+    index,
+    inner_index,
+    program_id,
+    action,
+    mint,
+    Amount,
+    liquidity_provider,
+    liquidity_pool_address,
+    _inserted_timestamp,
+    {{ dbt_utils.generate_surrogate_key(['TX_ID','INDEX','INNER_INDEX']) }} AS liquidity_pool_actions_orca_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM 
+    {{ ref('silver__pool_transfers_orca_non_whirlpool_view') }}
+{% if is_incremental() %}
+WHERE _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp)
+    FROM
+        {{ this }}
+)
+{% endif %}
+UNION
+{% endif %}
+-- always select from the active models
 SELECT
     block_id,
     block_timestamp,
@@ -85,65 +146,7 @@ SELECT
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM 
-    {{ ref('silver__mints_orca_non_whirlpool') }}
-{% if is_incremental() %}
-WHERE _inserted_timestamp >= (
-    SELECT
-        MAX(_inserted_timestamp)
-    FROM
-        {{ this }}
-)
-{% endif %}
-UNION
-SELECT
-    block_id,
-    block_timestamp,
-    tx_id,
-    succeeded,
-    index,
-    inner_index,
-    program_id,
-    action,
-    mint,
-    Amount,
-    liquidity_provider,
-    liquidity_pool_address,
-    _inserted_timestamp,
-    {{ dbt_utils.generate_surrogate_key(['TX_ID','INDEX','INNER_INDEX']) }} AS liquidity_pool_actions_orca_id,
-    SYSDATE() AS inserted_timestamp,
-    SYSDATE() AS modified_timestamp,
-    '{{ invocation_id }}' AS _invocation_id
-FROM 
     {{ ref('silver__mints_orca_whirlpool') }}
-{% if is_incremental() %}
-WHERE _inserted_timestamp >= (
-    SELECT
-        MAX(_inserted_timestamp)
-    FROM
-        {{ this }}
-)
-{% endif %}
-UNION
-SELECT
-    block_id,
-    block_timestamp,
-    tx_id,
-    succeeded,
-    index,
-    inner_index,
-    program_id,
-    action,
-    mint,
-    Amount,
-    liquidity_provider,
-    liquidity_pool_address,
-    _inserted_timestamp,
-    {{ dbt_utils.generate_surrogate_key(['TX_ID','INDEX','INNER_INDEX']) }} AS liquidity_pool_actions_orca_id,
-    SYSDATE() AS inserted_timestamp,
-    SYSDATE() AS modified_timestamp,
-    '{{ invocation_id }}' AS _invocation_id
-FROM 
-    {{ ref('silver__pool_transfers_orca_non_whirlpool') }}
 {% if is_incremental() %}
 WHERE _inserted_timestamp >= (
     SELECT
