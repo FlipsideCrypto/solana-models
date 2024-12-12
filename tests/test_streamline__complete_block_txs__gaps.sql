c7e1615383963c0a45dad927440456b4a1fb8667
desmond-hui <desmond@flipsidecrypto.com>
2024-12-12 14:48:50 -0800

    update start block to do checks, add streamline 2.0 data

--- tests/test_streamline__complete_block_txs__gaps.sql
+++ tests/test_streamline__complete_block_txs__gaps.sql
@@ -1,3 +1,6 @@
+/* TODO: Update this to be the block id of the cutover before merge */
+{% set cutover_block_id = 307000000 %}
+
 WITH base_blocks AS (
     SELECT
         *
@@ -8,7 +11,7 @@ WITH base_blocks AS (
         solana_dev.silver.blocks
         {% endif %}
     WHERE
-        block_id >= 154195836 -- this query wont give correct results prior to this block_id
+        block_id >= 306000000 -- this query wont give correct results prior to this block_id
         AND _inserted_date < CURRENT_DATE
 ),
 base_txs AS (
@@ -21,7 +24,7 @@ base_txs AS (
         solana_dev.silver.transactions
         {% endif %}
     WHERE
-        block_id >= 154195836
+        block_id >= 306000000
     UNION
     SELECT
         DISTINCT block_id
@@ -32,7 +35,7 @@ base_txs AS (
         solana_dev.silver.votes
         {% endif %}
     WHERE
-        block_id >= 154195836
+        block_id >= 306000000
 ),
 potential_missing_txs AS (
     SELECT
@@ -45,11 +48,20 @@ potential_missing_txs AS (
         base_txs.block_id IS NULL
 )
 SELECT
-    m.block_id
+    m.block_id 
 FROM
     potential_missing_txs m
     LEFT OUTER JOIN {{ ref('streamline__complete_block_txs') }} cmp
     ON m.block_id = cmp.block_id
+    LEFT OUTER JOIN {{ ref('streamline__complete_block_txs_2') }} cmp2
+    ON m.block_id = cmp2.block_id
 WHERE
-    cmp.error IS NOT NULL
-    OR cmp.block_id IS NULL
+    (
+        m.block_id < {{ cutover_block_id }} -- cutover block id from silver__transactions
+        AND (cmp.error IS NOT NULL OR cmp.block_id IS NULL)
+    )
+    OR (
+        m.block_id >= {{ cutover_block_id }}
+        AND cmp2._partition_id >= 150000
+        AND (cmp2.block_id IS NULL)
+    )
