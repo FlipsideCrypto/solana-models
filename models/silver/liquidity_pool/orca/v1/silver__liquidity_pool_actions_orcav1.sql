d025fa7fe213c471ee0dfba7ef0e15fbdb01464e
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2025-01-08 18:50:00 -0800

    tmp batch load 2

--- models/silver/liquidity_pool/orca/v1/silver__liquidity_pool_actions_orcav1.sql
+++ models/silver/liquidity_pool/orca/v1/silver__liquidity_pool_actions_orcav1.sql
@@ -48,7 +48,8 @@
         )
         {% if is_incremental() %}
         -- AND _inserted_timestamp > '{{ max_timestamp }}'
-        AND block_timestamp::date BETWEEN '2022-06-01'  AND '2023-01-01'
+        -- AND block_timestamp::date BETWEEN '2022-06-01' AND '2023-01-01'
+        AND block_timestamp::date BETWEEN '2023-01-01' AND '2023-06-01'
         {% else %}
         /* 
         there are data issues from 2021-02-14 to 2021-03-17 with transfers 
