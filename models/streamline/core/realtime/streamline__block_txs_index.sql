43f94b2a8bbc45c8cd4870fd9311381cd7cdb765
desmond-hui <desmond@flipsidecrypto.com>
2025-01-15 16:59:16 -0800

    adjust streamline request sizes

--- models/streamline/core/realtime/streamline__block_txs_index.sql
+++ models/streamline/core/realtime/streamline__block_txs_index.sql
@@ -4,9 +4,9 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"block_txs_index_backfill",
-            "sql_limit" :"10000",
-            "producer_batch_size" :"10000",
-            "worker_batch_size" :"10000",
+            "sql_limit" :"37500",
+            "producer_batch_size" :"37500",
+            "worker_batch_size" :"750",
             "sql_source" :"{{this.identifier}}", 
             "order_by_column": "block_id DESC",
             "exploded_key": tojson(["result.signatures"]),
@@ -23,6 +23,11 @@ WITH block_ids AS (
     WHERE 
         -- all blocks after this should have tx id filled in already so start the backfill here
         b.block_id <= 307868470 
+    EXCEPT
+    SELECT DISTINCT
+        block_id
+    FROM
+        {{ ref('silver__backfill_transactions_index') }}
 )
 SELECT
     block_id,
