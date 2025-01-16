5806eb99c047eb30256998fee77e9f8979b6b713
desmond-hui <desmond@flipsidecrypto.com>
2025-01-15 16:58:33 -0800

    add silver table to store backfilled data

---
+++ models/silver/backfill/silver__backfill_transactions_index.sql
@@ -0,0 +1,41 @@
+-- depends_on: {{ ref('bronze__streamline_block_tx_index_backfill') }}
+
+{{
+    config(
+        materialized="incremental",
+        cluster_by = ["_partition_by_created_date","_inserted_timestamp::date"],
+        tags=['tx_index_backfill']
+    )
+}}
+
+{% if execute %}
+    {% if is_incremental() %}
+        {% set max_partition_query %}
+        SELECT max(_partition_by_created_date) FROM {{ this }}
+        {% endset %}
+        {% set max_partition = run_query(max_partition_query)[0][0] %}
+    {% endif %}
+{% endif %}
+
+SELECT 
+    block_id,
+    to_timestamp_ntz(value:"result.blockTime"::int) AS block_timestamp,
+    data::string as tx_id,
+    value:array_index::int as tx_index,
+    _partition_by_created_date,
+    _inserted_timestamp,
+    {{ dbt_utils.generate_surrogate_key(['tx_id']) }} AS transactions_id,
+    sysdate() AS inserted_timestamp,
+    sysdate() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+    {% if is_incremental() %}
+    {{ ref('bronze__streamline_block_tx_index_backfill') }}
+    {% else %}
+    {{ ref('bronze__streamline_FR_block_tx_index_backfill') }}
+    {% endif %}
+{% if is_incremental() %}
+WHERE 
+    _partition_by_created_date >= {{ max_partition }}
+    AND _inserted_timestamp > (SELECT max(_inserted_timestamp) FROM {{ this }}) 
+{% endif %}
