83a01cb9305e784a87650de2fef9443328194e96
desmond-hui <desmond@flipsidecrypto.com>
2025-01-24 07:51:28 -0800

    revert

--- models/silver/validator/silver__snapshot_block_production.sql
+++ models/silver/validator/silver__snapshot_block_production.sql
@@ -1,6 +1,6 @@
 {{ config(
   materialized = 'incremental',
-  unique_key = "CONCAT_WS('-', epoch, node_pubkey, start_slot, end_slot)",
+  unique_key = "CONCAT_WS('-', epoch, node_pubkey)",
   incremental_strategy = 'delete+insert',
   cluster_by = ['_inserted_timestamp::DATE'],
   tags = ['validator']
@@ -15,7 +15,7 @@ select
     json_data:data:result:value:range:lastSlot :: INT as end_slot,
     _inserted_timestamp,
     {{ dbt_utils.generate_surrogate_key(
-        ['epoch', 'node_pubkey', 'start_slot', 'end_slot']
+        ['epoch', 'node_pubkey']
     ) }} AS snapshot_block_production_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
@@ -33,7 +33,6 @@ WHERE _inserted_timestamp > (
     {{ this }}
 )
 {% endif %}
-qualify(ROW_NUMBER() over(PARTITION BY epoch, node_pubkey, start_slot, end_slot
+qualify(ROW_NUMBER() over(PARTITION BY epoch, node_pubkey
 ORDER BY
     _inserted_timestamp DESC)) = 1
-
