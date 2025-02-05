6dfc513fe81814c968d6cf5911a62f3ba516641f
desmond-hui <desmond@flipsidecrypto.com>
2025-02-05 09:24:47 -0800

    update full refresh start

--- models/silver/liquidity_pool/meteora/dlmm/silver__liquidity_pool_actions_meteora_dlmm_2.sql
+++ models/silver/liquidity_pool/meteora/dlmm/silver__liquidity_pool_actions_meteora_dlmm_2.sql
@@ -70,7 +70,7 @@
             {% set end_date = max_block_ts + modules.datetime.timedelta(days=batch_backfill_size) %}
             AND _inserted_timestamp::date BETWEEN '{{ max_block_ts }}' AND '{{ end_date }}'
         {% else %}
-        AND _inserted_timestamp::date BETWEEN '2024-12-01' AND '2025-01-01'
+        AND _inserted_timestamp::date BETWEEN '2024-02-01' AND '2024-03-01'
         {% endif %}
     {% endset %}
     {% do run_query(base_query) %}
