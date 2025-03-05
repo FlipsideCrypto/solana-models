e02c3ab1b426530ad73a2c2738e2401078eb1da0
tarikceric <tarik@flipsidecrypto.com>
2025-03-04 16:43:20 -0800

    wip

---
+++ models/gold/defi/defi__ez_liquidity_pool_actions.sql
@@ -0,0 +1,182 @@
+{{
+    config(
+        materialized = 'incremental',
+        incremental_strategy = 'merge',
+        meta={'database_tags':{'table':{'PURPOSE': 'STAKING'}}},
+        unique_key = ['ez_liquidity_pool_actions_id'],
+        merge_exclude_columns = ['inserted_timestamp'],
+        post_hook = enable_search_optimization(
+            '{{this.schema}}',
+            '{{this.identifier}}',
+            'ON EQUALITY(ez_liquidity_pool_actions_id, pool_address, provider_address, tx_id, action_type, token_a_mint, token_b_mint)'),
+                cluster_by = ['block_timestamp::DATE','action_type','program_id'],
+        tags = ['scheduled_non_core'],
+    )
+}}
+
+{% set pool_platforms = [
+    'raydiumv4', 
+    'raydium_cpmm', 
+    'raydium_clmm', 
+    'orcav1', 
+    'orcav2',
+    'meteora_2',
+    'meteora_dlmm_2',
+    'orca_whirlpool',
+] %}
+-- tod0: figure out issue with meteora_mulit above
+-- todo - also need ot include the 3rd token for multi ones
+
+WITH 
+base AS (
+    {% for platform in pool_platforms %}
+        SELECT
+            lp.block_id,
+            lp.block_timestamp,
+            lp.tx_id,
+            lp.index,
+            lp.inner_index,
+            CASE
+                WHEN REGEXP_LIKE(lp.event_type, '^(increase|add|deposit|bootstrap|open).*', 'i') THEN 'deposit'
+                WHEN REGEXP_LIKE(lp.event_type, '^(decrease|remove|withdraw|close).*', 'i') THEN 'withdraw'
+                ELSE lp.event_type
+            END AS action_type,
+            lp.provider_address,
+            lp.token_a_mint,
+            CASE
+                WHEN lp.token_a_mint = m.token_a_mint THEN m.token_a_symbol
+                WHEN lp.token_a_mint = m.token_b_mint THEN m.token_b_symbol
+            END AS token_a_symbol,
+            lp.token_a_amount,
+            lp.token_b_mint,
+            CASE
+                WHEN lp.token_b_mint = m.token_a_mint THEN m.token_a_symbol
+                WHEN lp.token_b_mint = m.token_b_mint THEN m.token_b_symbol
+            END AS token_b_symbol,
+            lp.token_b_amount,
+            NULL AS token_c_mint,
+            NULL AS token_c_symbol,
+            NULL AS token_c_amount,
+            NULL AS token_d_mint,
+            NULL AS token_d_symbol,
+            NULL AS token_d_amount,
+            lp.pool_address,
+            m.pool_name,
+            lp.program_id,
+            m.platform,
+            lp.liquidity_pool_actions_{{ platform }}_id AS ez_liquidity_pool_actions_id
+        FROM 
+            {{ ref('silver__liquidity_pool_actions_' ~ platform) }} AS lp
+        INNER JOIN
+            {{ ref('silver__liquidity_pools') }} AS m
+            USING(pool_address)
+        {% if is_incremental() %}
+            where lp.modified_timestamp > (select max(modified_timestamp) from {{ this }})
+        {% endif %}
+        {% if not loop.last %}
+        UNION ALL
+        {% endif %}
+    {% endfor %}
+
+    UNION ALL
+    -- do meteora multi separately
+        SELECT
+            lp.block_id,
+            lp.block_timestamp,
+            lp.tx_id,
+            lp.index,
+            lp.inner_index,
+            CASE
+                WHEN REGEXP_LIKE(lp.event_type, '^(increase|add|deposit|bootstrap|open).*', 'i') THEN 'deposit'
+                WHEN REGEXP_LIKE(lp.event_type, '^(decrease|remove|withdraw|close).*', 'i') THEN 'withdraw'
+                ELSE lp.event_type
+            END AS action_type,
+            lp.provider_address,
+            lp.token_a_mint,
+            NULL AS token_a_symbol,
+            lp.token_a_amount,
+            lp.token_b_mint,
+            NULL AS token_b_symbol,
+            lp.token_b_amount,
+            token_c_mint,
+            NULL AS token_c_symbol,
+            token_c_amount,
+            token_d_mint,
+            NULL AS token_d_symbol,
+            token_d_amount,
+            lp.pool_address,
+            NULL AS pool_name,
+            lp.program_id,
+            NULL AS platform,
+            lp.liquidity_pool_actions_meteora_multi_2_id AS ez_liquidity_pool_actions_id
+        FROM 
+            {{ ref('silver__liquidity_pool_actions_meteora_multi_2') }} AS lp
+        where lp.modified_timestamp::date = '2025-03-01'
+        {% if is_incremental() %}
+            where lp.modified_timestamp > (select max(modified_timestamp) from {{ this }})
+        {% endif %}
+),
+token_prices AS (
+    SELECT
+        HOUR,
+        token_address,
+        price
+    FROM
+        {{ ref('price__ez_prices_hourly') }}
+    WHERE
+        HOUR :: DATE IN (
+            SELECT
+                DISTINCT block_timestamp :: DATE
+            FROM
+                base
+        )
+)
+SELECT
+    block_id,
+    block_timestamp,
+    tx_id,
+    index,
+    inner_index,
+    action_type,
+    provider_address,
+    token_a_mint,
+    token_a_symbol,
+    token_a_amount,
+    (token_a_amount * tp_a.price)::numeric(20,8) AS token_a_amount_usd,
+    token_b_mint,
+    token_b_symbol,
+    token_b_amount,
+    (token_b_amount * tp_b.price)::numeric(20,8) AS token_b_amount_usd,
+    token_c_mint,
+    token_c_symbol,
+    token_c_amount,
+    (token_c_amount * tp_c.price)::numeric(20,8) AS token_c_amount_usd,
+    token_d_mint,
+    token_d_symbol,
+    token_d_amount,
+    (token_d_amount * tp_d.price)::numeric(20,8) AS token_d_amount_usd,
+    pool_address,
+    pool_name,
+    program_id,
+    platform,
+    ez_liquidity_pool_actions_id,
+    sysdate() AS inserted_timestamp,
+    sysdate() AS modified_timestamp
+FROM
+    base AS b
+LEFT JOIN
+    token_prices AS tp_a
+    ON date_trunc('hour', b.block_timestamp) = tp_a.HOUR
+    AND b.token_a_mint = tp_a.token_address
+LEFT JOIN
+    token_prices AS tp_b
+    ON date_trunc('hour', b.block_timestamp) = tp_b.HOUR
+    AND b.token_b_mint = tp_b.token_address
+LEFT JOIN
+    token_prices AS tp_c
+    ON date_trunc('hour', b.block_timestamp) = tp_c.HOUR
+    AND b.token_c_mint = tp_c.token_address
+LEFT JOIN
+    token_prices AS tp_d
+    ON date_trunc('hour', b.block_timestamp) = tp_d.HOUR
+    AND b.token_d_mint = tp_d.token_address
---
+++ models/silver/liquidity_pool/pools/silver__liquidity_pools.sql
@@ -0,0 +1,161 @@
+{{
+    config(
+        materialized = 'incremental',
+        incremental_strategy = 'merge',
+        unique_key = ['pool_address'],
+        merge_exclude_columns = ['inserted_timestamp'],
+        post_hook = enable_search_optimization('{{this.schema}}', '{{this.identifier}}', 'ON EQUALITY(pool_address,token_a_mint,token_b_mint)'),
+        tags = ['scheduled_non_core'],
+    )
+}}
+
+{% set pool_platforms = [
+    'raydiumv4', 
+    'raydium_cpmm', 
+    'raydium_clmm', 
+    'orcav1', 
+    'orcav2', 
+    'orca_whirlpool', 
+    'meteora',
+    'meteora_dlmm'
+] %}
+
+WITH base AS (
+{% for platform in pool_platforms %}
+    SELECT 
+        pool_address,
+        pool_token_mint,
+        token_a_mint,
+        nullif(m.symbol,'') AS token_a_symbol,
+        token_a_account,
+        token_b_mint,
+        nullif(m2.symbol,'') AS token_b_symbol,
+        token_b_account,
+        block_id AS initialized_at_block_id,
+        block_timestamp AS initialized_at_block_timestamp,
+        tx_id AS initialized_at_tx_id,
+        index AS initialized_at_index,
+        inner_index AS initialized_at_inner_index,
+        program_id,
+        CASE
+            WHEN '{{ platform }}' ILIKE '%raydium%' THEN 'raydium'
+            WHEN '{{ platform }}' ILIKE '%orca%' THEN 'orca'
+            WHEN '{{ platform }}' ILIKE '%meteora%' THEN 'meteora'
+        END AS platform,
+        _inserted_timestamp,
+        {{ 'initialization_pools_' ~ platform ~ '_id' }} AS liquidity_pools_id
+    FROM
+        {{ ref('silver__initialization_pools_' ~ platform) }} AS p
+    LEFT JOIN
+        {{ ref('price__ez_asset_metadata') }} AS m
+        ON p.token_a_mint = m.token_address
+    LEFT JOIN
+        {{ ref('price__ez_asset_metadata') }} AS m2
+        ON p.token_b_mint = m2.token_address
+        -- where p.modified_timestamp::Date between '2025-01-01' and '2025-01-20'
+        -- where block_timestamp::Date < '2024-06-01'
+        {% if is_incremental() %}
+            WHERE p._inserted_timestamp > (SELECT max(_inserted_timestamp) FROM {{ this }})
+        {% endif %}
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+{% endfor %}
+),
+
+fill_null_symbols AS (
+    SELECT
+        pool_address,
+        case when token_a_symbol is null then
+        live.udf_api(
+            'GET',
+            concat('{Service}/token/meta?address=',token_a_mint),
+            object_construct(
+                'Content-Type',
+                'application/json',
+                'token',
+                '{Authentication}'
+            ),
+            {},
+            'Vault/prod/solana/solscan/v2'
+        ):data:data:symbol::string 
+        else token_a_symbol
+        end AS token_a_symbol,
+        case when token_b_symbol is null then
+        live.udf_api(
+            'GET',
+            concat('{Service}/token/meta?address=',token_b_mint),
+            object_construct(
+                'Content-Type',
+                'application/json',
+                'token',
+                '{Authentication}'
+            ),
+            {},
+            'Vault/prod/solana/solscan/v2'
+        ):data:data:symbol::string 
+        else token_b_symbol
+        end AS token_b_symbol
+    FROM
+        base
+    WHERE
+        (token_a_symbol IS NULL
+        OR token_b_symbol IS NULL)
+    limit 1000
+),
+
+pre_final AS (
+    SELECT
+        b.pool_address,
+        pool_token_mint,
+        token_a_mint,
+        coalesce(b.token_a_symbol,s.token_a_symbol) AS token_a_symbol,
+        token_a_account,
+        token_b_mint,
+        coalesce(b.token_b_symbol,s.token_b_symbol) AS token_b_symbol,
+        token_b_account,
+        initialized_at_block_id,
+        initialized_at_block_timestamp,
+        initialized_at_tx_id,
+        initialized_at_index,
+        initialized_at_inner_index,
+        program_id,
+        platform,
+        liquidity_pools_id,
+        _inserted_timestamp
+    FROM
+        base AS b
+    LEFT JOIN
+        fill_null_symbols AS s
+        ON b.pool_address = s.pool_address
+)
+
+SELECT
+    pool_address,
+    CASE
+        WHEN token_a_symbol IS NOT NULL 
+        AND token_b_symbol IS NOT NULL THEN 
+            token_a_symbol || '-' || token_b_symbol
+        ELSE
+            NULL
+    END AS pool_name,
+    pool_token_mint,
+    token_a_mint,
+    token_a_symbol,
+    token_a_account,
+    token_b_mint,
+    token_b_symbol,
+    token_b_account,
+    initialized_at_block_id,
+    initialized_at_block_timestamp,
+    initialized_at_tx_id,
+    initialized_at_index,
+    initialized_at_inner_index,
+    program_id,
+    platform,
+    liquidity_pools_id,
+    _inserted_timestamp,
+    sysdate() AS inserted_timestamp,
+    sysdate() AS modified_timestamp
+FROM
+    pre_final
