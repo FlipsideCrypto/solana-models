91a59e07eccbfc8b595cb864904161e970b8843f
tarikceric <tarik@flipsidecrypto.com>
2025-06-09 14:06:12 -0700

    convert core views to tables

--- models/gold/core/core__ez_events_decoded.sql
+++ models/gold/core/core__ez_events_decoded.sql
@@ -1,24 +1,48 @@
 {{ config(
-    materialized = 'view',
-    tags = ['scheduled_non_core'],
+    materialized = 'incremental',
+    unique_key = ['ez_events_decoded_id'],
+    incremental_predicates = ["dynamic_range_predicate", "block_timestamp::date"],
+    cluster_by = ['block_timestamp::DATE','ROUND(block_id, -3)','program_id'],
+    merge_exclude_columns = ["inserted_timestamp"],
+    post_hook = enable_search_optimization('{{this.schema}}', '{{this.identifier}}', 'ON EQUALITY(tx_id, event_type)'),
+    full_refresh = false,
+    tags = ['scheduled_non_core']
 ) }}
 
+{% if execute %}
+    {% if is_incremental() %}
+    {% set max_modified_query %}
+    SELECT
+        MAX(modified_timestamp) AS modified_timestamp
+    FROM
+        {{ this }}
+    {% endset %}
+    {% set max_modified_timestamp = run_query(max_modified_query)[0][0] %}
+    {% endif %}
+{% endif %}
+
 SELECT
-    A.block_timestamp,
-    A.block_id,
-    A.tx_id,
-    A.signers,
-    A.succeeded,
-    A.index,
-    A.inner_index,
-    A.event_type,
-    A.program_id,
-    A.decoded_instruction,
-    A.decoded_instruction :accounts :: ARRAY AS decoded_accounts,
-    A.decoded_instruction :args :: variant AS decoded_args,
-    A.decoded_instruction :error :: STRING AS decoding_error,
-    A.decoded_instructions_combined_id AS ez_events_decoded_id,
-    A.inserted_timestamp,
-    A.modified_timestamp
+    block_timestamp,
+    block_id,
+    tx_id,
+    signers,
+    succeeded,
+    index,
+    inner_index,
+    event_type,
+    program_id,
+    decoded_instruction,
+    decoded_instruction :accounts :: ARRAY AS decoded_accounts,
+    decoded_instruction :args :: variant AS decoded_args,
+    decoded_instruction :error :: STRING AS decoding_error,
+    decoded_instructions_combined_id AS ez_events_decoded_id,
+    sysdate() AS inserted_timestamp,
+    sysdate() AS modified_timestamp
 FROM
-    {{ ref('silver__decoded_instructions_combined') }} A
+    {{ ref('silver__decoded_instructions_combined') }}
+{% if is_incremental() %}
+WHERE
+    modified_timestamp >= '{{ max_modified_timestamp }}'
+{% endif %}
+
+
--- models/gold/core/core__fact_events.sql
+++ models/gold/core/core__fact_events.sql
@@ -1,7 +1,25 @@
 {{ config(
-    materialized = 'view',
+    materialized = 'incremental',
+    unique_key = ['fact_events_id'],
+    incremental_predicates = ["dynamic_range_predicate", "block_timestamp::date"],
+    cluster_by = ['block_timestamp::DATE','ROUND(block_id, -3)','program_id','succeeded'],
+    merge_exclude_columns = ["inserted_timestamp"],
+    post_hook = enable_search_optimization('{{this.schema}}', '{{this.identifier}}', 'ON EQUALITY(tx_id, index, event_type)'),
+    full_refresh = false,
     tags = ['scheduled_core']
 ) }}
+
+{% if execute %}
+    {% if is_incremental() %}
+    {% set max_modified_query %}
+    SELECT
+        MAX(modified_timestamp) AS modified_timestamp
+    FROM
+        {{ this }}
+    {% endset %}
+    {% set max_modified_timestamp = run_query(max_modified_query)[0][0] %}
+    {% endif %}
+{% endif %}
 
 SELECT 
     block_timestamp,
@@ -14,19 +32,13 @@ SELECT
     program_id,
     instruction,
     inner_instruction,
-    COALESCE (
-        events_id,
-        {{ dbt_utils.generate_surrogate_key(
-            ['block_id', 'tx_id', 'index']
-        ) }}
-    ) AS fact_events_id,
-    COALESCE(
-        inserted_timestamp,
-        '2000-01-01'
-    ) AS inserted_timestamp,
-    COALESCE(
-        modified_timestamp,
-        '2000-01-01'
-    ) AS modified_timestamp
+    events_id AS fact_events_id,
+    sysdate() AS inserted_timestamp,
+    sysdate() AS modified_timestamp
 FROM
     {{ ref('silver__events') }}
+
+{% if is_incremental() %}
+WHERE
+    modified_timestamp >= '{{ max_modified_timestamp }}'
+{% endif %}
