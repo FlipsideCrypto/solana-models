825e254687c85b8589f5fbff872ae8101090b340
tarikceric <tarik@flipsidecrypto.com>
2025-10-31 13:06:17 -0700

    hot fix signers amm filter

--- models/silver/nfts/silver__nft_sales_amm_sell_decoded.sql
+++ models/silver/nfts/silver__nft_sales_amm_sell_decoded.sql
@@ -191,7 +191,7 @@ mev2_sales AS(
     FROM
         base_decoded
     WHERE
-        COALESCE(signers[1],signers[0])::STRING = 'NTYeYJ1wr4bpM5xo6zx5En44SvJFAd35zTxxNoERYqd'
+        signers[0]::STRING = 'NTYeYJ1wr4bpM5xo6zx5En44SvJFAd35zTxxNoERYqd' OR signers[1]::STRING = 'NTYeYJ1wr4bpM5xo6zx5En44SvJFAd35zTxxNoERYqd'
 ),
 coral_cube_nft_sale_amount AS (
     SELECT
