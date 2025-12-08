e3d229f208d5d2f4befa7f2488bee7a42ef5afb5
tarikceric <tarik@flipsidecrypto.com>
2025-12-08 12:50:27 -0800

    add withdrawStakeAccount event

--- models/silver/protocols/marinade/silver__marinade_liquid_staking_actions.sql
+++ models/silver/protocols/marinade/silver__marinade_liquid_staking_actions.sql
@@ -27,7 +27,7 @@
         {{ ref('silver__decoded_instructions_combined') }}
     WHERE
         program_id = 'MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD'
-        AND event_type IN ('deposit', 'depositStakeAccount', 'orderUnstake', 'claim')
+        AND event_type IN ('deposit', 'depositStakeAccount', 'orderUnstake', 'claim','withdrawStakeAccount')
         AND succeeded
 
     {% if is_incremental() %}
@@ -68,6 +68,19 @@ mints AS (
         {{ ref('silver__token_mint_actions') }} a
     INNER JOIN 
         (SELECT DISTINCT tx_id, block_timestamp::date as bt FROM base WHERE event_type IN ('deposit', 'depositStakeAccount')) b 
+        ON b.tx_id = a.tx_id 
+        AND b.bt = a.block_timestamp::date
+    WHERE
+        a.succeeded
+        AND {{ between_stmts }}
+),
+burns AS (
+    SELECT 
+        a.*
+    FROM 
+        {{ ref('silver__token_burn_actions') }} a
+    INNER JOIN 
+        (SELECT DISTINCT tx_id, block_timestamp::date as bt FROM base WHERE event_type IN ('orderUnstake','withdrawStakeAccount')) b 
         ON b.tx_id = a.tx_id 
         AND b.bt = a.block_timestamp::date
     WHERE
@@ -137,23 +150,29 @@ deposits AS (
     WHERE
         a.event_type IN ('deposit', 'depositStakeAccount')
 ),
-order_unstakes AS (
+unstakes AS (
     SELECT
-        block_id,
-        block_timestamp,
-        tx_id,
-        index,
-        inner_index,
-        signer,
-        event_type AS action_type,
+        a.block_id,
+        a.block_timestamp,
+        a.tx_id,
+        a.index,
+        a.inner_index,
+        a.signer,
+        a.event_type AS action_type,
         silver.udf_get_account_pubkey_by_name('burnMsolAuthority', decoded_instruction:accounts) AS provider_address,
-        (decoded_instruction:args:msolAmount::int) * pow(10, -9) AS msol_burned,
-        program_id,
-        _inserted_timestamp
+        b.burn_amount * pow(10, -9) AS msol_burned,
+        a.program_id,
+        a._inserted_timestamp
     FROM
-        base
+        base a
+    LEFT JOIN burns b 
+        ON a.tx_id = b.tx_id
+        AND a.index = b.index
+        AND b.mint = 'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So'
+        AND COALESCE(b.inner_index, 0) > COALESCE(a.inner_index, -1)
+        AND COALESCE(b.inner_index, 0) < COALESCE(a.next_liquid_staking_action_inner_index, 9999)
     WHERE
-        event_type = 'orderUnstake'
+        a.event_type in ('orderUnstake','withdrawStakeAccount')
 ),
 claims AS (
     SELECT
@@ -223,7 +242,7 @@ SELECT
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS invocation_id
 FROM
-    order_unstakes
+    unstakes
 UNION ALL
 SELECT
     block_id,
